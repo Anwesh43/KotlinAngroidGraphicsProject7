60b11a5aead31c94819080e2e619e640907e1aa3
Anwesh43 <anweshthecool0@gmail.com>
2025-12-12 19:05:37 +0530

    leflineperprotview: adding LLPRNode class

--- leftlineperprotview/src/main/java/com/example/leftlineperprotview/LeftLinePerpRotView.kt
+++ leftlineperprotview/src/main/java/com/example/leftlineperprotview/LeftLinePerpRotView.kt
@@ -121,4 +121,45 @@ class LeftLinePerpRotView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class LLPRNode(var i : Int = 0, val state : State = State()) {
+
+        private var next : LLPRNode? = null
+        private var prev : LLPRNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = LLPRNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawLLPRNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : LLPRNode {
+            var curr : LLPRNode? = prev
+            if (dir === 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
