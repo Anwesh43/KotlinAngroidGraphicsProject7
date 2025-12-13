e76dfa42ea2d73cbd74be98018d10a9667cc55f9
Anwesh43 <anweshthecool0@gmail.com>
2025-12-13 17:35:52 +0530

    linehalgshiftdownview: adding Canvas extension method

--- linehalfshiftdownview/src/main/java/com/example/linehalfshiftdownview/LineHalfShiftDownView.kt
+++ linehalfshiftdownview/src/main/java/com/example/linehalfshiftdownview/LineHalfShiftDownView.kt
@@ -16,14 +16,51 @@ val colors : Array<String> = arrayOf(
     "#C51162",
     "#00C853"
 )
-val parts : Int = 4
+val parts : Int = 5
 val scGap : Float = 0.04f / parts
 val delay : Long = 20
 val backColor : Int = "#BDBDBD".toColorInt()
-val rot : Float = 90f
+val rot : Float = 180f
 val sizeFactor : Float = 5.9f
 val strokeFactor : Float = 90f
 
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+
+fun Canvas.drawXY(x : Float, y : Float, cb : () -> Unit) {
+    save()
+    translate(x, y)
+    cb()
+    restore()
+}
+
+fun Canvas.drawLineHalfShiftDown(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val dsc : (Int) -> Float = {
+        scale.divideScale(it, parts)
+    }
+    drawXY(w / 2, h / 2 + (h / 2) * dsc(4)) {
+        rotate(rot * dsc(2))
+        drawXY(-w * 0.25f * (1 - dsc(1)), 0f) {
+            drawLine(0f, 0f, 0f, -size * dsc(0), paint)
+        }
+        val lines : Int = 2
+        val hSize : Float = size / (lines + 1)
+        for (j in 0..1) {
+           drawXY(0f, -hSize) {
+               drawLine(0f, 0f, size * dsc(3), 0f, paint)
+           }
+        }
+    }
+}
+
+fun Canvas.drawLHSDNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color= colors[i].toColorInt()
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    paint.style = Paint.Style.STROKE
+    drawLineHalfShiftDown(scale, w, h, paint)
+}
