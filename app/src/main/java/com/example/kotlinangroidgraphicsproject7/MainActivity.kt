7c5cbfd8cdc5e604c1cdab9f93aade2e93e8abec
Anwesh43 <anweshthecool0@gmail.com>
2025-12-13 23:39:48 +0530

    linehalfshiftdownview: Adding view in activity

--- app/src/main/java/com/example/kotlinangroidgraphicsproject7/MainActivity.kt
+++ app/src/main/java/com/example/kotlinangroidgraphicsproject7/MainActivity.kt
@@ -12,12 +12,12 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.kotlinangroidgraphicsproject7.ui.theme.KotlinAngroidGraphicsProject7Theme
-import com.example.leftlineperprotview.LeftLinePerpRotView
+import com.example.linehalfshiftdownview.LineHalfShiftDownView
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        LeftLinePerpRotView.create(this)
+        LineHalfShiftDownView.create(this)
 
     }
 }
