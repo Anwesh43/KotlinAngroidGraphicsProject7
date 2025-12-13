a930ffd718d5e5f7b1511e8486aaf6cf5aed3953
Anwesh43 <anweshthecool0@gmail.com>
2025-12-13 23:35:03 +0530

    linehalfshiftdownview: Adding LineHalfShiftDown class

--- linehalfshiftdownview/src/main/java/com/example/linehalfshiftdownview/LineHalfShiftDownView.kt
+++ linehalfshiftdownview/src/main/java/com/example/linehalfshiftdownview/LineHalfShiftDownView.kt
@@ -167,6 +167,28 @@ class LineHalfShiftDownView(ctx : Context) : View(ctx) {
             cb()
             return this
         }
+    }
 
+    data class LineHalfShiftDown(var i : Int) {
+
+        private var curr : LHSDNode = LHSDNode(0)
+        private var dir : Int = 1
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            curr.draw(canvas, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            curr.update {
+                curr = curr.getNext(dir) {
+                    dir *= -1
+                }
+                cb(it)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            curr.startUpdating(cb)
+        }
     }
 }
