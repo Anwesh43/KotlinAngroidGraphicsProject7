d3460c7c682a157d9c0f24df375e2c35e9d2709e
Anwesh43 <anweshthecool0@gmail.com>
2025-12-16 16:07:49 +0530

    biarcbentupview: adding Canvas extension methods

--- biarcbentupview/src/main/java/com/example/biarcbentupview/BiArcBentUpView.kt
+++ biarcbentupview/src/main/java/com/example/biarcbentupview/BiArcBentUpView.kt
@@ -23,7 +23,44 @@ val backColor : Int = "#BDBDBD".toColorInt()
 val scGap : Float = 0.04f / parts
 val strokeFactor : Float = 90f
 val sizeFactor : Float = 5.9f
+val sweep : Float = 30f
+val start : Float = -90f
+val gap : Float = 60f
 
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+
+fun Canvas.drawXY(x : Float, y : Float, cb : () -> Unit) {
+    save()
+    translate(x, y)
+    cb()
+    restore()
+}
+
+fun Canvas.drawBiArcBentUp(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val dsc : (Int) -> Float = {
+        scale.divideScale(it, parts)
+    }
+    drawXY(w / 2, h / 2 - (h / 2) * dsc(5)) {
+        rotate(rot * dsc(4))
+        for (j in 0..1) {
+            drawArc(RectF(-size, -size, size, size), start + gap * j, sweep * dsc(3 * j), false, paint)
+            drawXY(0f, 0f) {
+                rotate(sweep * (j + 1))
+                drawLine(0f, -size * (1 - dsc(1 + j)) * (1 - j), 0f, -size * (1 - j) - size * j * dsc(1 + j), paint)
+            }
+        }
+    }
+}
+
+fun Canvas.drawBABUNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = colors[i].toColorInt()
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    paint.style = Paint.Style.STROKE
+    drawBiArcBentUp(scale, w, h, paint)
+}
