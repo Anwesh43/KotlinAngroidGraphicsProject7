4a6367153570630fde60a02f12109a3138f68de1
Anwesh43 <anweshthecool0@gmail.com>
2025-12-16 17:27:07 +0530

    biarcbentupview: adding BABUNode class

--- biarcbentupview/src/main/java/com/example/biarcbentupview/BiArcBentUpView.kt
+++ biarcbentupview/src/main/java/com/example/biarcbentupview/BiArcBentUpView.kt
@@ -127,4 +127,45 @@ class BiArcBentUpView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class BABUNode(var i : Int = 0, val state : State = State()) {
+
+        private var next : BABUNode? = null
+        private var prev : BABUNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = BABUNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawBABUNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : BABUNode {
+            var curr : BABUNode? = prev
+            if (dir === 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
