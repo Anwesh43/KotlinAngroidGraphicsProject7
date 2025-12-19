6389fec0c12ef619a30bfae2716660d3cfbaa21d
Anwesh43 <anweshthecool0@gmail.com>
2025-12-19 13:39:38 +0530

    bialtarcsweepview: adding BiAltArcSweepView class

--- bialtarcsweepview/src/main/java/com/example/bialtarcsweepview/BiAltArcSweepView.kt
+++ bialtarcsweepview/src/main/java/com/example/bialtarcsweepview/BiAltArcSweepView.kt
@@ -69,3 +69,19 @@ fun Canvas.drawBAASNode(i : Int, scale : Float, paint : Paint) {
     paint.style = Paint.Style.STROKE
     drawBiAltArcSweep(scale, w, h, paint)
 }
+
+class BiAltArcSweepView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN-> {
+
+            }
+        }
+        return true
+    }
+}
