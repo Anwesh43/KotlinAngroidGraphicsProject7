bdc194ddd1a78661abf3d6e36e35617387580631
Anwesh43 <anweshthecool0@gmail.com>
2025-12-20 00:08:44 +0530

    bialtarcsweepview: adding view in activity

--- app/src/main/java/com/example/kotlinangroidgraphicsproject7/MainActivity.kt
+++ app/src/main/java/com/example/kotlinangroidgraphicsproject7/MainActivity.kt
@@ -11,13 +11,13 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import com.example.biarcbentupview.BiArcBentUpView
+import com.example.bialtarcsweepview.BiAltArcSweepView
 import com.example.kotlinangroidgraphicsproject7.ui.theme.KotlinAngroidGraphicsProject7Theme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        BiArcBentUpView.create(this)
+        BiAltArcSweepView.create(this)
 
     }
 }
