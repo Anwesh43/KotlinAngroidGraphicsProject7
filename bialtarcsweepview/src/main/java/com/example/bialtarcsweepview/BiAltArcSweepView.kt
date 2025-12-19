2d73810173212e13b746f1c9b208ee34b4b467ed
Anwesh43 <anweshthecool0@gmail.com>
2025-12-19 13:38:31 +0530

    bialtarcsweepview: adding Canvas extension methods

--- bialtarcsweepview/src/main/java/com/example/bialtarcsweepview/BiAltArcSweepView.kt
+++ bialtarcsweepview/src/main/java/com/example/bialtarcsweepview/BiAltArcSweepView.kt
@@ -16,7 +16,7 @@ val colors: Array<String> = arrayOf(
     "#C51162",
     "#00C853"
 )
-val parts : Int = 5
+val parts : Int = 4
 val scGap : Float = 0.04f / parts
 val strokeFactor : Float = 90f
 val sizeFactor : Float = 5.9f
@@ -28,3 +28,44 @@ val sweep : Float = 90f
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+
+fun Canvas.drawXY(x : Float,y : Float, cb : () -> Unit) {
+    save()
+    translate(x, y)
+    cb()
+    restore()
+}
+
+fun Canvas.drawBiAltArcSweep(scale : Float, w : Float, h : Float, paint : Paint) {
+    val dsc : (Int) -> Float = {
+        scale.divideScale(it, parts)
+    }
+    val size : Float = Math.min(w, h) / sizeFactor
+    drawXY(w / 2, h / 2) {
+        for (j in 0..1) {
+            drawXY((w / 2) * (1f - 2 * j) * dsc(3), 0f) {
+                scale(1f - 2 * j, 1f)
+                drawXY(0f, 0f) {
+                    rotate(rot * dsc(2))
+                    drawArc(
+                        RectF(-size / 2, -size / 2, size / 2, size / 2),
+                        180F,
+                        sweep * (dsc(0) + dsc(2)),
+                        false,
+                        paint
+                    )
+                }
+            }
+        }
+    }
+}
+
+fun Canvas.drawBAASNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = colors[i].toColorInt()
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    paint.style = Paint.Style.STROKE
+    drawBiAltArcSweep(scale, w, h, paint)
+}
