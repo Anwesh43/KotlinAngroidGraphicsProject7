416e32947f51d268719f76acbfa22936b8b643a7
Anwesh43 <anweshthecool0@gmail.com>
2025-12-21 14:23:45 +0530

    concrotsiderightview: adding view in activity

--- app/src/main/java/com/example/kotlinangroidgraphicsproject7/MainActivity.kt
+++ app/src/main/java/com/example/kotlinangroidgraphicsproject7/MainActivity.kt
@@ -11,13 +11,13 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import com.example.bialtarcsweepview.BiAltArcSweepView
+import com.example.concrotsiderightview.ConcRotSideRightView
 import com.example.kotlinangroidgraphicsproject7.ui.theme.KotlinAngroidGraphicsProject7Theme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        BiAltArcSweepView.create(this)
+        ConcRotSideRightView.create(this)
 
     }
 }
