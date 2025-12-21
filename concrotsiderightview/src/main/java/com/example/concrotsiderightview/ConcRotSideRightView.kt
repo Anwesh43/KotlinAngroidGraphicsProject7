f20bff547a7c07285a673af73d121ee6fbefa7de
Anwesh43 <anweshthecool0@gmail.com>
2025-12-21 10:44:37 +0530

    concrotsiderightview: adding ConcRotSideRightView class

--- concrotsiderightview/src/main/java/com/example/concrotsiderightview/ConcRotSideRightView.kt
+++ concrotsiderightview/src/main/java/com/example/concrotsiderightview/ConcRotSideRightView.kt
@@ -61,3 +61,19 @@ fun Canvas.drawCRSRNode(i : Int, scale : Float, paint : Paint) {
     paint.style = Paint.Style.STROKE
     drawConcRotSideRight(scale, w, h, paint)
 }
+
+class ConcRotSideRightView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
