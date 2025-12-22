3ddc01d60778d4c7e96e4b4fc795204a6e20f18d
Anwesh43 <anweshthecool0@gmail.com>
2025-12-22 14:41:54 +0530

    parallellinejoindownview: adding constants and libraries

---
+++ parallellinejoindownview/src/main/java/com/example/parallellinejoindownview/ParallelLineJoinDownView.kt
@@ -0,0 +1,25 @@
+package com.example.parallellinejoindownview
+
+import android.view.View
+import android.view.MotionEvent
+import android.app.Activity
+import android.content.Context
+import android.graphics.Paint
+import android.graphics.Canvas
+import androidx.core.graphics.toColorInt
+
+val colors : Array<String> = arrayOf(
+    "#1A237E",
+    "#EF5350",
+    "#AA00FF",
+    "#C51162",
+    "#00C853"
+)
+val parts : Int = 5
+val rot : Float = 90f
+val mainRot : Float = 180f
+val scGap : Float = 0.04f / parts
+val delay : Long = 20
+val backColor : Int = "#BDBDBD".toColorInt()
+val sizeFactor : Float = 4.9f
+val strokeFactor : Float = 90f
