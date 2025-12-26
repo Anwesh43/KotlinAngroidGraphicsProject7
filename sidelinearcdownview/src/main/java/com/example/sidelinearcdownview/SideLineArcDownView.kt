e681cfc812ad4352c6ab66d80c0513fab134ab46
Anwesh43 <anweshthecool0@gmail.com>
2025-12-26 12:37:13 +0530

    sidelinearcdownview: adding SideLineArcDownView class

--- sidelinearcdownview/src/main/java/com/example/sidelinearcdownview/SideLineArcDownView.kt
+++ sidelinearcdownview/src/main/java/com/example/sidelinearcdownview/SideLineArcDownView.kt
@@ -62,3 +62,19 @@ fun Canvas.drawSLADNode(i : Int, scale : Float, paint : Paint) {
     paint.style = Paint.Style.STROKE
     drawSideLineArcDown(scale, w, h, paint)
 }
+
+class SideLineArcDownView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
