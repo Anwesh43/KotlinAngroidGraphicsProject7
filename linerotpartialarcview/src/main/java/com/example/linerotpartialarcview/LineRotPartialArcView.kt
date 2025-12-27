4e63e63cea39fae7c5c66f86dc357c6a3cc44e71
Anwesh43 <anweshthecool0@gmail.com>
2025-12-27 13:05:30 +0530

    linerotpartialarcview: adding LineRotPartialArcView class

--- linerotpartialarcview/src/main/java/com/example/linerotpartialarcview/LineRotPartialArcView.kt
+++ linerotpartialarcview/src/main/java/com/example/linerotpartialarcview/LineRotPartialArcView.kt
@@ -57,3 +57,19 @@ fun Canvas.drawLRPANode(i : Int, scale : Float, paint : Paint) {
     paint.style = Paint.Style.STROKE
     drawLineRotPartialArc(scale, w, h, paint)
 }
+
+class LineRotPartialArcView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
