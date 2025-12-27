feb7ded9af33bca7ca9d88cd9baefb320f87a2f0
Anwesh43 <anweshthecool0@gmail.com>
2025-12-28 00:50:43 +0530

    biquartersemiarcview: adding module setup

--- app/src/main/java/com/example/kotlinangroidgraphicsproject7/MainActivity.kt
+++ app/src/main/java/com/example/kotlinangroidgraphicsproject7/MainActivity.kt
@@ -12,11 +12,10 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.kotlinangroidgraphicsproject7.ui.theme.KotlinAngroidGraphicsProject7Theme
-import com.example.linerotpartialarcview.LineRotPartialArcView
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        LineRotPartialArcView.create(this)
+        //LineRotPartialArcView.create(this)
     }
 }
