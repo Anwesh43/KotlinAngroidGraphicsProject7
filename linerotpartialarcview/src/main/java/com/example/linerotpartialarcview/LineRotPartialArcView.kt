f6fb47504e5e098101c9124cc4c9b8353f1c2162
Anwesh43 <anweshthecool0@gmail.com>
2025-12-27 14:17:16 +0530

    linerotpartialarcview: adding Renderer in View

--- linerotpartialarcview/src/main/java/com/example/linerotpartialarcview/LineRotPartialArcView.kt
+++ linerotpartialarcview/src/main/java/com/example/linerotpartialarcview/LineRotPartialArcView.kt
@@ -60,14 +60,16 @@ fun Canvas.drawLRPANode(i : Int, scale : Float, paint : Paint) {
 
 class LineRotPartialArcView(ctx : Context) : View(ctx) {
 
-    override fun onDraw(canvas : Canvas) {
+    private val renderer : Renderer = Renderer(this)
 
+    override fun onDraw(canvas : Canvas) {
+        renderer.render(canvas)
     }
 
     override fun onTouchEvent(event : MotionEvent) : Boolean {
         when (event.action) {
             MotionEvent.ACTION_DOWN -> {
-
+                renderer.handleTap()
             }
         }
         return true
