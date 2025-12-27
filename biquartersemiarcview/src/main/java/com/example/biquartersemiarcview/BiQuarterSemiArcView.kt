5b8eea6c002be4cf244663d3528f6ebb5cd4ea4e
Anwesh43 <anweshthecool0@gmail.com>
2025-12-28 01:16:33 +0530

    biquartersemiarcview: adding BiQuarterSemiArcView class

--- biquartersemiarcview/src/main/java/com/example/biquartersemiarcview/BiQuarterSemiArcView.kt
+++ biquartersemiarcview/src/main/java/com/example/biquartersemiarcview/BiQuarterSemiArcView.kt
@@ -62,3 +62,19 @@ fun Canvas.drawBQSANode(i : Int, scale : Float, paint : Paint) {
     paint.style = Paint.Style.STROKE
     drawBiQuarterSemiArc(scale, w, h, paint)
 }
+
+class BiQuarterSemiArcView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent): Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
