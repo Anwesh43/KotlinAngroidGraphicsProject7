6f75a985f56d0e21ce5ec726bd36b9d381458351
Anwesh43 <anweshthecool0@gmail.com>
2025-12-29 19:04:59 +0530

    mirrorintosemicircleview: adding MirrorIntoSemiCircleView class

--- mirrorintosemicircleview/src/main/java/com/example/mirrorintosemicircleview/MirrorIntoSemiCircleView.kt
+++ mirrorintosemicircleview/src/main/java/com/example/mirrorintosemicircleview/MirrorIntoSemiCircleView.kt
@@ -64,3 +64,19 @@ fun Canvas.drawMSCNode(i : Int, scale : Float, paint : Paint) {
     paint.style = Paint.Style.STROKE
     drawMirrorSemiCircle(w, h, scale, paint)
 }
+
+class MirrorIntoSemiCircleView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
