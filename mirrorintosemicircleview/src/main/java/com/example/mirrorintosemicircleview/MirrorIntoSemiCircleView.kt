e2c9baf68ac234df6190bd581f53e8f7650b45ef
Anwesh43 <anweshthecool0@gmail.com>
2025-12-29 19:03:50 +0530

    mirrorintosemicircleview: adding Canvas extension methods

--- mirrorintosemicircleview/src/main/java/com/example/mirrorintosemicircleview/MirrorIntoSemiCircleView.kt
+++ mirrorintosemicircleview/src/main/java/com/example/mirrorintosemicircleview/MirrorIntoSemiCircleView.kt
@@ -27,3 +27,40 @@ val backColor : Int = "#BDBDBD".toColorInt()
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+
+fun Canvas.drawXY(x : Float, y : Float, cb : () -> Unit) {
+    save()
+    translate(x, y)
+    cb()
+    restore()
+}
+
+
+fun Canvas.drawMirrorSemiCircle(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val dsc : (Int) -> Float = {
+        scale.divideScale(it, parts)
+    }
+    drawXY(w / 2, h / 2) {
+        for (j in 0..1) {
+            drawXY(size * j, 0f) {
+                rotate(rot * dsc(4))
+                drawXY(size * 0.5f * j, 0f) {
+                    drawLine(0f, -size * j * (1 - dsc(2 * j + 1)), 0f, -size * (1 - j) * dsc(j), paint)
+                    drawArc(RectF(-size / 2, -size / 2, size / 2, size / 2), -90f + 270f * j, rot * (dsc(j + 1) * (1 - j) + dsc(2 * j) * j), false, paint)
+                }
+
+            }
+        }
+    }
+}
+
+fun Canvas.drawMSCNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = colors[i].toColorInt()
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h)/ strokeFactor
+    paint.style = Paint.Style.STROKE
+    drawMirrorSemiCircle(w, h, scale, paint)
+}
