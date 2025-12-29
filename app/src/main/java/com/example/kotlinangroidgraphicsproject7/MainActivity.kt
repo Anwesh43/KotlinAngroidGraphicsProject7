60ceb635d4f7d73a2650672c6615c2e0f1964836
Anwesh43 <anweshthecool0@gmail.com>
2025-12-29 19:31:05 +0530

    mirrorintosemicircleview: adding view in activity

--- app/src/main/java/com/example/kotlinangroidgraphicsproject7/MainActivity.kt
+++ app/src/main/java/com/example/kotlinangroidgraphicsproject7/MainActivity.kt
@@ -11,13 +11,13 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import com.example.biquartersemiarcview.BiQuarterSemiArcView
 import com.example.kotlinangroidgraphicsproject7.ui.theme.KotlinAngroidGraphicsProject7Theme
+import com.example.mirrorintosemicircleview.MirrorIntoSemiCircleView
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         //LineRotPartialArcView.create(this)
-        BiQuarterSemiArcView.create(this)
+        MirrorIntoSemiCircleView.create(this)
     }
 }
