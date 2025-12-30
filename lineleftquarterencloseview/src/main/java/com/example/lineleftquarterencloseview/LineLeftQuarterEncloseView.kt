cef5564bf3724375658c981208d3ac7d3b926e25
Anwesh43 <anweshthecool0@gmail.com>
2025-12-30 09:19:45 +0530

    lineleftquarterencloseview: adding LineLeftQuarterEncloseView class

--- lineleftquarterencloseview/src/main/java/com/example/lineleftquarterencloseview/LineLeftQuarterEncloseView.kt
+++ lineleftquarterencloseview/src/main/java/com/example/lineleftquarterencloseview/LineLeftQuarterEncloseView.kt
@@ -59,3 +59,19 @@ fun Canvas.drawLLQENode(i : Int, scale : Float, paint : Paint) {
     paint.style = Paint.Style.STROKE
     drawLineLeftQuarterEnclose(w, h, scale, paint)
 }
+
+class LineLeftQuarterEncloseView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
