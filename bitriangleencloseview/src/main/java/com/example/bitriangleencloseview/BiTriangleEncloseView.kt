2c09b51abfab798f428ad4c7ec9d45ab35b3f86f
Anwesh43 <anweshthecool0@gmail.com>
2025-12-31 01:16:48 +0530

    bitriangleencloseview: adding BiTriangleEncloseView class

--- bitriangleencloseview/src/main/java/com/example/bitriangleencloseview/BiTriangleEncloseView.kt
+++ bitriangleencloseview/src/main/java/com/example/bitriangleencloseview/BiTriangleEncloseView.kt
@@ -62,3 +62,19 @@ fun Canvas.drawBTENode(i : Int, scale : Float, paint : Paint) {
     paint.style = Paint.Style.STROKE
     drawBiTirangleEnclose(scale, w, h, paint)
 }
+
+class BiTriangleEncloseView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
