600348ceffea9d902389ac36ac20cad671d28c75
Anwesh43 <anweshthecool0@gmail.com>
2025-12-31 01:36:48 +0530

    bitriangleencloseview: adding animate method in Animator class

--- bitriangleencloseview/src/main/java/com/example/bitriangleencloseview/BiTriangleEncloseView.kt
+++ bitriangleencloseview/src/main/java/com/example/bitriangleencloseview/BiTriangleEncloseView.kt
@@ -101,7 +101,15 @@ class BiTriangleEncloseView(ctx : Context) : View(ctx) {
     data class Animator(var view : View, var animated : Boolean = false) {
 
         fun animate(cb : () -> Unit) {
+            if (animated) {
+                cb()
+                try {
+                    Thread.sleep(delay)
+                    view.invalidate()
+                } catch (ex : Exception) {
 
+                }
+            }
         }
 
         fun start() {
