ccade1e6831d838ccf689d35e086ae3ed4a6c71a
Anwesh43 <anweshthecool0@gmail.com>
2025-12-30 10:05:16 +0530

    lineleftquarterencloseview: adding LLQENode class

--- lineleftquarterencloseview/src/main/java/com/example/lineleftquarterencloseview/LineLeftQuarterEncloseView.kt
+++ lineleftquarterencloseview/src/main/java/com/example/lineleftquarterencloseview/LineLeftQuarterEncloseView.kt
@@ -123,4 +123,45 @@ class LineLeftQuarterEncloseView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class LLQENode(var i : Int = 0, val state : State = State()) {
+
+        private var next : LLQENode? = null
+        private var prev : LLQENode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = LLQENode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawLLQENode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : LLQENode {
+            var curr : LLQENode? = prev
+            if (dir === 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
