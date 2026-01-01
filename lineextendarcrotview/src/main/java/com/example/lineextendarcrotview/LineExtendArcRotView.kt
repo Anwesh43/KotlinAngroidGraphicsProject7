7cd46b4aa34917c0f499237120ff8047b8dcc75f
Anwesh43 <anweshthecool0@gmail.com>
2026-01-02 02:39:31 +0530

    lineextendarcrotview: adding Canvas extension methods

--- lineextendarcrotview/src/main/java/com/example/lineextendarcrotview/LineExtendArcRotView.kt
+++ lineextendarcrotview/src/main/java/com/example/lineextendarcrotview/LineExtendArcRotView.kt
@@ -27,3 +27,39 @@ val rot : Float = 90f
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+
+fun Canvas.drawXY(x : Float, y : Float, cb : () -> Unit) {
+    save()
+    translate(x, y)
+    cb()
+    restore()
+}
+
+fun Canvas.drawLineExtendArcRot(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val dsc : (Int) -> Float = {
+        scale.divideScale(it, parts)
+    }
+    drawXY(w / 2 + (w / 2) * dsc(4), h / 2) {
+        rotate(rot * dsc(3))
+        drawXY(-size / 2, 0f) {
+            drawLine(0f, 0f, 0f, -size * 0.5f * dsc(0), paint)
+        }
+        drawXY(0f, -size / 2) {
+            drawArc(RectF(-size / 2, -size / 2, size / 2, size / 2), 180f, 180f * dsc(1), false, paint)
+        }
+        drawXY(0f, -size) {
+            drawLine(0f, 0f, 0f, size * dsc(2), paint)
+        }
+    }
+}
+
+fun Canvas.drawLEARNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = colors[i].toColorInt()
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    paint.style = Paint.Style.STROKE
+    drawLineExtendArcRot(scale, w, h, paint)
+}
