692365cdbfe5d16a2f609056c48cd44e3a413a6b
Anwesh43 <anweshthecool0@gmail.com>
2026-01-04 01:44:10 +0530

    lineextendarcdownview: adding LEADNode class

--- lineextendarcdownview/src/main/java/com/example/lineextendarcdownview/LineExtendArcDownView.kt
+++ lineextendarcdownview/src/main/java/com/example/lineextendarcdownview/LineExtendArcDownView.kt
@@ -121,4 +121,45 @@ class LineExtendArcDownView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class LEADNode(var i : Int = 0, val state : State = State()) {
+
+        private var prev : LEADNode? = null
+        private var next : LEADNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = LEADNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawLEADNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : LEADNode {
+            var curr : LEADNode? = prev
+            if (dir === 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
