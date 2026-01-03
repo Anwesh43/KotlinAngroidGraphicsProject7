0bd5ef43d4224f94efec58072294d038ffc3af91
Anwesh43 <anweshthecool0@gmail.com>
2026-01-04 01:41:41 +0530

    lineextendarcdownview: adding Animator class

--- lineextendarcdownview/src/main/java/com/example/lineextendarcdownview/LineExtendArcDownView.kt
+++ lineextendarcdownview/src/main/java/com/example/lineextendarcdownview/LineExtendArcDownView.kt
@@ -99,4 +99,26 @@ class LineExtendArcDownView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class Animator(var view : View, var animated : Boolean = false) {
+
+        fun animate(cb : () -> Unit) {
+            if (animated) {
+
+            }
+        }
+
+        fun start() {
+            if (!animated) {
+                animated = true
+                view.postInvalidate()
+            }
+        }
+
+        fun stop() {
+            if (animated) {
+                animated = false
+            }
+        }
+    }
 }
