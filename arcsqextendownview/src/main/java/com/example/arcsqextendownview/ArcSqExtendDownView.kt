f8f34f6fbcb3db0316cccffe9b101f91c9c56fb1
Anwesh43 <anweshthecool0@gmail.com>
2026-01-07 21:47:55 +0530

    arcqextenddownview: adding ArcSqExtendDownView class

--- arcsqextendownview/src/main/java/com/example/arcsqextendownview/ArcSqExtendDownView.kt
+++ arcsqextendownview/src/main/java/com/example/arcsqextendownview/ArcSqExtendDownView.kt
@@ -56,3 +56,19 @@ fun Canvas.drawASEDNode(i : Int, scale : Float, paint : Paint) {
     paint.strokeCap = Paint.Cap.ROUND
     drawArcSqExtendDown(scale, w, h, paint)
 }
+
+class ArcSqExtendDownView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
