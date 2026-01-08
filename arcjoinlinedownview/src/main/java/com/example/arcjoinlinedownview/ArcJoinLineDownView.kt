1fc768ee1ea56899a3017a836c141824f4c644c3
Anwesh43 <anweshthecool0@gmail.com>
2026-01-08 23:08:18 +0530

    arcjoinlinedownview: adding static create method

--- arcjoinlinedownview/src/main/java/com/example/arcjoinlinedownview/ArcJoinLineDownView.kt
+++ arcjoinlinedownview/src/main/java/com/example/arcjoinlinedownview/ArcJoinLineDownView.kt
@@ -222,4 +222,13 @@ class ArcJoinLineDownView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    companion object {
+
+        fun create(activity: Activity) : ArcJoinLineDownView {
+            val view : ArcJoinLineDownView = ArcJoinLineDownView(activity)
+            activity.setContentView(view)
+            return view
+        }
+    }
 }
