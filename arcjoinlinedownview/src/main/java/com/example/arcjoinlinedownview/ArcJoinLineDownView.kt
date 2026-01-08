b9648a5a1f351be444e6736c45db9acec35a71c1
Anwesh43 <anweshthecool0@gmail.com>
2026-01-08 22:55:43 +0530

    arcjoinlinedownview: adding AJLDNode class

--- arcjoinlinedownview/src/main/java/com/example/arcjoinlinedownview/ArcJoinLineDownView.kt
+++ arcjoinlinedownview/src/main/java/com/example/arcjoinlinedownview/ArcJoinLineDownView.kt
@@ -133,4 +133,45 @@ class ArcJoinLineDownView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class AJLDNode(var i : Int = 0, val state : State = State()) {
+
+        private var next : AJLDNode? = null
+        private var prev : AJLDNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = AJLDNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawAJLDNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : AJLDNode {
+            var curr : AJLDNode? = prev
+            if (dir === 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
