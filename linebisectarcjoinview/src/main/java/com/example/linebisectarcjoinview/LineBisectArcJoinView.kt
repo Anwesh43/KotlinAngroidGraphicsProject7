14c577c9c96817922daf63d7e4d56996e5385d14
Anwesh43 <anweshthecool0@gmail.com>
2026-01-10 12:16:47 +0530

    linebisectarcjoinview: adding LineBisectArcJoinView class

--- linebisectarcjoinview/src/main/java/com/example/linebisectarcjoinview/LineBisectArcJoinView.kt
+++ linebisectarcjoinview/src/main/java/com/example/linebisectarcjoinview/LineBisectArcJoinView.kt
@@ -61,3 +61,19 @@ fun Canvas.drawLBAJNode(i : Int, scale : Float, paint : Paint) {
     paint.style = Paint.Style.STROKE
     drawLineBisectArcJoin(scale, w, h, paint)
 }
+
+class LineBisectArcJoinView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
