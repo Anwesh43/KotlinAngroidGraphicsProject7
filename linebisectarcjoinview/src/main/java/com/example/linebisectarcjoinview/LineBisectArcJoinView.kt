419fd6bfd6dc68779bf58104fc63c221aee30c11
Anwesh43 <anweshthecool0@gmail.com>
2026-01-10 12:10:07 +0530

    linebisectarcjoinview: adding Canvas extension methods

--- linebisectarcjoinview/src/main/java/com/example/linebisectarcjoinview/LineBisectArcJoinView.kt
+++ linebisectarcjoinview/src/main/java/com/example/linebisectarcjoinview/LineBisectArcJoinView.kt
@@ -22,8 +22,42 @@ val strokeFactor : Float = 90f
 val sizeFactor : Float = 5.9f
 val delay : Long = 20
 val backColor : Int = "#BDBDBD".toColorInt()
-val rot : Float = 90f
+val rot : Float = 135f
+val sweep : Float = 90f
 
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+
+fun Canvas.drawXY(x : Float, y : Float, cb : () -> Unit) {
+    save()
+    translate(x, y)
+    cb()
+    restore()
+}
+
+fun Canvas.drawLineBisectArcJoin(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val dsc : (Int) -> Float = {
+        scale.divideScale(it, parts)
+    }
+    drawXY(w / 2 + (w / 2) * dsc(3), h / 2) {
+        for (j in 0..1) {
+            drawXY(-(w / 2) * (1 - dsc(0)), 0f) {
+                rotate(rot * (1f - 2 * j) * dsc(1))
+                drawLine(0f, 0f, -size, 0f, paint)
+            }
+        }
+        drawArc(RectF(-size, -size, size, size), -sweep * 0.5f, sweep * dsc(2), false, paint)
+    }
+}
+
+fun Canvas.drawLBAJNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = colors[i].toColorInt()
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    paint.style = Paint.Style.STROKE
+    drawLineBisectArcJoin(scale, w, h, paint)
+}
