4f64c328b7946aae1c77009c49cbbf412cb0eee4
Anwesh43 <anweshthecool0@gmail.com>
2026-01-11 10:40:49 +0530

    breakarcjoinrightview: adding BreakArcJoinRightView class

--- breakarcjoinrightview/src/main/java/com/example/breakarcjoinrightview/BreakArcJoinRightView.kt
+++ breakarcjoinrightview/src/main/java/com/example/breakarcjoinrightview/BreakArcJoinRightView.kt
@@ -61,3 +61,19 @@ fun Canvas.drawBAJRNode(i : Int, scale : Float, paint : Paint) {
     paint.style = Paint.Style.STROKE
     drawBreakArcJoinRight(scale, w, h, paint)
 }
+
+class BreakArcJoinRightView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
