e339e89afdb65acfff7678d10d844889df7efd26
Anwesh43 <anweshthecool0@gmail.com>
2026-01-11 10:51:43 +0530

    breakarcjoinrightview: adding BAJRNode class

--- breakarcjoinrightview/src/main/java/com/example/breakarcjoinrightview/BreakArcJoinRightView.kt
+++ breakarcjoinrightview/src/main/java/com/example/breakarcjoinrightview/BreakArcJoinRightView.kt
@@ -124,4 +124,45 @@ class BreakArcJoinRightView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class BAJRNode(var i : Int = 0, val state : State = State()) {
+
+        private var next : BAJRNode? = null
+        private var prev : BAJRNode?= null
+
+        init {
+            addNeighbors()
+        }
+
+        fun addNeighbors() {
+            if (i < colors.size - 1) {
+                next = BAJRNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawBAJRNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : BAJRNode {
+            var curr : BAJRNode? = prev
+            if (dir === 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
