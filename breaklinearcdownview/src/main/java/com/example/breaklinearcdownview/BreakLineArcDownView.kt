b793995a00df1b29ce73d0dce429e5c3933495a6
Anwesh43 <anweshthecool0@gmail.com>
2026-01-12 09:07:03 +0530

    breaklinearcdownview: adding BreakLineArcDownView class

--- breaklinearcdownview/src/main/java/com/example/breaklinearcdownview/BreakLineArcDownView.kt
+++ breaklinearcdownview/src/main/java/com/example/breaklinearcdownview/BreakLineArcDownView.kt
@@ -61,3 +61,19 @@ fun Canvas.drawBLADNode(i : Int, scale : Float, paint : Paint) {
     paint.strokeCap = Paint.Cap.ROUND
     drawBreakLineArcDown(scale, w, h, paint)
 }
+
+class BreakLineArcDownView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
