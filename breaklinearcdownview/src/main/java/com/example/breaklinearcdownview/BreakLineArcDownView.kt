dca47f1207d78a13d25a1f228b96c070ed023f16
Anwesh43 <anweshthecool0@gmail.com>
2026-01-12 09:05:04 +0530

    breaklinearcdownview: adding Canvas extension methods

--- breaklinearcdownview/src/main/java/com/example/breaklinearcdownview/BreakLineArcDownView.kt
+++ breaklinearcdownview/src/main/java/com/example/breaklinearcdownview/BreakLineArcDownView.kt
@@ -28,3 +28,36 @@ val sweep : Float = 180f
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float= Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+
+fun Canvas.drawXY(x : Float, y : Float, cb : () -> Unit) {
+    save()
+    translate(x, y)
+    cb()
+    restore()
+}
+
+fun Canvas.drawBreakLineArcDown(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val dsc : (Int) -> Float = {
+        scale.divideScale(it, parts)
+    }
+    drawXY(w / 2, h * 0.5f * (1 + dsc(3))) {
+        for (j in 0..1) {
+            drawXY(0f, h * 0.5f * (1 - dsc(0))) {
+                rotate(rot * (1f - 2 * j) * dsc(1))
+                drawLine(0f, 0f, 0f, -size / 2, paint)
+            }
+        }
+        drawArc(RectF(-size / 2, -size / 2, size / 2 , size / 2), 0f, sweep * dsc(2), false, paint)
+    }
+}
+
+fun Canvas.drawBLADNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = colors[i].toColorInt()
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    paint.style = Paint.Style.STROKE
+    paint.strokeCap = Paint.Cap.ROUND
+    drawBreakLineArcDown(scale, w, h, paint)
+}
