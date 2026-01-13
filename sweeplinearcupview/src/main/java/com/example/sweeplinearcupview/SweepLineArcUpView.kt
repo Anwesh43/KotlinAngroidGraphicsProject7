a4db242c2d949695c034f6a7f1194b227279a181
Anwesh43 <anweshthecool0@gmail.com>
2026-01-14 02:04:12 +0530

    sweeplinearcupview: adding SweepLineArcUpView class

--- sweeplinearcupview/src/main/java/com/example/sweeplinearcupview/SweepLineArcUpView.kt
+++ sweeplinearcupview/src/main/java/com/example/sweeplinearcupview/SweepLineArcUpView.kt
@@ -62,3 +62,19 @@ fun Canvas.drawSLAUNode(i : Int, scale : Float, paint : Paint) {
     paint.style = Paint.Style.STROKE
     drawSweepLineArcUp(scale, w, h, paint)
 }
+
+class SweepLineArcUpView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
