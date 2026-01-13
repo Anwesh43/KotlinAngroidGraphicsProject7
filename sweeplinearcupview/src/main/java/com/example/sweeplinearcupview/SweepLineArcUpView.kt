8fac0939dfc5d4ab4c94ac4971fef358cef4c792
Anwesh43 <anweshthecool0@gmail.com>
2026-01-14 02:03:05 +0530

    sweeplinearcupview: adding Canvas extension methods

--- sweeplinearcupview/src/main/java/com/example/sweeplinearcupview/SweepLineArcUpView.kt
+++ sweeplinearcupview/src/main/java/com/example/sweeplinearcupview/SweepLineArcUpView.kt
@@ -23,7 +23,42 @@ val sizeFactor : Float = 5.9f
 val delay : Long = 20
 val backColor : Int = "#BDBDBD".toColorInt()
 val rot : Float = 135f
+val deg : Float = -180f
+val sweep : Float = 90f
 
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+
+fun Canvas.drawXY(x : Float, y : Float, cb : () -> Unit) {
+    save()
+    translate(x, y)
+    cb()
+    restore()
+}
+
+fun Canvas.drawSweepLineArcUp(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val dsc : (Int) -> Float = {
+        scale.divideScale(it, parts)
+    }
+    drawXY(w / 2, h / 2 - (h / 2) * dsc(5)) {
+        rotate(rot * dsc(4))
+        drawLine(0f, 0f, -size * dsc(0), 0f, paint)
+        drawXY(-size, 0f) {
+            rotate(deg * dsc(2))
+            drawLine(0f, 0f, 0f, -size * dsc(1), paint)
+        }
+        drawArc(RectF(-size, -size, size, size), sweep, sweep * dsc(3), false, paint)
+    }
+}
+
+fun Canvas.drawSLAUNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = colors[i].toColorInt()
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    paint.style = Paint.Style.STROKE
+    drawSweepLineArcUp(scale, w, h, paint)
+}
