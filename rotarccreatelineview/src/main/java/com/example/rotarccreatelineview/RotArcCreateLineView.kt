0a0a97ccc4d5b6e9146ee1604537d38f64df933c
Anwesh43 <anweshthecool0@gmail.com>
2026-01-15 04:11:57 +0530

    rotarccreatelineview: adding RACLNodeclass

--- rotarccreatelineview/src/main/java/com/example/rotarccreatelineview/RotArcCreateLineView.kt
+++ rotarccreatelineview/src/main/java/com/example/rotarccreatelineview/RotArcCreateLineView.kt
@@ -123,4 +123,45 @@ class RotArcCreateLineView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class RACLNode(var i : Int = 0, val state : State = State()) {
+
+        private var prev : RACLNode? = null
+        private var next : RACLNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = RACLNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawRACLNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : RACLNode {
+            var curr : RACLNode? = prev
+            if (dir === 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
