e847063295f4cdb28eb153c5005d1f4a0d1147ce
Anwesh43 <anweshthecool0@gmail.com>
2026-01-16 02:16:25 +0530

    partialrotarcrightview: adding Canvas extension methods

--- partialrotarcrightview/src/main/java/com/example/partialrotarcrightview/PartialRotArcRightView.kt
+++ partialrotarcrightview/src/main/java/com/example/partialrotarcrightview/PartialRotArcRightView.kt
@@ -28,3 +28,35 @@ val sweep : Float = 90f
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+
+fun Canvas.drawXY(x : Float, y : Float, cb : () -> Unit) {
+    save()
+    translate(x, y)
+    cb()
+    restore()
+}
+
+fun Canvas.drawPartialRotArcRight(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val dsc : (Int) -> Float = {
+        scale.divideScale(it, parts)
+    }
+    drawXY(w  / 2 + (w / 2) * dsc(4), h / 2) {
+        rotate(rot * dsc(3))
+        drawXY(-size, 0f) {
+            drawLine(0f, 0f, size * dsc(0), 0f, paint)
+            drawLine(0f, 0f, 0f, -size * dsc(1), paint)
+            drawArc(RectF(-size, -size, size, size), -90f, 90f * dsc(2), false, paint)
+        }
+    }
+}
+
+fun Canvas.drawPRARNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = colors[i].toColorInt()
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    paint.style = Paint.Style.STROKE
+    drawPartialRotArcRight(scale, w, h, paint)
+}
