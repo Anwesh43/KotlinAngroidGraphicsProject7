f1fe680e1f60be8b618e61207c02e095c1b48222
Anwesh43 <anweshthecool0@gmail.com>
2026-01-16 02:57:35 +0530

    partialrotarcrightview: adding view in activity

--- app/src/main/java/com/example/kotlinangroidgraphicsproject7/MainActivity.kt
+++ app/src/main/java/com/example/kotlinangroidgraphicsproject7/MainActivity.kt
@@ -12,12 +12,12 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.kotlinangroidgraphicsproject7.ui.theme.KotlinAngroidGraphicsProject7Theme
-import com.example.rotarccreatelineview.RotArcCreateLineView
+import com.example.partialrotarcrightview.PartialRotArcRightView
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         //LineRotPartialArcView.create(this)
-        RotArcCreateLineView.create(this)
+        PartialRotArcRightView.create(this)
     }
 }
