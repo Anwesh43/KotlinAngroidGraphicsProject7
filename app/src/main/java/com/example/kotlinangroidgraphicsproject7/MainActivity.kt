8562ae156582fe70e2cdffe8173d4c294ebd369d
Anwesh43 <anweshthecool0@gmail.com>
2026-01-17 11:45:05 +0530

    linejoinendarcview: adding view in activity

--- app/src/main/java/com/example/kotlinangroidgraphicsproject7/MainActivity.kt
+++ app/src/main/java/com/example/kotlinangroidgraphicsproject7/MainActivity.kt
@@ -12,12 +12,12 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.kotlinangroidgraphicsproject7.ui.theme.KotlinAngroidGraphicsProject7Theme
-import com.example.partialrotarcrightview.PartialRotArcRightView
+import com.example.linejoinendarcview.LineJoinEndArcView
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         //LineRotPartialArcView.create(this)
-        PartialRotArcRightView.create(this)
+        LineJoinEndArcView.create(this)
     }
 }
