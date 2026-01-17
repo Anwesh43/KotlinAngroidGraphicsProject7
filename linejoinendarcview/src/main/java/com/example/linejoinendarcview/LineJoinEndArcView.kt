487cc82af18e7e24140e37e355ffd280a99f8f01
Anwesh43 <anweshthecool0@gmail.com>
2026-01-17 11:24:33 +0530

    linejoinendarcview: adding LineJoinEndArcView class

--- linejoinendarcview/src/main/java/com/example/linejoinendarcview/LineJoinEndArcView.kt
+++ linejoinendarcview/src/main/java/com/example/linejoinendarcview/LineJoinEndArcView.kt
@@ -61,3 +61,19 @@ fun Canvas.drawLJEANode(i : Int, scale : Float, paint : Paint) {
     paint.style = Paint.Style.STROKE
     drawLineJoinEndArc(scale, w, h, paint)
 }
+
+class LineJoinEndArcView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
