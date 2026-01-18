3459fc509dbad1f6d1bac7dfd3cb64b2528bc4fd
Anwesh43 <anweshthecool0@gmail.com>
2026-01-18 10:38:58 +0530

    parallellinterlinejoinview: adding view in activity

--- app/src/main/java/com/example/kotlinangroidgraphicsproject7/MainActivity.kt
+++ app/src/main/java/com/example/kotlinangroidgraphicsproject7/MainActivity.kt
@@ -12,12 +12,12 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.kotlinangroidgraphicsproject7.ui.theme.KotlinAngroidGraphicsProject7Theme
-import com.example.linejoinendarcview.LineJoinEndArcView
+import com.example.parallellinterlinejoinview.ParallelInterLineJoinView
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         //LineRotPartialArcView.create(this)
-        LineJoinEndArcView.create(this)
+        ParallelInterLineJoinView.create(this)
     }
 }
