7e60905fce7dfad0ca6217e78e8a15d7da768e6d
Anwesh43 <anweshthecool0@gmail.com>
2026-01-18 10:34:41 +0530

    parallellinterlinejoinview: adding static create method

--- parallellinterlinejoinview/src/main/java/com/example/parallellinterlinejoinview/ParallelInterLineJoinView.kt
+++ parallellinterlinejoinview/src/main/java/com/example/parallellinterlinejoinview/ParallelInterLineJoinView.kt
@@ -215,4 +215,13 @@ class ParallelInterLineJoinView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    companion object {
+
+        fun create(activity: Activity) : ParallelInterLineJoinView {
+            val view : ParallelInterLineJoinView = ParallelInterLineJoinView(activity)
+            activity.setContentView(view)
+            return view
+        }
+    }
 }
