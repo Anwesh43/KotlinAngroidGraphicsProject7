4406cedddceabd8309b96a03f118ff34a58fad75
Anwesh43 <anweshthecool0@gmail.com>
2026-01-20 02:15:50 +0530

    bentlinearcrightview: adding view in activity

--- app/src/main/java/com/example/kotlinangroidgraphicsproject7/MainActivity.kt
+++ app/src/main/java/com/example/kotlinangroidgraphicsproject7/MainActivity.kt
@@ -11,13 +11,13 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import com.example.arclineslantrightview.ArcLineSlantRightView
+import com.example.bentlinearcrightview.BentLineArcRightView
 import com.example.kotlinangroidgraphicsproject7.ui.theme.KotlinAngroidGraphicsProject7Theme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         //LineRotPartialArcView.create(this)
-        ArcLineSlantRightView.create(this)
+        BentLineArcRightView.create(this)
     }
 }
