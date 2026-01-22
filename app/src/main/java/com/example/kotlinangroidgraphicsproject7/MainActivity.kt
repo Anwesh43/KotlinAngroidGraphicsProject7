966d1dea7733ce0167a60b80ba92cecda3e1d156
Anwesh43 <anweshthecool0@gmail.com>
2026-01-22 14:08:09 +0530

    linearcdroprotview: adding view in activity

--- app/src/main/java/com/example/kotlinangroidgraphicsproject7/MainActivity.kt
+++ app/src/main/java/com/example/kotlinangroidgraphicsproject7/MainActivity.kt
@@ -11,13 +11,13 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import com.example.joinlinesemicircleview.JoinLineSemiCircleView
 import com.example.kotlinangroidgraphicsproject7.ui.theme.KotlinAngroidGraphicsProject7Theme
+import com.example.linearcdroprotview.LineArcDropRotView
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         //LineRotPartialArcView.create(this)
-        JoinLineSemiCircleView.create(this)
+        LineArcDropRotView.create(this)
     }
 }
