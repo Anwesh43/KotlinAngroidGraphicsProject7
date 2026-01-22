f7cc665c02f0c2db41b2e2dd098f65ef6f5dfbdb
Anwesh43 <anweshthecool0@gmail.com>
2026-01-22 13:18:36 +0530

    linearcdroprotview: adding LineArcDropRotView class

--- linearcdroprotview/src/main/java/com/example/linearcdroprotview/LineArcDropRotView.kt
+++ linearcdroprotview/src/main/java/com/example/linearcdroprotview/LineArcDropRotView.kt
@@ -59,3 +59,19 @@ fun Canvas.drawLADRNode(i : Int, scale : Float, paint : Paint) {
     paint.style = Paint.Style.STROKE
     drawLineArcDropRot(scale, w, h, paint)
 }
+
+class LineArcDropRotView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
