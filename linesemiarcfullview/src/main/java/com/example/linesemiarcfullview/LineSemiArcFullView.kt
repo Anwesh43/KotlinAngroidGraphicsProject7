f8753fd1d98c0f6d9e57d551b16315808ee3fe9c
Anwesh43 <anweshthecool0@gmail.com>
2026-01-24 16:26:11 +0530

    linesemiarcfullview: adding LineSemiArcFull class

--- linesemiarcfullview/src/main/java/com/example/linesemiarcfullview/LineSemiArcFullView.kt
+++ linesemiarcfullview/src/main/java/com/example/linesemiarcfullview/LineSemiArcFullView.kt
@@ -166,4 +166,27 @@ class LineSemiArcFullView(ctx : Context) : View(ctx) {
             return this
         }
     }
+
+    data class LineSemiArcFull(var i : Int) {
+
+        private var curr : LSAFNode = LSAFNode(0)
+        private var dir : Int = 1
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            curr.draw(canvas, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            curr.update {
+                curr = curr.getNext(dir) {
+                    dir *= -1
+                }
+                cb(it)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            curr.startUpdating(cb)
+        }
+    }
 }
