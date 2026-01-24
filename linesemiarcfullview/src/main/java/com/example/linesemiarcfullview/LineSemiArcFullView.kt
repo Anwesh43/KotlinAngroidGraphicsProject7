743ca0e5c2c76d8baf5caf45b5ab692713f8cb63
Anwesh43 <anweshthecool0@gmail.com>
2026-01-24 12:02:55 +0530

    linesemiarcfullview: adding Canvas extension methods

--- linesemiarcfullview/src/main/java/com/example/linesemiarcfullview/LineSemiArcFullView.kt
+++ linesemiarcfullview/src/main/java/com/example/linesemiarcfullview/LineSemiArcFullView.kt
@@ -23,7 +23,42 @@ val sweep : Float = 180f
 val sizeFactor : Float = 5.9f
 val delay : Long = 20
 val backColor : Int = "#BDBDBD".toColorInt()
+val strokeFactor : Float = 90f
 
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+
+fun Canvas.drawXY(x : Float, y : Float, cb : () -> Unit) {
+    save()
+    translate(x, y)
+    cb()
+    restore()
+}
+
+fun Canvas.drawLineSemiArcFull(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val dsc : (Int) -> Float = {
+        scale.divideScale(it, parts)
+    }
+    drawXY(w / 2 + (w / 2) * dsc(4), h  / 2) {
+        rotate(rot * dsc(2))
+        drawLine(0f, 0f, 0f, -size * dsc(0), paint)
+        drawArc(RectF(-size / 2, -size, size / 2, 0f), -90f, sweep * dsc(1), false, paint)
+        for (j in 0..1) {
+            drawXY(0f, -size * 0.5f * j) {
+                drawArc(RectF(-size / 4, -size / 2, size / 4, 0f), 90f, sweep * dsc(3).divideScale(j, 2), false, paint)
+            }
+        }
+    }
+}
+
+fun Canvas.drawLSAFNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = colors[i].toColorInt()
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    paint.style = Paint.Style.STROKE
+    drawLineSemiArcFull(scale, w, h, paint)
+}
