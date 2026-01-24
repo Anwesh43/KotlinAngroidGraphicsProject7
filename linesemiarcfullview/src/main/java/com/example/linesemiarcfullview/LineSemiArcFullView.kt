be41b82ee53b7f55b9c15073018f921bf6a06f9b
Anwesh43 <anweshthecool0@gmail.com>
2026-01-24 12:16:03 +0530

    linesemiarcfullview: adding LineSemiArcFullView class

--- linesemiarcfullview/src/main/java/com/example/linesemiarcfullview/LineSemiArcFullView.kt
+++ linesemiarcfullview/src/main/java/com/example/linesemiarcfullview/LineSemiArcFullView.kt
@@ -62,3 +62,19 @@ fun Canvas.drawLSAFNode(i : Int, scale : Float, paint : Paint) {
     paint.style = Paint.Style.STROKE
     drawLineSemiArcFull(scale, w, h, paint)
 }
+
+class LineSemiArcFullView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
