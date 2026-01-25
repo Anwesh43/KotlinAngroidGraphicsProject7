5218a1c63983a56fd8b68067a6d5971a6340834b
Anwesh43 <anweshthecool0@gmail.com>
2026-01-25 10:24:56 +0530

    linebentarcfullview: adding LineBentArcFullView class

--- linebentarcfullview/src/main/java/com/example/linebentarcfullview/LineBentArcFullView.kt
+++ linebentarcfullview/src/main/java/com/example/linebentarcfullview/LineBentArcFullView.kt
@@ -59,3 +59,19 @@ fun Canvas.drawLBAFNode(i : Int, scale : Float, paint : Paint) {
     paint.style = Paint.Style.STROKE
     drawLineBentArcFull(scale, w, h, paint)
 }
+
+class LineBentArcFullView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
