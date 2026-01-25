6ddde64a845cec059627aadc9181443e8c4ffee6
Anwesh43 <anweshthecool0@gmail.com>
2026-01-25 10:22:20 +0530

    linebentarcfullview: adding Canvas extension methods

--- linebentarcfullview/src/main/java/com/example/linebentarcfullview/LineBentArcFullView.kt
+++ linebentarcfullview/src/main/java/com/example/linebentarcfullview/LineBentArcFullView.kt
@@ -29,3 +29,33 @@ fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
 
+fun Canvas.drawXY(x : Float, y : Float, cb : () -> Unit) {
+    save()
+    translate(x, y)
+    cb()
+    restore()
+}
+
+fun Canvas.drawLineBentArcFull(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val dsc : (Int) -> Float = {
+        scale.divideScale(it, parts)
+    }
+    drawXY(w / 2, h / 2 - (h / 2) * dsc(4)) {
+        drawXY(0f, -size) {
+            rotate(rot * (1 - dsc(2)))
+            drawLine(0f, size * (1 - dsc(0)), 0f, size, paint)
+        }
+        drawArc(RectF(-size / 2, -size, size / 2, 0f), -90f, sweep * (dsc(1) + dsc(3)), false, paint)
+    }
+}
+
+fun Canvas.drawLBAFNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = colors[i].toColorInt()
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    paint.style = Paint.Style.STROKE
+    drawLineBentArcFull(scale, w, h, paint)
+}
