d131ebb8b720b839b2f958e968be25237d021501
Anwesh43 <anweshthecool0@gmail.com>
2026-01-25 10:37:24 +0530

    linebentarcfullview: adding LineBentArcFull class

--- linebentarcfullview/src/main/java/com/example/linebentarcfullview/LineBentArcFullView.kt
+++ linebentarcfullview/src/main/java/com/example/linebentarcfullview/LineBentArcFullView.kt
@@ -163,4 +163,27 @@ class LineBentArcFullView(ctx : Context) : View(ctx) {
             return this
         }
     }
+
+    data class LineBentArcFull(var i : Int) {
+
+        private var curr : LBAFNode = LBAFNode(0)
+        private var dir : Int = 1
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            curr.draw(canvas, paint)
+        }
+
+        fun update(cb : (Floa) -> Unit) {
+            curr.update {
+                curr = curr.getNext(dir) {
+                    dir *= -1
+                }
+                cb(it)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            curr.startUpdating(cb)
+        }
+    }
 }
