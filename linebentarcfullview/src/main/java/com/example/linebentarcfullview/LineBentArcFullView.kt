4e535fe63aea0be289d549f6690e628995b41c0f
Anwesh43 <anweshthecool0@gmail.com>
2026-01-25 10:33:41 +0530

    linebentarcfullview: adding LBAFNode class

--- linebentarcfullview/src/main/java/com/example/linebentarcfullview/LineBentArcFullView.kt
+++ linebentarcfullview/src/main/java/com/example/linebentarcfullview/LineBentArcFullView.kt
@@ -122,4 +122,45 @@ class LineBentArcFullView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class LBAFNode(var i : Int = 0, val state : State = State()) {
+
+        private var next : LBAFNode? = null
+        private var prev : LBAFNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = LBAFNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawLBAFNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : LBAFNode {
+            var curr : LBAFNode? = prev
+            if (dir === 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
