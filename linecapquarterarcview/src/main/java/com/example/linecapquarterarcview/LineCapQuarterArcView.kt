fcf98e3a6900374a1c2c2e59fa312ae6ce431f7d
Anwesh43 <anweshthecool0@gmail.com>
2026-01-27 06:42:01 +0530

    linecapquarterarcview: adding LineCapQuarterArcView class

--- linecapquarterarcview/src/main/java/com/example/linecapquarterarcview/LineCapQuarterArcView.kt
+++ linecapquarterarcview/src/main/java/com/example/linecapquarterarcview/LineCapQuarterArcView.kt
@@ -56,3 +56,19 @@ fun Canvas.drawLCQANode(i : Int, scale : Float, paint : Paint) {
     paint.style = Paint.Style.STROKE
     drawLineCapQuarterArc(scale, w, h, paint)
 }
+
+class LineCapQuarterArcView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
