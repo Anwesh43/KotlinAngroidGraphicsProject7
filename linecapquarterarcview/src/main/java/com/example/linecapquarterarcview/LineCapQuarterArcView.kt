4165ff4ac56bb628c3849b7707c347f110b9e3e1
Anwesh43 <anweshthecool0@gmail.com>
2026-01-27 06:44:52 +0530

    linecapquarterarcview: adding Animator class

--- linecapquarterarcview/src/main/java/com/example/linecapquarterarcview/LineCapQuarterArcView.kt
+++ linecapquarterarcview/src/main/java/com/example/linecapquarterarcview/LineCapQuarterArcView.kt
@@ -91,4 +91,31 @@ class LineCapQuarterArcView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class Animator(var view : View, var animated : Boolean = false) {
+
+        fun animate() {
+            if (animated) {
+                try {
+                    Thread.sleep(delay)
+                    view.invalidate()
+                } catch(ex : Exception) {
+
+                }
+            }
+        }
+
+        fun start() {
+            if (!animated) {
+                animated = true
+                view.postInvalidate()
+            }
+        }
+
+        fun stop() {
+            if (animated) {
+                animated = false
+            }
+        }
+    }
 }
