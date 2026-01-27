034c0785f62c96e725afc590d0cf5e8c7f3ceee2
Anwesh43 <anweshthecool0@gmail.com>
2026-01-27 07:01:34 +0530

    linecapquarterarcview: adding LCQANode class

--- linecapquarterarcview/src/main/java/com/example/linecapquarterarcview/LineCapQuarterArcView.kt
+++ linecapquarterarcview/src/main/java/com/example/linecapquarterarcview/LineCapQuarterArcView.kt
@@ -118,4 +118,44 @@ class LineCapQuarterArcView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class LCQANode(var i : Int = 0, val state : State = State()) {
+
+        private var next : LCQANode? = null
+        private var prev : LCQANode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = LCQANode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawLCQANode(i, state.scale, paint)
+        }
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : LCQANode {
+            var curr : LCQANode? = prev
+            if (dir === 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
