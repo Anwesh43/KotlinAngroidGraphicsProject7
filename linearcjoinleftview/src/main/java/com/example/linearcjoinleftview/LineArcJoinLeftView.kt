37e85a5e9d253e8413d78c7fed2ead988429a7a4
Anwesh43 <anweshthecool0@gmail.com>
2026-01-31 10:57:59 +0530

    linearcjoinleftview: adding LAJLNode class

--- linearcjoinleftview/src/main/java/com/example/linearcjoinleftview/LineArcJoinLeftView.kt
+++ linearcjoinleftview/src/main/java/com/example/linearcjoinleftview/LineArcJoinLeftView.kt
@@ -124,4 +124,45 @@ class LineArcJoinLeftView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class LAJLNode(var i : Int = 0, val state : State = State()) {
+
+        private var next : LAJLNode? = null
+        private var prev : LAJLNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = LAJLNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawLAJLNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : LAJLNode {
+            var curr : LAJLNode? = prev
+            if (dir === 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
