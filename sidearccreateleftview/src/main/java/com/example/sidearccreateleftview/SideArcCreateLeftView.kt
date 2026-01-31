d8db21d1422cbb072eb4c1e47f78e660f2aaac5f
Anwesh43 <anweshthecool0@gmail.com>
2026-02-01 03:16:00 +0530

    sidearccreateleftview: adding Renderer class

--- sidearccreateleftview/src/main/java/com/example/sidearccreateleftview/SideArcCreateLeftView.kt
+++ sidearccreateleftview/src/main/java/com/example/sidearccreateleftview/SideArcCreateLeftView.kt
@@ -187,4 +187,27 @@ class SideArcCreateLeftView(ctx : Context) : View(ctx) {
             curr.startUpdating(cb)
         }
     }
+
+    data class Renderer(var view: SideArcCreateLeftView) {
+
+        private val animator : Animator = Animator(view)
+        private val paint : Paint = Paint(Paint.ANTI_ALIAS_FLAG)
+        private val sacl : SideArcCreateLeft = SideArcCreateLeft(0)
+
+        fun render(canvas : Canvas) {
+            canvas.drawColor(backColor)
+            sacl.draw(canvas, paint)
+            animator.animate {
+                sacl.update {
+                    animator.stop()
+                }
+            }
+        }
+
+        fun handleTap() {
+            sacl.startUpdating {
+                animator.start()
+            }
+        }
+    }
 }
