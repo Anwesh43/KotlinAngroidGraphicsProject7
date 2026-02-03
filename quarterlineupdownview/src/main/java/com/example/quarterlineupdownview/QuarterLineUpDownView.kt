363ab4aeef2f39183aa2d53cf410466897933ff7
Anwesh43 <anweshthecool0@gmail.com>
2026-02-04 02:12:00 +0530

    quarterlineupdownview: adding QuarterLineUpDownView class

--- quarterlineupdownview/src/main/java/com/example/quarterlineupdownview/QuarterLineUpDownView.kt
+++ quarterlineupdownview/src/main/java/com/example/quarterlineupdownview/QuarterLineUpDownView.kt
@@ -68,3 +68,19 @@ fun Canvas.drawQLUDNode(i : Int, scale : Float, paint : Paint) {
     paint.style = Paint.Style.STROKE
     drawQuarterLineUpDown(scale, w, h, paint)
 }
+
+class QuarterLineUpDownView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
