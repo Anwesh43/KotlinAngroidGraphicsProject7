416286882cc7b04ca8f592fa963a2d685bda5c0b
Anwesh43 <anweshthecool0@gmail.com>
2026-02-03 07:53:42 +0530

    sidequarterhalfarcview: adding SQHADNode class

--- sidequarterhalfarcview/src/main/java/com/example/sidequarterhalfarcview/SideQuarterHalfArcView.kt
+++ sidequarterhalfarcview/src/main/java/com/example/sidequarterhalfarcview/SideQuarterHalfArcView.kt
@@ -126,4 +126,45 @@ class SideQuarterHalfArcView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class SQHADNode(var i : Int = 0, val state : State = State()) {
+
+        private var next : SQHADNode? = null
+        private var prev : SQHADNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = SQHADNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawSQHANode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : SQHADNode {
+            var curr : SQHADNode? = prev
+            if (dir === 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
