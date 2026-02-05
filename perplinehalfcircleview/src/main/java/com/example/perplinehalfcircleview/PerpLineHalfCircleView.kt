dc8889622ac40e726c75c6976071c641d946794f
Anwesh43 <anweshthecool0@gmail.com>
2026-02-05 09:34:41 +0530

    perplinehalfcircleview: adding PLHCNode class

--- perplinehalfcircleview/src/main/java/com/example/perplinehalfcircleview/PerpLineHalfCircleView.kt
+++ perplinehalfcircleview/src/main/java/com/example/perplinehalfcircleview/PerpLineHalfCircleView.kt
@@ -125,4 +125,45 @@ class PerpLineHalfCircleView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class PLHCNode(var i : Int = 0, val state : State = State()) {
+
+        private var prev : PLHCNode? = null
+        private var next : PLHCNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = PLHCNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawLPHCNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : ()-> Unit) : PLHCNode {
+            var curr : PLHCNode? = prev
+            if (dir === 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
