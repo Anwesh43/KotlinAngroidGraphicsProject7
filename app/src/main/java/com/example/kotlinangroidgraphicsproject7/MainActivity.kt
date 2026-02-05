41655e951ffd9d37850c7373c4721822a60df964
Anwesh43 <anweshthecool0@gmail.com>
2026-02-05 09:40:24 +0530

    perplinehalfcircleview: adding view in activity

--- app/src/main/java/com/example/kotlinangroidgraphicsproject7/MainActivity.kt
+++ app/src/main/java/com/example/kotlinangroidgraphicsproject7/MainActivity.kt
@@ -12,12 +12,12 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.kotlinangroidgraphicsproject7.ui.theme.KotlinAngroidGraphicsProject7Theme
-import com.example.quarterlineupdownview.QuarterLineUpDownView
+import com.example.perplinehalfcircleview.PerpLineHalfCircleView
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         //LineRotPartialArcView.create(this)
-        QuarterLineUpDownView.create(this)
+        PerpLineHalfCircleView.create(this)
     }
 }
