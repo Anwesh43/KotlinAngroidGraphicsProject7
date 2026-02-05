32dc23a785581bb846fb2d444b180e4dcd7315df
Anwesh43 <anweshthecool0@gmail.com>
2026-02-05 09:38:28 +0530

    perplinehalfcircleview: adding static create method

--- perplinehalfcircleview/src/main/java/com/example/perplinehalfcircleview/PerpLineHalfCircleView.kt
+++ perplinehalfcircleview/src/main/java/com/example/perplinehalfcircleview/PerpLineHalfCircleView.kt
@@ -214,4 +214,12 @@ class PerpLineHalfCircleView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    companion object {
+        fun create(activity : Activity) : PerpLineHalfCircleView {
+            val view : PerpLineHalfCircleView = PerpLineHalfCircleView(activity)
+            activity.setContentView(view)
+            return view
+        }
+    }
 }
