f817a6f21c300558bc7a9c6a9def034381916a13
Anwesh43 <anweshthecool0@gmail.com>
2026-02-06 10:04:22 +0530

    quarterarcjoinhalfview: adding QuarterArcJoinHalfView class

--- quarterarcjoinhalfview/src/main/java/com/example/quarterarcjoinhalfview/QuarterArcJoinHalfView.kt
+++ quarterarcjoinhalfview/src/main/java/com/example/quarterarcjoinhalfview/QuarterArcJoinHalfView.kt
@@ -65,3 +65,19 @@ fun Canvas.drawQAJHNode(i : Int, scale : Float, paint : Paint) {
     paint.style = Paint.Style.STROKE
     drawQuarterArcJoinHalf(scale, w, h, paint)
 }
+
+class QuarterArcJoinHalfView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
