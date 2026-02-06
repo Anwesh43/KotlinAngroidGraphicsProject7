c88663a28e0b4cb3d840cd1387b8361c822e3b08
Anwesh43 <anweshthecool0@gmail.com>
2026-02-06 10:10:09 +0530

    quarterarcjoinhalfview: adding QAJHNode class

--- quarterarcjoinhalfview/src/main/java/com/example/quarterarcjoinhalfview/QuarterArcJoinHalfView.kt
+++ quarterarcjoinhalfview/src/main/java/com/example/quarterarcjoinhalfview/QuarterArcJoinHalfView.kt
@@ -128,4 +128,45 @@ class QuarterArcJoinHalfView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class QAJHNode(var i : Int = 0, val state : State = State()) {
+
+        private var prev : QAJHNode? = null
+        private var next : QAJHNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = QAJHNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawQAJHNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : QAJHNode {
+            var curr : QAJHNode? = prev
+            if (dir === 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
