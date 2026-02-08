9df7c4814e283d32feef81efdae8c5443dff692b
Anwesh43 <anweshthecool0@gmail.com>
2026-02-08 12:50:37 +0530

    closearclineupview: adding CloseArcLineUpView class

--- closearclineupview/src/main/java/com/example/closearclineupview/CloseArcLineUpView.kt
+++ closearclineupview/src/main/java/com/example/closearclineupview/CloseArcLineUpView.kt
@@ -59,3 +59,19 @@ fun Canvas.drawCALUNode(i : Int, scale : Float, paint : Paint) {
     paint.style = Paint.Style.STROKE
     drawCloseArcLineUp(scale, w, h, paint)
 }
+
+class CloseArcLineUpView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN ->{
+
+            }
+        }
+        return true
+    }
+}
