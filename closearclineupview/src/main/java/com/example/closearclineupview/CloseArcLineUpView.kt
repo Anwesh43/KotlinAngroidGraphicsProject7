c2bae9eaa9f28ca1115b7fb44264e78986379575
Anwesh43 <anweshthecool0@gmail.com>
2026-02-08 13:04:57 +0530

    closearclineupview: adding CALUNode class

--- closearclineupview/src/main/java/com/example/closearclineupview/CloseArcLineUpView.kt
+++ closearclineupview/src/main/java/com/example/closearclineupview/CloseArcLineUpView.kt
@@ -120,4 +120,42 @@ class CloseArcLineUpView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class CALUNode(var i : Int = 0, val state : State = State()) {
+
+        private var next : CALUNode? = null
+        private var prev : CALUNode? = null
+
+        init {
+
+        }
+
+        fun addNeighbor() {
+
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawCALUNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : CALUNode {
+            var curr : CALUNode ?= prev
+            if (dir === 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
