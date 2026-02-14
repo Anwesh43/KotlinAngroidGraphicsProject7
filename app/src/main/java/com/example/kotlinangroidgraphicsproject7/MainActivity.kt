21fc6035a77e05344ab5b5c590f994ec6e853b0f
Anwesh43 <anweshthecool0@gmail.com>
2026-02-14 11:11:43 +0530

    biarcmirrordownview: adding view in activity

--- app/src/main/java/com/example/kotlinangroidgraphicsproject7/MainActivity.kt
+++ app/src/main/java/com/example/kotlinangroidgraphicsproject7/MainActivity.kt
@@ -11,13 +11,13 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.example.biarcmirrordownview.BiArcMirrorDownView
 import com.example.kotlinangroidgraphicsproject7.ui.theme.KotlinAngroidGraphicsProject7Theme
-import com.example.lineenclosequarterarcview.LineEncloseQuarterArcView
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         //LineRotPartialArcView.create(this)
-        LineEncloseQuarterArcView.create(this)
+        BiArcMirrorDownView.create(this)
     }
 }
