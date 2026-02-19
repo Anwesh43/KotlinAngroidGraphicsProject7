f88ed0ea085a653df5d11027f820366292287f0d
Anwesh43 <anweshthecool0@gmail.com>
2026-02-19 13:28:31 +0530

    downlinerightarcview: adding Renderer class

--- downlinerightarcview/src/main/java/com/example/downlinerightarcview/DownLineRightArcView.kt
+++ downlinerightarcview/src/main/java/com/example/downlinerightarcview/DownLineRightArcView.kt
@@ -185,4 +185,27 @@ class DownLineRightArcView(ctx : Context) : View(ctx) {
             curr.startUpdating(cb)
         }
     }
+
+    data class Renderer(var view : DownLineRightArcView) {
+
+        private val animator : Animator = Animator(view)
+        private val paint : Paint = Paint(Paint.ANTI_ALIAS_FLAG)
+        private val dlra : DownLineRightArc = DownLineRightArc(0)
+
+        fun render(canvas : Canvas) {
+            canvas.drawColor(backColor)
+            dlra.draw(canvas, paint)
+            animator.animate {
+                dlra.update {
+                    animator.stop()
+                }
+            }
+        }
+
+        fun handleTap() {
+            dlra.startUpdating {
+                animator.start()
+            }
+        }
+    }
 }
