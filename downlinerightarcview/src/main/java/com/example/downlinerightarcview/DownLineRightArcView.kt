6e041eb795f76a27d383895e8f71536114fdeee5
Anwesh43 <anweshthecool0@gmail.com>
2026-02-19 13:03:36 +0530

    downlinerightarcview: adding DownLineRightArcView class

--- downlinerightarcview/src/main/java/com/example/downlinerightarcview/DownLineRightArcView.kt
+++ downlinerightarcview/src/main/java/com/example/downlinerightarcview/DownLineRightArcView.kt
@@ -58,3 +58,19 @@ fun Canvas.drawDLRANode(i : Int, scale : Float, paint : Paint) {
     paint.style = Paint.Style.STROKE
     drawDownLneRightArc(scale, w, h, paint)
 }
+
+class DownLineRightArcView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
