087842b801d2226a2322ca01ffb5086bcffd5c01
Anwesh43 <anweshthecool0@gmail.com>
2026-02-19 13:30:22 +0530

    downlinerightarcview: adding view in activity

--- app/src/main/java/com/example/kotlinangroidgraphicsproject7/MainActivity.kt
+++ app/src/main/java/com/example/kotlinangroidgraphicsproject7/MainActivity.kt
@@ -11,13 +11,13 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import com.example.halfarcjoindownview.HalfArcJoinDownView
+import com.example.downlinerightarcview.DownLineRightArcView
 import com.example.kotlinangroidgraphicsproject7.ui.theme.KotlinAngroidGraphicsProject7Theme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         //LineRotPartialArcView.create(this)
-        HalfArcJoinDownView.create(this)
+        DownLineRightArcView.create(this)
     }
 }
