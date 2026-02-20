2d19cffeb481876041d54d8d5411fda970cdc441
Anwesh43 <anweshthecool0@gmail.com>
2026-02-20 09:45:56 +0530

    quarterarclinehalfview: adding QuarterArcLineHalf class

--- quarterarclinehalfview/src/main/java/com/example/quarterarclinehalfview/QuarterArcLineHalfView.kt
+++ quarterarclinehalfview/src/main/java/com/example/quarterarclinehalfview/QuarterArcLineHalfView.kt
@@ -164,4 +164,27 @@ class QuarterArcLineHalfView(ctx : Context) : View(ctx) {
             return this
         }
     }
+
+    data class QuarterArcLineHalf(var i : Int) {
+
+        private var curr : QALHNode = QALHNode(0)
+        private var dir : Int = 1
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            curr.draw(canvas, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            curr.update {
+                curr = curr.getNext(dir) {
+                    dir *= -1
+                }
+                cb(it)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            curr.startUpdating(cb)
+        }
+    }
 }
