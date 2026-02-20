792093b43fb02be694be0351068c366501496926
Anwesh43 <anweshthecool0@gmail.com>
2026-02-20 09:44:16 +0530

    quarterarclinehalfview: adding QALHNode class

--- quarterarclinehalfview/src/main/java/com/example/quarterarclinehalfview/QuarterArcLineHalfView.kt
+++ quarterarclinehalfview/src/main/java/com/example/quarterarclinehalfview/QuarterArcLineHalfView.kt
@@ -123,4 +123,45 @@ class QuarterArcLineHalfView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class QALHNode(var i : Int = 0, val state : State = State()) {
+
+        private var next : QALHNode? = null
+        private var prev : QALHNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = QALHNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawQALHNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : QALHNode {
+            var curr : QALHNode? = prev
+            if (dir === 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
