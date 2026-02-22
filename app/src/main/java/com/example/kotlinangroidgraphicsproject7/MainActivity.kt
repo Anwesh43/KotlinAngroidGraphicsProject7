b25a14ba088d32cc324fd2dbf00675a880324ff9
Anwesh43 <anweshthecool0@gmail.com>
2026-02-22 11:54:11 +0530

    quarterarchalfdownview: adding view in activity

--- app/src/main/java/com/example/kotlinangroidgraphicsproject7/MainActivity.kt
+++ app/src/main/java/com/example/kotlinangroidgraphicsproject7/MainActivity.kt
@@ -12,12 +12,12 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.kotlinangroidgraphicsproject7.ui.theme.KotlinAngroidGraphicsProject7Theme
-import com.example.linepeakrotrightview.LinePeakRotRightView
+import com.example.quarterarchalfdownview.QuarterArcHalfDownView
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         //LineRotPartialArcView.create(this)
-        LinePeakRotRightView.create(this)
+        QuarterArcHalfDownView.create(this)
     }
 }
