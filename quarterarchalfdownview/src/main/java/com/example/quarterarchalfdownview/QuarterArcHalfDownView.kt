79d4599892814b923db10d1eb4a4b1de518e43c2
Anwesh43 <anweshthecool0@gmail.com>
2026-02-22 11:47:48 +0530

    quarterarchalfdownview: adding QAHDNode class

--- quarterarchalfdownview/src/main/java/com/example/quarterarchalfdownview/QuarterArcHalfDownView.kt
+++ quarterarchalfdownview/src/main/java/com/example/quarterarchalfdownview/QuarterArcHalfDownView.kt
@@ -122,4 +122,45 @@ class QuarterArcHalfDownView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class QAHDNode(var i : Int = 0, val state : State = State()) {
+
+        private var next : QAHDNode? = null
+        private var prev : QAHDNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = QAHDNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawQAHDNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : QAHDNode {
+            var curr : QAHDNode? = prev
+            if (dir === 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
