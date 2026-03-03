2398b3c7cd24eb5e915bda59c5ba028b33a040f3
Anwesh43 <anweshthecool0@gmail.com>
2026-03-03 06:47:33 +0530

    linepointjoinbentview: adding LPJBNode class

--- linepointjoinbentview/src/main/java/com/example/linepointjoinbentview/LinePointJoinBentView.kt
+++ linepointjoinbentview/src/main/java/com/example/linepointjoinbentview/LinePointJoinBentView.kt
@@ -123,4 +123,45 @@ class LinePointJoinBentView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class LPJBNode(var i : Int = 0, val state : State = State()) {
+
+        private var next : LPJBNode? = null
+        private var prev : LPJBNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = LPJBNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawLPJBNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : LPJBNode {
+            var curr : LPJBNode? = prev
+            if (dir === 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
