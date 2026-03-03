7c7f082ebfe64a6c3bb195e774b196af16832338
Anwesh43 <anweshthecool0@gmail.com>
2026-03-03 06:52:10 +0530

    linepointjoinbentview: adding view in activity

--- app/src/main/java/com/example/kotlinangroidgraphicsproject7/MainActivity.kt
+++ app/src/main/java/com/example/kotlinangroidgraphicsproject7/MainActivity.kt
@@ -12,12 +12,12 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.kotlinangroidgraphicsproject7.ui.theme.KotlinAngroidGraphicsProject7Theme
-import com.example.quarterarchalfdownview.QuarterArcHalfDownView
+import com.example.linepointjoinbentview.LinePointJoinBentView
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         //LineRotPartialArcView.create(this)
-        QuarterArcHalfDownView.create(this)
+        LinePointJoinBentView.create(this)
     }
 }
