69bd5b62525e21d4762b43a7a3fda40607f8b18e
Anwesh43 <anweshthecool0@gmail.com>
2026-03-04 06:49:25 +0530

    linerothalfarcview: adding LineRotHalfArcView class

--- linerothalfarcview/src/main/java/com/example/linerothalfarcview/LineRotHalfArcView.kt
+++ linerothalfarcview/src/main/java/com/example/linerothalfarcview/LineRotHalfArcView.kt
@@ -67,3 +67,19 @@ fun Canvas.drawLRHANode(i : Int, scale : Float, paint : Paint) {
     paint.style = Paint.Style.STROKE
     drawLineRotHalfArc(scale, w, h, paint)
 }
+
+class LineRotHalfArcView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
