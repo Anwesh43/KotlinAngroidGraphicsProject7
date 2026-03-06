52ac81f7f8a67fbeb4bd48d288dba0889539966e
Anwesh43 <anweshthecool0@gmail.com>
2026-03-06 09:24:10 +0530

    linearclinerotview: adding Canvas extension methods

--- linearclinerotview/src/main/java/com/example/linearclinerotview/LineArcLineRotView.kt
+++ linearclinerotview/src/main/java/com/example/linearclinerotview/LineArcLineRotView.kt
@@ -23,9 +23,40 @@ val backColor : Int = "#BDBDBD".toColorInt()
 val strokeFactor : Float = 90f
 val sizeFactor : Float = 5.9f
 val scGap : Float = 0.04f / parts
+val deg : Float = 270f
 
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
 
+fun Canvas.drawXY(x : Float, y : Float, cb : () -> Unit) {
+    save()
+    translate(x, y)
+    cb()
+    restore()
+}
 
+fun Canvas.drawLineArcLineRot(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val dsc : (Int) -> Float = {
+        scale.divideScale(it, parts)
+    }
+    drawXY(w / 2 + (w / 2) * dsc(4), h / 2) {
+        drawLine(0f, 0f, 0f, -size * dsc(0), paint)
+        drawXY(size, 0f) {
+            rotate(deg * dsc(3))
+            drawLine(0f, 0f, 0f, -size * dsc(2), paint)
+        }
+        drawArc(RectF(-size, -size, size, size), -rot, rot * dsc(1), false, paint)
+    }
+}
+
+fun Canvas.drawLALRNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = colors[i].toColorInt()
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    paint.style = Paint.Style.STROKE
+    drawLineArcLineRot(scale, w, h, paint)
+}
