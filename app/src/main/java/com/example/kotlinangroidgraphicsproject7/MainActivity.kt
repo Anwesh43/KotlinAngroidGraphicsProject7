b61472394b991ffcea5edbabafb4e71fd0160c2e
Anwesh43 <anweshthecool0@gmail.com>
2026-03-07 14:03:27 +0530

    linearclinerotview: adding view in activity

--- app/src/main/java/com/example/kotlinangroidgraphicsproject7/MainActivity.kt
+++ app/src/main/java/com/example/kotlinangroidgraphicsproject7/MainActivity.kt
@@ -12,12 +12,12 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.kotlinangroidgraphicsproject7.ui.theme.KotlinAngroidGraphicsProject7Theme
-import com.example.linerothalfarcview.LineRotHalfArcView
+import com.example.linearclinerotview.LineArcLineRotView
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         //LineRotPartialArcView.create(this)
-        LineRotHalfArcView.create(this)
+        LineArcLineRotView.create(this)
     }
 }
