3b288f08c558500d3dc1a988002b4c065dbee649
Anwesh43 <anweshthecool0@gmail.com>
2026-03-08 08:04:02 +0530

    concarcsemiseparaeview: adding ConcArcSemiSeparateView class

--- concarcsemiseparateview/src/main/java/com/example/concarcsemiseparateview/ConcArcSemiSeparateView.kt
+++ concarcsemiseparateview/src/main/java/com/example/concarcsemiseparateview/ConcArcSemiSeparateView.kt
@@ -61,3 +61,19 @@ fun Canvas.drawCASSNode(i : Int, scale : Float, paint : Paint) {
     paint.style = Paint.Style.STROKE
     drawConcArcSemiSeparate(scale, w, h, paint)
 }
+
+class ConcArcSemiSeparateView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
