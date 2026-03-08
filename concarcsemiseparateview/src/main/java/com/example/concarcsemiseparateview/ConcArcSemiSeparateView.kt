26d9208602ee352d09058f3287b1e2540cdb943d
Anwesh43 <anweshthecool0@gmail.com>
2026-03-08 08:26:56 +0530

    concarcsemiseparaeview: adding ConcArcSemiSeparate class

--- concarcsemiseparateview/src/main/java/com/example/concarcsemiseparateview/ConcArcSemiSeparateView.kt
+++ concarcsemiseparateview/src/main/java/com/example/concarcsemiseparateview/ConcArcSemiSeparateView.kt
@@ -165,4 +165,27 @@ class ConcArcSemiSeparateView(ctx : Context) : View(ctx) {
             return this
         }
     }
+
+    data class ConcArcSemiSeparate(var i : Int) {
+
+        private val curr : CASSNode = CASSNode(0)
+        private var dir : Int = 1
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            curr.draw(canvas, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            curr.update {
+                curr = curr.getNext(dir) {
+                    dir *= -1
+                }
+                cb(it)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            curr.startUpdating(cb)
+        }
+    }
 }
