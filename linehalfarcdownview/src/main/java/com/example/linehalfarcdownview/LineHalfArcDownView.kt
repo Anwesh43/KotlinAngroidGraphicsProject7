2566905661804b5d96abfcdd7106b4b35d343440
Anwesh43 <anweshthecool0@gmail.com>
2026-03-10 09:20:52 +0530

    linehalfarcdonview: adding LineHalfArcDownView class

--- linehalfarcdownview/src/main/java/com/example/linehalfarcdownview/LineHalfArcDownView.kt
+++ linehalfarcdownview/src/main/java/com/example/linehalfarcdownview/LineHalfArcDownView.kt
@@ -61,3 +61,19 @@ fun Canvas.drawLHADNode(i : Int, scale : Float, paint : Paint) {
     paint.strokeWidth = Math.min(w, h) / strokeFactor
     drawLineHalfArcDown(scale, w, h, paint)
 }
+
+class LineHalfArcDownView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
