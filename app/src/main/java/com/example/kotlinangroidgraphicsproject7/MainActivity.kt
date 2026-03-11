568969ba919af87b699e9b7dc54bb8cdb8f2d644
Anwesh43 <anweshthecool0@gmail.com>
2026-03-11 19:12:22 +0530

    linehalfarcdonview: adding view in activity

--- app/src/main/java/com/example/kotlinangroidgraphicsproject7/MainActivity.kt
+++ app/src/main/java/com/example/kotlinangroidgraphicsproject7/MainActivity.kt
@@ -11,13 +11,13 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import com.example.concarcsemiseparateview.ConcArcSemiSeparateView
 import com.example.kotlinangroidgraphicsproject7.ui.theme.KotlinAngroidGraphicsProject7Theme
+import com.example.linehalfarcdownview.LineHalfArcDownView
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         //LineRotPartialArcView.create(this)
-        ConcArcSemiSeparateView.create(this)
+        LineHalfArcDownView.create(this)
     }
 }
