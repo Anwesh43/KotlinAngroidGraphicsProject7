ad711c9cf2f8fe2909d586aa53b4568e75d107b4
Anwesh43 <anweshthecool0@gmail.com>
2026-03-11 13:06:29 +0530

    linehalfarcdonview: adding Renderer class

--- linehalfarcdownview/src/main/java/com/example/linehalfarcdownview/LineHalfArcDownView.kt
+++ linehalfarcdownview/src/main/java/com/example/linehalfarcdownview/LineHalfArcDownView.kt
@@ -188,4 +188,27 @@ class LineHalfArcDownView(ctx : Context) : View(ctx) {
             curr.startUpdating(cb)
         }
     }
+
+    data class Renderer(var view : LineHalfArcDownView) {
+
+        private val paint : Paint = Paint(Paint.ANTI_ALIAS_FLAG)
+        private val animator : Animator = Animator(view)
+        private val lhad : LineHalfArcDown = LineHalfArcDown(0)
+
+        fun render(canvas : Canvas) {
+            canvas.drawColor(backColor)
+            lhad.draw(canvas, paint)
+            animator.animate {
+                lhad.update {
+                    animator.stop()
+                }
+            }
+        }
+
+        fun handleTap() {
+            lhad.startUpdating {
+                animator.start()
+            }
+        }
+    }
 }
