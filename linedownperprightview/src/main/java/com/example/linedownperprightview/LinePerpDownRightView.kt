502d541c35f20ef25312bd4c9c66a92d6a03ca9d
Anwesh43 <anweshthecool0@gmail.com>
2026-03-12 07:54:07 +0530

    lineperpdownrightview: adding LinePerpDownRight class

--- linedownperprightview/src/main/java/com/example/linedownperprightview/LinePerpDownRightView.kt
+++ linedownperprightview/src/main/java/com/example/linedownperprightview/LinePerpDownRightView.kt
@@ -162,4 +162,27 @@ class LinePerpDownRightView(ctx : Context) : View(ctx) {
             return this
         }
     }
+
+    data class LinePerpDownRight(var i : Int = 0) {
+
+        private var curr : LPDRNode = LPDRNode(0)
+        private var dir : Int = 1
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            curr.draw(canvas, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            curr.update {
+                curr = curr.getNext(dir) {
+                    dir *= -1
+                }
+                cb(it)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            curr.startUpdating(cb)
+        }
+    }
 }
