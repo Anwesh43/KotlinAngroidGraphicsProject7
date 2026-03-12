3463b0e772b7cfdbe61c178fe00aa0440384bbdd
Anwesh43 <anweshthecool0@gmail.com>
2026-03-12 08:02:27 +0530

    lineperpdownrightview: adding Renderer in View

--- linedownperprightview/src/main/java/com/example/linedownperprightview/LinePerpDownRightView.kt
+++ linedownperprightview/src/main/java/com/example/linedownperprightview/LinePerpDownRightView.kt
@@ -61,14 +61,16 @@ fun Canvas.drawLPDRNode(i : Int, scale : Float, paint : Paint) {
 
 class LinePerpDownRightView(ctx : Context) : View(ctx) {
 
-    override fun onDraw(canvas : Canvas) {
+    private val renderer : Renderer = Renderer(this)
 
+    override fun onDraw(canvas : Canvas) {
+        renderer.render(canvas)
     }
 
     override fun onTouchEvent(event : MotionEvent) : Boolean {
         when (event.action) {
             MotionEvent.ACTION_DOWN -> {
-
+                renderer.handleTap()
             }
         }
         return true
@@ -190,8 +192,9 @@ class LinePerpDownRightView(ctx : Context) : View(ctx) {
 
         private val animator : Animator = Animator(view)
         private val lpdr : LinePerpDownRight = LinePerpDownRight(0)
+        private val paint : Paint = Paint(Paint.ANTI_ALIAS_FLAG)
 
-        fun render(canvas : Canvas, paint : Paint) {
+        fun render(canvas : Canvas) {
             canvas.drawColor(backColor)
             lpdr.draw(canvas, paint)
             animator.animate {
