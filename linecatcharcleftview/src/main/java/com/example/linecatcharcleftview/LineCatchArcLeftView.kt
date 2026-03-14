d45a030d326b89644cece3edaefd923de2e5cb85
Anwesh43 <anweshthecool0@gmail.com>
2026-03-14 11:17:26 +0530

    linecatcharcleftview: adding Canvas extension methods

--- linecatcharcleftview/src/main/java/com/example/linecatcharcleftview/LineCatchArcLeftView.kt
+++ linecatcharcleftview/src/main/java/com/example/linecatcharcleftview/LineCatchArcLeftView.kt
@@ -27,3 +27,34 @@ val delay : Long = 20
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+
+fun Canvas.drawXY(x : Float, y : Float, cb : () -> Unit) {
+    save()
+    translate(x, y)
+    cb()
+    restore()
+}
+
+fun Canvas.drawLineCatchArcLeft(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = width.toFloat()
+    val dsc : (Int) -> Float = {
+        scale.divideScale(it, parts)
+    }
+    drawXY(w / 2 - (w / 2)  * dsc(3), h / 2) {
+        drawXY(-w * 0.25f * (1 - dsc(1)), 0f) {
+            drawLine(0f, 0f, -size * dsc(0), 0f, paint)
+        }
+        drawXY(w * 0.25f * (1 - dsc(1)), 0f) {
+            drawArc(RectF(0f, -size / 2, size, size / 2), 180f, 180f * dsc(2), false, paint)
+        }
+    }
+}
+
+fun Canvas.drawLCALNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = colors[i].toColorInt()
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    paint.style = Paint.Style.STROKE
+}
