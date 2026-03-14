8d4602f4a83be240ca7d0ddde1b802f9651048e8
Anwesh43 <anweshthecool0@gmail.com>
2026-03-14 11:35:12 +0530

    linecatcharcleftview: adding Renderer class

--- linecatcharcleftview/src/main/java/com/example/linecatcharcleftview/LineCatchArcLeftView.kt
+++ linecatcharcleftview/src/main/java/com/example/linecatcharcleftview/LineCatchArcLeftView.kt
@@ -185,4 +185,27 @@ class LineCatchArcLeftView(ctx : Context) : View(ctx) {
             curr.startUpdating(cb)
         }
     }
+
+    data class Renderer(var view : LineCatchArcLeftView) {
+
+        private val animator : Animator = Animator(view)
+        private val paint : Paint = Paint(Paint.ANTI_ALIAS_FLAG)
+        private val lcal : LineCatchArcLeft = LineCatchArcLeft(0)
+
+        fun render(canvas : Canvas) {
+            canvas.drawColor(backColor)
+            lcal.draw(canvas, paint)
+            animator.animate {
+                lcal.update {
+                    animator.stop()
+                }
+            }
+        }
+
+        fun handleTap() {
+            lcal.startUpdating {
+                animator.start()
+            }
+        }
+    }
 }
