6442be720a85bee425d45725eb6481febc91b994
Anwesh43 <anweshthecool0@gmail.com>
2026-03-14 11:32:05 +0530

    linecatcharcleftview: adding LCALNode class

--- linecatcharcleftview/src/main/java/com/example/linecatcharcleftview/LineCatchArcLeftView.kt
+++ linecatcharcleftview/src/main/java/com/example/linecatcharcleftview/LineCatchArcLeftView.kt
@@ -121,4 +121,45 @@ class LineCatchArcLeftView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class LCALNode(var i : Int = 0, val state : State = State()) {
+
+        private var next : LCALNode? = null
+        private var prev : LCALNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = LCALNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawLCALNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : LCALNode {
+            var curr : LCALNode? = prev
+            if (dir === 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
