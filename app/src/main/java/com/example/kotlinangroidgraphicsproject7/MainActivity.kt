792c9985e7ad476df5f6eba6a245095c582f94e0
Anwesh43 <anweshthecool0@gmail.com>
2026-03-15 10:50:57 +0530

    rightlinehypoleftview: adding view in activity

--- app/src/main/java/com/example/kotlinangroidgraphicsproject7/MainActivity.kt
+++ app/src/main/java/com/example/kotlinangroidgraphicsproject7/MainActivity.kt
@@ -12,12 +12,12 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.kotlinangroidgraphicsproject7.ui.theme.KotlinAngroidGraphicsProject7Theme
-import com.example.linecatcharcleftview.LineCatchArcLeftView
+import com.example.rightlinehypoleftview.RightLineHypoLeftView
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         //LineRotPartialArcView.create(this)
-        LineCatchArcLeftView.create(this)
+        RightLineHypoLeftView.create(this)
     }
 }
