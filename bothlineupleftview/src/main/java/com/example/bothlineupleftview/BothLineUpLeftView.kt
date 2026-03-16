cc571a90ebb4411913e99dc1b064a74a654268e7
Anwesh43 <anweshthecool0@gmail.com>
2026-03-16 08:36:31 +0530

    bothlineupleftview: adding BothLineUpLeftView class

--- bothlineupleftview/src/main/java/com/example/bothlineupleftview/BothLineUpLeftView.kt
+++ bothlineupleftview/src/main/java/com/example/bothlineupleftview/BothLineUpLeftView.kt
@@ -59,3 +59,19 @@ fun Canvas.drawBLULNode(i : Int, scale : Float, paint : Paint) {
     paint.style = Paint.Style.STROKE
     drawBothLineUpLeft(scale, w, h, paint)
 }
+
+class BothLineUpLeftView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
