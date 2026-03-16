56f7cb7a0dd9c3a0b428d0734b3eef0cad098a8f
Anwesh43 <anweshthecool0@gmail.com>
2026-03-16 08:41:13 +0530

    bothlineupleftview: adding BLULNode class

--- bothlineupleftview/src/main/java/com/example/bothlineupleftview/BothLineUpLeftView.kt
+++ bothlineupleftview/src/main/java/com/example/bothlineupleftview/BothLineUpLeftView.kt
@@ -122,4 +122,45 @@ class BothLineUpLeftView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class BLULNode(var i : Int = 0, val state : State = State()) {
+
+        private var next : BLULNode? = null
+        private var prev : BLULNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = BLULNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawBLULNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : BLULNode {
+            var curr : BLULNode? = prev
+            if (dir === 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
