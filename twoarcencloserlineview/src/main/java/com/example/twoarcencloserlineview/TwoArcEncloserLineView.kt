c678dbc724ecd256c4c11a5a005f96e94bc4666e
Anwesh43 <anweshthecool0@gmail.com>
2026-03-18 17:16:52 +0530

    twoarcencloserlineview: adding TAELNode class

--- twoarcencloserlineview/src/main/java/com/example/twoarcencloserlineview/TwoArcEncloserLineView.kt
+++ twoarcencloserlineview/src/main/java/com/example/twoarcencloserlineview/TwoArcEncloserLineView.kt
@@ -125,4 +125,45 @@ class TwoArcEncloserLineView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class TAELNode(var i : Int = 0, val state : State = State()) {
+
+        private var next : TAELNode? = null
+        private var prev : TAELNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = TAELNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawTAELNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : TAELNode {
+            var curr : TAELNode? = prev
+            if (dir === 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
