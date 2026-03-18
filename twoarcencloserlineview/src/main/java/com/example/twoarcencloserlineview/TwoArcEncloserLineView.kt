9da53bf1a6fb0648ba7101c0b78874d5c943bfe3
Anwesh43 <anweshthecool0@gmail.com>
2026-03-18 16:58:11 +0530

    twoarcencloserlineview: adding Canvas extension methods

--- twoarcencloserlineview/src/main/java/com/example/twoarcencloserlineview/TwoArcEncloserLineView.kt
+++ twoarcencloserlineview/src/main/java/com/example/twoarcencloserlineview/TwoArcEncloserLineView.kt
@@ -22,8 +22,43 @@ val strokeFactor : Float = 90f
 val sizeFactor : Float = 5.9f
 val delay : Long = 20
 val backColor : Int = "#BDBDBD".toColorInt()
-val rot : Float = 180f
+val rot : Float = -180f
 
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+
+fun Canvas.drawXY(x : Float, y : Float, cb : () -> Unit) {
+    save()
+    translate(x, y)
+    cb()
+    restore()
+}
+
+fun Canvas.drawTwoArcEncloseLine(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val dsc : (Int) -> Float = {
+        scale.divideScale(it, parts)
+    }
+    drawXY(w / 2 + (w / 2) * dsc(4), h / 2) {
+        drawXY(0f, 0f) {
+            rotate(rot * dsc(3))
+            drawLine(-size * (1 - dsc(0)), 0f, -size, 0f, paint)
+        }
+        for (j in 0..1) {
+            drawXY(size * 0.5f * j, 0f) {
+                drawArc(RectF(0f, -size / 4, size / 2, size / 4), 180f, 180f * dsc(j + 1), false, paint)
+            }
+        }
+    }
+}
+
+fun Canvas.drawTAELNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = colors[i].toColorInt()
+    paint.style = Paint.Style.STROKE
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    paint.strokeCap = Paint.Cap.ROUND
+    drawTwoArcEncloseLine(scale, w, h, paint)
+}
