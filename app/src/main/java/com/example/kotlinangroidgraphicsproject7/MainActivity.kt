4777ee25bd920ed7a717a74fade6de0a01b38bba
Anwesh43 <anweshthecool0@gmail.com>
2026-03-19 07:12:43 +0530

    linerotenclosearcview: adding view in activity

--- app/src/main/java/com/example/kotlinangroidgraphicsproject7/MainActivity.kt
+++ app/src/main/java/com/example/kotlinangroidgraphicsproject7/MainActivity.kt
@@ -12,12 +12,12 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.kotlinangroidgraphicsproject7.ui.theme.KotlinAngroidGraphicsProject7Theme
-import com.example.twoarcencloserlineview.TwoArcEncloserLineView
+import com.example.linerotenclosearcview.LineRotEncloseArcView
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         //LineRotPartialArcView.create(this)
-        TwoArcEncloserLineView.create(this)
+        LineRotEncloseArcView.create(this)
     }
 }
