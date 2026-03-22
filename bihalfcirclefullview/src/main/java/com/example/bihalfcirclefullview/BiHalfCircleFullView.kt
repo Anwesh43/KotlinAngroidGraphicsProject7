fcdeda4bd9705eb81f8a66a4bf0481b585f70a23
Anwesh43 <anweshthecool0@gmail.com>
2026-03-22 12:56:53 +0530

    bihalfcirlefullview: adding Canvas extension methods

--- bihalfcirclefullview/src/main/java/com/example/bihalfcirclefullview/BiHalfCircleFullView.kt
+++ bihalfcirclefullview/src/main/java/com/example/bihalfcirclefullview/BiHalfCircleFullView.kt
@@ -5,7 +5,7 @@ import android.view.MotionEvent
 import android.app.Activity
 import android.content.Context
 import android.graphics.Paint
-import android.graphics.Rect
+import android.graphics.RectF
 import android.graphics.Canvas
 import androidx.core.graphics.toColorInt
 
@@ -27,3 +27,36 @@ val strokeFactor : Float = 90f
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+
+fun Canvas.drawXY(x : Float, y : Float, cb : () -> Unit) {
+    save()
+    translate(x, y)
+    cb()
+    restore()
+}
+
+fun Canvas.drawBiHalfCircleFull(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val dsc : (Int) -> Float = {
+        scale.divideScale(it, parts)
+    }
+    drawXY(w / 2, h / 2 - h * dsc(3)) {
+        for (j in 0..1) {
+            drawXY(0f, -w * 0.25f * (1 - dsc(1)) * (1f - 2 * j)) {
+                rotate(rot * dsc(2) * j)
+                scale(1f, 1f - 2 * j)
+                drawArc(RectF(-size / 2, -size, size / 2 , 0f), -90f, 180f * dsc(0), false, paint)
+            }
+        }
+    }
+}
+
+fun Canvas.drawBHCFNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = colors[i].toColorInt()
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    paint.style = Paint.Style.STROKE
+    drawBiHalfCircleFull(scale, w, h, paint)
+}
