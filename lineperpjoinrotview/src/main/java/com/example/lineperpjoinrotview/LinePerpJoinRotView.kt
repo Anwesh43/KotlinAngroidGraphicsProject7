c89a0f5240783aaccf4cf2cde1a60c6b9616e920
Anwesh43 <anweshthecool0@gmail.com>
2026-03-24 07:03:11 +0530

    lineperpjoinrotview: adding LPJRNode class

--- lineperpjoinrotview/src/main/java/com/example/lineperpjoinrotview/LinePerpJoinRotView.kt
+++ lineperpjoinrotview/src/main/java/com/example/lineperpjoinrotview/LinePerpJoinRotView.kt
@@ -123,4 +123,45 @@ class LinePerpJoinRotView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class LPJRNode(var i : Int = 0, val state : State = State()) {
+
+        private var next : LPJRNode? = null
+        private var prev : LPJRNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = LPJRNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawLPJRNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : LPJRNode {
+            var curr : LPJRNode? = prev
+            if (dir === 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
