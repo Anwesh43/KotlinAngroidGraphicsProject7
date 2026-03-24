6ee0048244b60e51621b47c02afce9537201915c
Anwesh43 <anweshthecool0@gmail.com>
2026-03-24 06:46:20 +0530

    lineperpjoinrotview: adding LinePerpJoinRotView class

--- lineperpjoinrotview/src/main/java/com/example/lineperpjoinrotview/LinePerpJoinRotView.kt
+++ lineperpjoinrotview/src/main/java/com/example/lineperpjoinrotview/LinePerpJoinRotView.kt
@@ -60,3 +60,19 @@ fun Canvas.drawLPJRNode(i : Int, scale : Float, paint : Paint) {
     paint.style = Paint.Style.STROKE
     drawLinePerpJoinRot(scale, w, h, paint)
 }
+
+class LinePerpJoinRotView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
