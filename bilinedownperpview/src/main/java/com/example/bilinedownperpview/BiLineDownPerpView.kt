efdf915522b63b244016ff12edcb516806d4b1cf
Anwesh43 <anweshthecool0@gmail.com>
2026-03-27 07:50:15 +0530

    bilinedownperpview: adding Canvas extension methods

--- bilinedownperpview/src/main/java/com/example/bilinedownperpview/BiLineDownPerpView.kt
+++ bilinedownperpview/src/main/java/com/example/bilinedownperpview/BiLineDownPerpView.kt
@@ -20,10 +20,44 @@ val parts : Int = 6
 val scGap : Float = 0.05f / parts
 val rot : Float = 90f
 val sizeFactor : Float = 5.9f
-val strokeFactor : Float = 90f
+val strokeFactor : Float = 180f
 val backColor : Int = "#BDBDBD".toColorInt()
 val delay : Long = 20
 
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+
+fun Canvas.drawXY(x : Float, y : Float, cb : () -> Unit) {
+    save()
+    translate(x, y)
+    cb()
+    restore()
+}
+
+fun Canvas.drawBiLineDownPerp(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val dsc : (Int) -> Float = {
+        scale.divideScale(it, parts)
+    }
+    drawXY(w / 2, h / 2) {
+        for (j in 0..1) {
+            drawXY(0f, 0f) {
+                scale(1f - 2 * j, 1f)
+                drawXY(-w / 2, h * 0.5f * (-(1 - dsc(3 * j)) + dsc(3 * j + 2))) {
+                    rotate(rot * dsc(3 * j + 1))
+                    drawLine(0f, 0f, 0f, -size, paint)
+                }
+            }
+        }
+    }
+}
+
+fun Canvas.drawBLDPNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = colors[i].toColorInt()
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    drawBiLineDownPerp(scale, w, h, paint)
+}
