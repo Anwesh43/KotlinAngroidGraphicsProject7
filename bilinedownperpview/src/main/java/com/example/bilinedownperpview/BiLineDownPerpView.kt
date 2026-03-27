1f022296db7e48c3f1156b7a33a861ac18e7afff
Anwesh43 <anweshthecool0@gmail.com>
2026-03-27 08:03:00 +0530

    bilinedownperpview: adding BLDPNode class

--- bilinedownperpview/src/main/java/com/example/bilinedownperpview/BiLineDownPerpView.kt
+++ bilinedownperpview/src/main/java/com/example/bilinedownperpview/BiLineDownPerpView.kt
@@ -124,4 +124,45 @@ class BiLineDownPerpView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class BLDPNode(var i : Int = 0, val state : State = State()) {
+
+        private var next : BLDPNode? = null
+        private var prev : BLDPNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = BLDPNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawBLDPNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : BLDPNode {
+            var curr : BLDPNode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
