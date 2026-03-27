9355bb8b0341aecd2f3c3b4ed908a790f26d4e39
Anwesh43 <anweshthecool0@gmail.com>
2026-03-27 08:41:28 +0530

    bilinedownperpview: adding view in activity

--- app/src/main/java/com/example/kotlinangroidgraphicsproject7/MainActivity.kt
+++ app/src/main/java/com/example/kotlinangroidgraphicsproject7/MainActivity.kt
@@ -11,13 +11,13 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import com.example.bilineperpdownview.BiLinePerpDownView
+import com.example.bilinedownperpview.BiLineDownPerpView
 import com.example.kotlinangroidgraphicsproject7.ui.theme.KotlinAngroidGraphicsProject7Theme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         //LineRotPartialArcView.create(this)
-        BiLinePerpDownView.create(this)
+        BiLineDownPerpView.create(this)
     }
 }
