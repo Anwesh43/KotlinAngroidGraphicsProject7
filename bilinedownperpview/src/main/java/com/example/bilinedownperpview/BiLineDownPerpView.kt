134f86e14499fe73f9b0af63ec840a9b1ca1ec81
Anwesh43 <anweshthecool0@gmail.com>
2026-03-27 07:51:25 +0530

    bilinedownperpview: adding BiLineDownPerpView class

--- bilinedownperpview/src/main/java/com/example/bilinedownperpview/BiLineDownPerpView.kt
+++ bilinedownperpview/src/main/java/com/example/bilinedownperpview/BiLineDownPerpView.kt
@@ -61,3 +61,19 @@ fun Canvas.drawBLDPNode(i : Int, scale : Float, paint : Paint) {
     paint.strokeWidth = Math.min(w, h) / strokeFactor
     drawBiLineDownPerp(scale, w, h, paint)
 }
+
+class BiLineDownPerpView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
