e3eb4d745b86654f2011f3aa163e79f3621919ec
Anwesh43 <anweshthecool0@gmail.com>
2026-03-28 16:50:37 +0530

    linepartrotdownview: adding Renderer in View

--- linepartrotdownview/src/main/java/com/example/linepartrotdownview/LineRotPartDownView.kt
+++ linepartrotdownview/src/main/java/com/example/linepartrotdownview/LineRotPartDownView.kt
@@ -63,14 +63,16 @@ fun Canvas.drawLRPDNode(i : Int, scale : Float, paint : Paint) {
 
 class LineRotPartDownView(ctx : Context) : View(ctx) {
 
-    override fun onDraw(canvas : Canvas) {
+    private val renderer : Renderer = Renderer(this)
 
+    override fun onDraw(canvas : Canvas) {
+        renderer.render(canvas)
     }
 
     override fun onTouchEvent(event : MotionEvent) : Boolean {
         when (event.action) {
             MotionEvent.ACTION_DOWN -> {
-
+                renderer.handleTap()
             }
         }
         return true
