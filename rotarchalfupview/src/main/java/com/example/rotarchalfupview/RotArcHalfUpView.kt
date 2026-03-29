6d4a6a295295e2e783e190dab017970ab446e97c
Anwesh43 <anweshthecool0@gmail.com>
2026-03-29 14:44:14 +0530

    rotarchalfupview: adding RotArcHalfUpView class

--- rotarchalfupview/src/main/java/com/example/rotarchalfupview/RotArcHalfUpView.kt
+++ rotarchalfupview/src/main/java/com/example/rotarchalfupview/RotArcHalfUpView.kt
@@ -56,3 +56,19 @@ fun Canvas.drawRAHUNode(i : Int, scale : Float, paint : Paint) {
     paint.style = Paint.Style.STROKE
     drawRotArcHalfUp(scale, w, h, paint)
 }
+
+class RotArcHalfUpView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
