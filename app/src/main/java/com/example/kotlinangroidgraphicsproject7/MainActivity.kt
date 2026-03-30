6d5eee7c329705c1bcdfc6069c9821706a2f77f0
Anwesh43 <anweshthecool0@gmail.com>
2026-03-30 08:10:59 +0530

    bilinerotarcview: adding view in activity

--- app/src/main/java/com/example/kotlinangroidgraphicsproject7/MainActivity.kt
+++ app/src/main/java/com/example/kotlinangroidgraphicsproject7/MainActivity.kt
@@ -11,13 +11,13 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.example.bilinerotarcview.BiLineRotArcView
 import com.example.kotlinangroidgraphicsproject7.ui.theme.KotlinAngroidGraphicsProject7Theme
-import com.example.rotarchalfupview.RotArcHalfUpView
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         //LineRotPartialArcView.create(this)
-        RotArcHalfUpView.create(this)
+        BiLineRotArcView.create(this)
     }
 }
