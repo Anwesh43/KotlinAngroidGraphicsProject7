06a997a5b70667235e0ae40b0ec4b6357f0090b7
Anwesh43 <anweshthecool0@gmail.com>
2026-04-02 09:57:00 +0530

    arcupjoinrightview: adding ArcUpJoinRight class

--- arcupjoinrightview/src/main/java/com/example/arcupjoinrightview/ArcUpJoinRightView.kt
+++ arcupjoinrightview/src/main/java/com/example/arcupjoinrightview/ArcUpJoinRightView.kt
@@ -164,4 +164,27 @@ class ArcUpJoinRightView(ctx : Context) : View(ctx) {
             return this
         }
     }
+
+    data class ArcUpJoinRight(var i : Int) {
+
+        private var curr : AUJRNode = AUJRNode(0)
+        private var dir : Int = 1
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            curr.draw(canvas, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            curr.update {
+                curr = curr.getNext(dir) {
+                    dir *= -1
+                }
+                cb(it)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            curr.startUpdating(cb)
+        }
+    }
 }
