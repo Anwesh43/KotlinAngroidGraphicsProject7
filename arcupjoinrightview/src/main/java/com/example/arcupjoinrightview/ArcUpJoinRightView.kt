bc3fe30620f85521332e763fb2a722258b62737e
Anwesh43 <anweshthecool0@gmail.com>
2026-04-02 09:55:08 +0530

    arcupjoinrightview: adding AUJRNode class

--- arcupjoinrightview/src/main/java/com/example/arcupjoinrightview/ArcUpJoinRightView.kt
+++ arcupjoinrightview/src/main/java/com/example/arcupjoinrightview/ArcUpJoinRightView.kt
@@ -123,4 +123,45 @@ class ArcUpJoinRightView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class AUJRNode(var i : Int = 0, private val state : State = State()) {
+
+        private var next : AUJRNode? = null
+        private var prev : AUJRNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = AUJRNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawAUJRNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : AUJRNode {
+            var curr : AUJRNode? = prev
+            if (dir === 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
