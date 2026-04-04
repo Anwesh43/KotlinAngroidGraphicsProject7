7e89e5a94fe7b057064761dfe36d6c6a9f0ed888
Anwesh43 <anweshthecool0@gmail.com>
2026-04-05 04:15:02 +0530

    arcbentsizedownview: adding ABSDNode class

--- arcbentsizedownview/src/main/java/com/example/arcbentsizedownview/ArcBentSizeDownView.kt
+++ arcbentsizedownview/src/main/java/com/example/arcbentsizedownview/ArcBentSizeDownView.kt
@@ -123,4 +123,45 @@ class ArcBentSizeDownView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class ABSDNode(var i : Int = 0, val state : State = State()) {
+
+        private var next : ABSDNode? = null
+        private var prev : ABSDNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = ABSDNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawABSDNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : ABSDNode {
+            var curr : ABSDNode? = prev
+            if (dir === 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
