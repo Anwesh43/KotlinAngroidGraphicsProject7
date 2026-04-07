aa551c03dcc3498798dd36f503a6aa0323795727
Anwesh43 <anweshthecool0@gmail.com>
2026-04-07 07:28:47 +0530

    arcbentlineleftview: adding ABLLNode class

--- arcbentlineleftview/src/main/java/com/example/arcbentlineleftview/ArcBentLineLeftView.kt
+++ arcbentlineleftview/src/main/java/com/example/arcbentlineleftview/ArcBentLineLeftView.kt
@@ -121,4 +121,45 @@ class ArcBentLineLeftView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class ABLLNode(var i : Int = 0, val state : State = State()) {
+
+        private var next : ABLLNode? = null
+        private var prev : ABLLNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = ABLLNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawABLLNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : ABLLNode {
+            var curr : ABLLNode? = prev
+            if (dir === 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
