8f63e4856d30f75c082bb3e26cbaf856747f57de
Anwesh43 <anweshthecool0@gmail.com>
2026-04-07 07:17:21 +0530

    arcbentlineleftview: adding ArcBentLineLeftView class

--- arcbentlineleftview/src/main/java/com/example/arcbentlineleftview/ArcBentLineLeftView.kt
+++ arcbentlineleftview/src/main/java/com/example/arcbentlineleftview/ArcBentLineLeftView.kt
@@ -58,3 +58,19 @@ fun Canvas.drawABLLNode(i : Int, scale : Float, paint : Paint) {
     paint.style = Paint.Style.STROKE
     drawArcBentLineLeft(scale, w, h, paint)
 }
+
+class ArcBentLineLeftView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
