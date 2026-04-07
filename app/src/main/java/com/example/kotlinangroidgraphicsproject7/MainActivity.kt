b5c9e5adaf7a7213fee85d4882d157033bd6096c
Anwesh43 <anweshthecool0@gmail.com>
2026-04-07 07:39:28 +0530

    arcbentlineleftview: adding view in activity

--- app/src/main/java/com/example/kotlinangroidgraphicsproject7/MainActivity.kt
+++ app/src/main/java/com/example/kotlinangroidgraphicsproject7/MainActivity.kt
@@ -11,13 +11,13 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import com.example.arcbentsizedownview.ArcBentSizeDownView
+import com.example.arcbentlineleftview.ArcBentLineLeftView
 import com.example.kotlinangroidgraphicsproject7.ui.theme.KotlinAngroidGraphicsProject7Theme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         //LineRotPartialArcView.create(this)
-        ArcBentSizeDownView.create(this)
+        ArcBentLineLeftView.create(this)
     }
 }
