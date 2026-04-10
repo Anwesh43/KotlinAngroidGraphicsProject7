4fb51dcec2f545faed07cb05fdf7b96d3c22b1d6
Anwesh43 <anweshthecool0@gmail.com>
2026-04-11 00:38:30 +0530

    linetshaperotdownview: adding LineTShapeRotDownView class

--- linetshaperotdownview/src/main/java/com/example/linetshaperotdownview/LineTShapeRotDownView.kt
+++ linetshaperotdownview/src/main/java/com/example/linetshaperotdownview/LineTShapeRotDownView.kt
@@ -58,3 +58,19 @@ fun Canvas.drawLTSRDNode(i : Int, scale : Float, paint : Paint) {
     paint.style = Paint.Style.STROKE
     drawLineTShapeRotDown(scale, w, h, paint)
 }
+
+class LineTShapeRotDownView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
