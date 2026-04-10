d8c7bd14b3315f8efacd4251618cd0fbe5cf943a
Anwesh43 <anweshthecool0@gmail.com>
2026-04-11 00:48:38 +0530

    linetshaperotdownview: adding LTSRDNode class

--- linetshaperotdownview/src/main/java/com/example/linetshaperotdownview/LineTShapeRotDownView.kt
+++ linetshaperotdownview/src/main/java/com/example/linetshaperotdownview/LineTShapeRotDownView.kt
@@ -115,4 +115,45 @@ class LineTShapeRotDownView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class LTSRDNode(var i : Int = 0, val state : State = State()) {
+
+        private var next : LTSRDNode? = null
+        private var prev : LTSRDNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = LTSRDNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawLTSRDNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : LTSRDNode {
+            var curr : LTSRDNode? = prev
+            if (dir === 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
