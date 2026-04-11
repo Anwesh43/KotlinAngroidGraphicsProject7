ea174c11b6a1eebde3b9ce9d176f2073f77cb3fc
Anwesh43 <anweshthecool0@gmail.com>
2026-04-12 02:30:41 +0530

    dividearcrotcircleview: adding Renderer class

--- dividearcrotcircleview/src/main/java/com/example/dividearcrotcircleview/DivideArcRotCircleView.kt
+++ dividearcrotcircleview/src/main/java/com/example/dividearcrotcircleview/DivideArcRotCircleView.kt
@@ -191,4 +191,27 @@ class DivideArcRotCircleView(ctx : Context) : View(ctx) {
             curr.startUpdating(cb)
         }
     }
+
+    data class Renderer(var view : DivideArcRotCircleView) {
+
+        private val animator : Animator = Animator(view)
+        private val paint : Paint = Paint(Paint.ANTI_ALIAS_FLAG)
+        private val darc : DivideArcRotCircle = DivideArcRotCircle(0)
+
+        fun render(canvas : Canvas) {
+            canvas.drawColor(backColor)
+            darc.draw(canvas, paint)
+            animator.animate {
+                darc.update {
+                    animator.stop()
+                }
+            }
+        }
+
+        fun handleTap() {
+            darc.startUpdating {
+                animator.start()
+            }
+        }
+    }
 }
