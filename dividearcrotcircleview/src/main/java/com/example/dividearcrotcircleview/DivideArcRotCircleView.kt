9cbdb701c0079b13671b64949137fa9406cdef0d
Anwesh43 <anweshthecool0@gmail.com>
2026-04-12 02:20:19 +0530

    dividearcrotcircleview: adding DivideArcRotCircleView class

--- dividearcrotcircleview/src/main/java/com/example/dividearcrotcircleview/DivideArcRotCircleView.kt
+++ dividearcrotcircleview/src/main/java/com/example/dividearcrotcircleview/DivideArcRotCircleView.kt
@@ -64,3 +64,19 @@ fun Canvas.drawDARCNode(i : Int, scale : Float, paint : Paint) {
     paint.style = Paint.Style.STROKE
     drawDivideArcRotCircle(scale, w, h, paint)
 }
+
+class DivideArcRotCircleView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
