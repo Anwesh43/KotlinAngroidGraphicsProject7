8ae4ea8b0dfc95c02c229662a081112bf67b68a2
Anwesh43 <anweshthecool0@gmail.com>
2026-04-12 02:25:33 +0530

    dividearcrotcircleview: adding DARCNode class

--- dividearcrotcircleview/src/main/java/com/example/dividearcrotcircleview/DivideArcRotCircleView.kt
+++ dividearcrotcircleview/src/main/java/com/example/dividearcrotcircleview/DivideArcRotCircleView.kt
@@ -127,4 +127,45 @@ class DivideArcRotCircleView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class DARCNode(var i : Int = 0, val state : State = State()) {
+
+        private var next : DARCNode? = null
+        private var prev : DARCNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = DARCNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawDARCNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : DARCNode {
+            var curr : DARCNode? = prev
+            if (dir === 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
