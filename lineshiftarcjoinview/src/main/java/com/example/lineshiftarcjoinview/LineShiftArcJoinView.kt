ee04dd1156b0a2abb2674b46304aaac7de503817
Anwesh43 <anweshthecool0@gmail.com>
2026-04-15 16:46:59 +0530

    lineshiftarcjoinview: adding LSAJNode class

--- lineshiftarcjoinview/src/main/java/com/example/lineshiftarcjoinview/LineShiftArcJoinView.kt
+++ lineshiftarcjoinview/src/main/java/com/example/lineshiftarcjoinview/LineShiftArcJoinView.kt
@@ -123,4 +123,45 @@ class LineShiftArcJoinView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class LSAJNode(var i : Int = 0, val state : State = State()) {
+
+        private var next : LSAJNode? = null
+        private var prev : LSAJNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = LSAJNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawLSAJNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : LSAJNode {
+            var curr : LSAJNode? = prev
+            if (dir === 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
