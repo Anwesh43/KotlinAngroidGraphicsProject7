3ef4a69fdbb6b7164eac1ea917cdc8df64ed63c2
Anwesh43 <anweshthecool0@gmail.com>
2026-04-16 21:48:51 +0530

    lineshiftarcjoinview: adding view in activity

--- app/src/main/java/com/example/kotlinangroidgraphicsproject7/MainActivity.kt
+++ app/src/main/java/com/example/kotlinangroidgraphicsproject7/MainActivity.kt
@@ -12,12 +12,12 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.kotlinangroidgraphicsproject7.ui.theme.KotlinAngroidGraphicsProject7Theme
-import com.example.dividearcrotcircleview.DivideArcRotCircleView
+import com.example.lineshiftarcjoinview.LineShiftArcJoinView
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         //LineRotPartialArcView.create(this)
-        DivideArcRotCircleView.create(this)
+        LineShiftArcJoinView.create(this)
     }
 }
