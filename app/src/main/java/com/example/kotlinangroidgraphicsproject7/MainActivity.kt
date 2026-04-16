9f59aff88146fc319d5c85a7c2382196411dc8a8
Anwesh43 <anweshthecool0@gmail.com>
2026-04-17 05:25:49 +0530

    bisectarclineencloseview: adding view in activity

--- app/src/main/java/com/example/kotlinangroidgraphicsproject7/MainActivity.kt
+++ app/src/main/java/com/example/kotlinangroidgraphicsproject7/MainActivity.kt
@@ -11,13 +11,13 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.example.bisectarclineencloseview.BisectArcLineEncloseView
 import com.example.kotlinangroidgraphicsproject7.ui.theme.KotlinAngroidGraphicsProject7Theme
-import com.example.lineshiftarcjoinview.LineShiftArcJoinView
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         //LineRotPartialArcView.create(this)
-        LineShiftArcJoinView.create(this)
+        BisectArcLineEncloseView.create(this)
     }
 }
