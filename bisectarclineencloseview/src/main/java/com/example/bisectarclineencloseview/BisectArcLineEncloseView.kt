71b5d8da5ef7fee1fe3c20ffb3fa9d740257931c
Anwesh43 <anweshthecool0@gmail.com>
2026-04-17 05:24:52 +0530

    bisectarclineencloseview: adding static create method

--- bisectarclineencloseview/src/main/java/com/example/bisectarclineencloseview/BisectArcLineEncloseView.kt
+++ bisectarclineencloseview/src/main/java/com/example/bisectarclineencloseview/BisectArcLineEncloseView.kt
@@ -214,4 +214,12 @@ class BisectArcLineEncloseView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    companion object {
+        fun create(activity : Activity) : BisectArcLineEncloseView {
+            val view : BisectArcLineEncloseView = BisectArcLineEncloseView(activity)
+            activity.setContentView(view)
+            return view
+        }
+    }
 }
