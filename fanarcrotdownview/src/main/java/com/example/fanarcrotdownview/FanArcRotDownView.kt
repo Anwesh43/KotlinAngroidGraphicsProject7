cb269ba68adde889712b1967fe87c68fa10d3a74
Anwesh43 <anweshthecool0@gmail.com>
2026-04-19 00:32:47 +0530

    fanarcrotdownview: adding Canvas extension methods

--- fanarcrotdownview/src/main/java/com/example/fanarcrotdownview/FanArcRotDownView.kt
+++ fanarcrotdownview/src/main/java/com/example/fanarcrotdownview/FanArcRotDownView.kt
@@ -23,7 +23,42 @@ val sizeFactor : Float = 5.9f
 val delay : Long = 20
 val backColor : Int = "#BDBDBD".toColorInt()
 val rot : Float = 60f
+val deg : Float = 90f
 
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+
+fun Canvas.drawXY(x : Float, y : Float, cb : () -> Unit) {
+    save()
+    translate(x, y)
+    cb()
+    restore()
+}
+
+fun Canvas.drawFanArcRotDown(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val dsc : (Int) -> Float = {
+        scale.divideScale(it, parts)
+    }
+    drawXY(w / 2, h / 2 + (h / 2) * dsc(4)) {
+        rotate(deg * dsc(3))
+        for (j in 0..1) {
+            drawXY(0f, 0f) {
+                rotate(-rot * 0.5f * (1f - 2 * j))
+                drawLine(size * j * (1 - dsc(2 * j)), 0f, size * (dsc(j) * (1 - j) + j), 0f, paint)
+
+            }
+        }
+    }
+}
+
+fun Canvas.drawFARDNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = colors[i].toColorInt()
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    paint.style = Paint.Style.STROKE
+    drawFanArcRotDown(scale, w, h, paint)
+}
