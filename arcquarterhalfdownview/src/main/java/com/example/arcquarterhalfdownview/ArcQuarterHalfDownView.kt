ff7c9af230876ca779a85386adb9c2df92b35e64
Anwesh43 <anweshthecool0@gmail.com>
2026-04-18 07:38:19 +0530

    arcquarterhalfdownview: adding ArcQuarterHalfDown class

--- arcquarterhalfdownview/src/main/java/com/example/arcquarterhalfdownview/ArcQuarterHalfDownView.kt
+++ arcquarterhalfdownview/src/main/java/com/example/arcquarterhalfdownview/ArcQuarterHalfDownView.kt
@@ -163,4 +163,27 @@ class ArcQuarterHalfDownView(ctx : Context) : View(ctx) {
             return this
         }
     }
+
+    data class ArcQuarterHalfDown(var i : Int) {
+
+        private var curr : AQHDNode = AQHDNode(0)
+        private var dir : Int = 1
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            curr.draw(canvas, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            curr.update {
+                curr = curr.getNext(dir) {
+                    dir *= -1
+                }
+                cb(it)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            curr.startUpdating(cb)
+        }
+    }
 }
