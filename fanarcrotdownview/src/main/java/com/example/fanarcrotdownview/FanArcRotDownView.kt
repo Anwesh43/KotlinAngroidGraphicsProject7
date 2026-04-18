5dc0e0bff1a54b278dadcbe075b17a4cdde6192f
Anwesh43 <anweshthecool0@gmail.com>
2026-04-19 00:41:04 +0530

    fanarcrotdownview: adding FARDNode class

--- fanarcrotdownview/src/main/java/com/example/fanarcrotdownview/FanArcRotDownView.kt
+++ fanarcrotdownview/src/main/java/com/example/fanarcrotdownview/FanArcRotDownView.kt
@@ -125,4 +125,45 @@ class FanArcRotDownView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class FARDNode(var i : Int = 0, val state : State = State()) {
+
+        private var next : FARDNode? = null
+        private var prev : FARDNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = FARDNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawFARDNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : FARDNode {
+            var curr : FARDNode? = prev
+            if (dir === 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
