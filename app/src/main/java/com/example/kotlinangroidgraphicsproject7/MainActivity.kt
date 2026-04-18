38325bb930b2a0bcd173f015171ffcacdc33f92f
Anwesh43 <anweshthecool0@gmail.com>
2026-04-18 07:46:39 +0530

    arcquarterhalfdownview: adding view in activity

--- app/src/main/java/com/example/kotlinangroidgraphicsproject7/MainActivity.kt
+++ app/src/main/java/com/example/kotlinangroidgraphicsproject7/MainActivity.kt
@@ -11,13 +11,13 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import com.example.bisectarclineencloseview.BisectArcLineEncloseView
+import com.example.arcquarterhalfdownview.ArcQuarterHalfDownView
 import com.example.kotlinangroidgraphicsproject7.ui.theme.KotlinAngroidGraphicsProject7Theme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         //LineRotPartialArcView.create(this)
-        BisectArcLineEncloseView.create(this)
+        ArcQuarterHalfDownView.create(this)
     }
 }
