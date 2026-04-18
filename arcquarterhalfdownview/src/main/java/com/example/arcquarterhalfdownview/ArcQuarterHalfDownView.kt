c323fb630bb337e38bcc28d6c6ebef55e3ca3bff
Anwesh43 <anweshthecool0@gmail.com>
2026-04-18 07:37:04 +0530

    arcquarterhalfdownview: adding AQHDNode class

--- arcquarterhalfdownview/src/main/java/com/example/arcquarterhalfdownview/ArcQuarterHalfDownView.kt
+++ arcquarterhalfdownview/src/main/java/com/example/arcquarterhalfdownview/ArcQuarterHalfDownView.kt
@@ -122,4 +122,45 @@ class ArcQuarterHalfDownView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class AQHDNode(var i : Int = 0, val state : State = State()) {
+
+        private var next : AQHDNode? = null
+        private var prev : AQHDNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = AQHDNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawAQHDNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : AQHDNode {
+            var curr : AQHDNode? = prev
+            if (dir === 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
