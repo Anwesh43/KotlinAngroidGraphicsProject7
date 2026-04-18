c25da3a57237c4c21945dfcce30a3118a6914146
Anwesh43 <anweshthecool0@gmail.com>
2026-04-19 00:36:38 +0530

    fanarcrotdownview: adding FanArcRotDownView class

--- fanarcrotdownview/src/main/java/com/example/fanarcrotdownview/FanArcRotDownView.kt
+++ fanarcrotdownview/src/main/java/com/example/fanarcrotdownview/FanArcRotDownView.kt
@@ -62,3 +62,19 @@ fun Canvas.drawFARDNode(i : Int, scale : Float, paint : Paint) {
     paint.style = Paint.Style.STROKE
     drawFanArcRotDown(scale, w, h, paint)
 }
+
+class FanArcRotDownView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
