f012a76775f57d589ec49e02c8614617790a1dc3
Anwesh43 <anweshthecool0@gmail.com>
2026-04-21 07:21:25 +0530

    midarchalflineview: adding MAHLNode class

--- midarchalflineview/src/main/java/com/example/midarchalflineview/MidArcHalfLineView.kt
+++ midarchalflineview/src/main/java/com/example/midarchalflineview/MidArcHalfLineView.kt
@@ -123,4 +123,45 @@ class MidArcHalfLneView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class MAHLNode(var i : Int = 0, val state : State = State()) {
+
+        private var next : MAHLNode? = null
+        private var prev : MAHLNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = MAHLNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawMHALNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : MAHLNode {
+            var curr : MAHLNode? = prev
+            if (dir === 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
