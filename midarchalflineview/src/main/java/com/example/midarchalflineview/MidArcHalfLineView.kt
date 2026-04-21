ae7dd6462de62981dccbdaadacfe9b461df5fdcb
Anwesh43 <anweshthecool0@gmail.com>
2026-04-21 07:15:44 +0530

    midarchalflineview: adding MidArcHalfLineView class

--- midarchalflineview/src/main/java/com/example/midarchalflineview/MidArcHalfLineView.kt
+++ midarchalflineview/src/main/java/com/example/midarchalflineview/MidArcHalfLineView.kt
@@ -60,3 +60,19 @@ fun Canvas.drawMHALNode(i : Int, scale : Float, paint : Paint) {
     paint.style = Paint.Style.STROKE
     drawMidHalfArcLine(scale,w , h, paint)
 }
+
+class MidArcHalfLneView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
