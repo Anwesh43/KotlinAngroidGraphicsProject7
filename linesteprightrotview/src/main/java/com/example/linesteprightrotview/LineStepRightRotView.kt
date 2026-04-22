8a38594835efc27c288390b73887c3fb2e2d62a5
Anwesh43 <anweshthecool0@gmail.com>
2026-04-22 09:59:11 +0530

    linesteprightrotview: adding constants and libraries

---
+++ linesteprightrotview/src/main/java/com/example/linesteprightrotview/LineStepRightRotView.kt
@@ -0,0 +1,24 @@
+package com.example.linesteprightrotview
+
+import android.view.View
+import android.view.MotionEvent
+import android.graphics.Paint
+import android.graphics.Canvas
+import androidx.core.graphics.toColorInt
+import android.app.Activity
+import android.content.Context
+
+val colors : Array<String> = arrayOf(
+    "#1A237E",
+    "#EF5350",
+    "#AA00FF",
+    "#C51162",
+    "#00C853"
+)
+val parts : Int = 7
+val scGap : Float = 0.05f / parts
+val rot : Float = 90f
+val delay : Long = 20
+val backColor : Int = "#BDBDBD".toColorInt()
+val sizeFactor : Float = 5.9f
+val strokeFactor : Float = 90f
