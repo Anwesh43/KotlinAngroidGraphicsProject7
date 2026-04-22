647570044246e20f4ab99cb17f4e5badea690cc7
Anwesh43 <anweshthecool0@gmail.com>
2026-04-22 10:06:28 +0530

    linesteprightrotview: adding LineStepRightRotView class

--- linesteprightrotview/src/main/java/com/example/linesteprightrotview/LineStepRightRotView.kt
+++ linesteprightrotview/src/main/java/com/example/linesteprightrotview/LineStepRightRotView.kt
@@ -64,3 +64,18 @@ fun Canvas.drawLSRRNode(i : Int, scale : Float, paint : Paint) {
     drawLineStepRightRot(scale, w, h, paint)
 }
 
+class LineStepRightRotView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
