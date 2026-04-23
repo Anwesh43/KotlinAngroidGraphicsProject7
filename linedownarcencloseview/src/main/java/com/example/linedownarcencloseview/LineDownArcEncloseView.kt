e728e37e28d2a055277d8f0d1a8e46b3ff5a1068
Anwesh43 <anweshthecool0@gmail.com>
2026-04-23 08:48:45 +0530

    linedownarcencloseview: adding Renderer class

--- linedownarcencloseview/src/main/java/com/example/linedownarcencloseview/LineDownArcEncloseView.kt
+++ linedownarcencloseview/src/main/java/com/example/linedownarcencloseview/LineDownArcEncloseView.kt
@@ -189,4 +189,27 @@ class LineDownArcEncloseView(ctx : Context) : View(ctx) {
             curr.startUpdating(cb)
         }
     }
+
+    data class Renderer(var view : LineDownArcEncloseView) {
+
+        private val animator : Animator = Animator(view)
+        private val paint : Paint = Paint(Paint.ANTI_ALIAS_FLAG)
+        private val ldea : LineDownArcEnclose = LineDownArcEnclose(0)
+
+        fun render(canvas : Canvas) {
+            canvas.drawColor(backColor)
+            ldea.draw(canvas, paint)
+            animator.animate {
+                ldea.update {
+                    animator.stop()
+                }
+            }
+        }
+
+        fun handleTap() {
+            ldea.startUpdating {
+                animator.start()
+            }
+        }
+    }
 }
