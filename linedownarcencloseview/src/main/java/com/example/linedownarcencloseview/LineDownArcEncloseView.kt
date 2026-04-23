25b9d901ac49a4ae8db77f7aaa0fd5be23e1ef6b
Anwesh43 <anweshthecool0@gmail.com>
2026-04-23 08:12:07 +0530

    linedownarcencloseview: adding LineDownArcEncloseView class

--- linedownarcencloseview/src/main/java/com/example/linedownarcencloseview/LineDownArcEncloseView.kt
+++ linedownarcencloseview/src/main/java/com/example/linedownarcencloseview/LineDownArcEncloseView.kt
@@ -62,3 +62,19 @@ fun Canvas.drawLDAENode(i : Int, scale : Float, paint : Paint) {
     paint.style = Paint.Style.STROKE
     drawLineDownArcEnclose(scale, w, h, paint)
 }
+
+class LineDownArcEncloseView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
