808c2c04a017d56f84125d3657b74174b2ffe58b
Anwesh43 <anweshthecool0@gmail.com>
2026-04-23 08:10:11 +0530

    linedownarcencloseview: adding Canvas extension methods

--- linedownarcencloseview/src/main/java/com/example/linedownarcencloseview/LineDownArcEncloseView.kt
+++ linedownarcencloseview/src/main/java/com/example/linedownarcencloseview/LineDownArcEncloseView.kt
@@ -29,3 +29,36 @@ fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int): Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
 
+fun Canvas.drawXY(x : Float, y : Float, cb : () -> Unit) {
+    save()
+    translate(x, y)
+    cb()
+    restore()
+}
+
+fun Canvas.drawLineDownArcEnclose(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val dsc : (Int) -> Float = {
+        scale.divideScale(it, parts)
+    }
+    drawXY(w / 2 - (w / 2) * dsc(4), h / 2) {
+        drawXY(0f, 0f) {
+            rotate(rot * dsc(2))
+            drawArc(RectF(0f, -size / 2, size, size / 2), 180f, 90f * dsc(1), false, paint)
+        }
+        drawXY(0f, -h * 0.5f * (1 - dsc(0))) {
+            rotate(deg * dsc(3))
+            drawLine(0f, 0f, 0f, -size, paint)
+        }
+    }
+}
+
+fun Canvas.drawLDAENode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = colors[i].toColorInt()
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    paint.style = Paint.Style.STROKE
+    drawLineDownArcEnclose(scale, w, h, paint)
+}
