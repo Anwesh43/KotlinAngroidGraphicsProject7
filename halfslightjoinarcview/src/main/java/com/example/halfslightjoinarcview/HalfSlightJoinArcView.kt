e063fd3bccabf1b5e50948feaf566452c8038ac9
Anwesh43 <anweshthecool0@gmail.com>
2026-04-24 08:18:32 +0530

    halfslightjoinarcview: adding HalfSlightJoinArcView class

--- halfslightjoinarcview/src/main/java/com/example/halfslightjoinarcview/HalfSlightJoinArcView.kt
+++ halfslightjoinarcview/src/main/java/com/example/halfslightjoinarcview/HalfSlightJoinArcView.kt
@@ -63,3 +63,19 @@ fun Canvas.drawLDAENode(i : Int, scale : Float, paint : Paint) {
     paint.style = Paint.Style.STROKE
     drawHalfSlightJoinArc(scale, w, h, paint)
 }
+
+class HalfSlightJoinArcView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
