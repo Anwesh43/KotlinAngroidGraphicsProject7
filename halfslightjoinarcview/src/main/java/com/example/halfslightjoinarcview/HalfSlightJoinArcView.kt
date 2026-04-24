fbb2c80ed9013889faaa0b8fe5e5da3f1d3c1d4e
Anwesh43 <anweshthecool0@gmail.com>
2026-04-24 08:36:45 +0530

    halfslightjoinarcview: adding Renderer class

--- halfslightjoinarcview/src/main/java/com/example/halfslightjoinarcview/HalfSlightJoinArcView.kt
+++ halfslightjoinarcview/src/main/java/com/example/halfslightjoinarcview/HalfSlightJoinArcView.kt
@@ -190,4 +190,27 @@ class HalfSlightJoinArcView(ctx : Context) : View(ctx) {
             curr.startUpdating(cb)
         }
     }
+
+    data class Renderer(var view : HalfSlightJoinArcView) {
+
+        private val animator : Animator = Animator(view)
+        private val paint : Paint = Paint(Paint.ANTI_ALIAS_FLAG)
+        private val hsja : HalfSlightJoinArc = HalfSlightJoinArc(0)
+
+        fun render(canvas : Canvas) {
+            canvas.drawColor(backColor)
+            hsja.draw(canvas, paint)
+            animator.animate {
+                hsja.update {
+                    animator.stop()
+                }
+            }
+        }
+
+        fun handleTap() {
+            hsja.startUpdating {
+                animator.start()
+            }
+        }
+    }
 }
