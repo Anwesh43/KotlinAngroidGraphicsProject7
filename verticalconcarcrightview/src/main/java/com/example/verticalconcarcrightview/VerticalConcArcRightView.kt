0baab7d7fc97bc5ac8bd83f126808cbf65c9c963
Anwesh43 <anweshthecool0@gmail.com>
2026-04-25 07:02:16 +0530

    verticalconcarcrightview: adding VerricalConcArcRight class

--- verticalconcarcrightview/src/main/java/com/example/verticalconcarcrightview/VerticalConcArcRightView.kt
+++ verticalconcarcrightview/src/main/java/com/example/verticalconcarcrightview/VerticalConcArcRightView.kt
@@ -163,4 +163,27 @@ class VerticalConcArcRightView(ctx : Context) : View(ctx) {
             return this
         }
     }
+
+    data class VerticalConcArcRight(var i : Int) {
+
+        private var curr : VCARNode = VCARNode(0)
+        private var dir : Int = 1
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            curr.draw(canvas, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            curr.update {
+                curr = curr.getNext(dir) {
+                    dir *= -1
+                }
+                cb(it)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            curr.startUpdating(cb)
+        }
+    }
 }
