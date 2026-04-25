3435d65d7ad3edaeaf5351d8bc478b5b95b6155b
Anwesh43 <anweshthecool0@gmail.com>
2026-04-25 07:01:04 +0530

    verticalconcarcrightview: adding VCARNode class

--- verticalconcarcrightview/src/main/java/com/example/verticalconcarcrightview/VerticalConcArcRightView.kt
+++ verticalconcarcrightview/src/main/java/com/example/verticalconcarcrightview/VerticalConcArcRightView.kt
@@ -122,4 +122,45 @@ class VerticalConcArcRightView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class VCARNode(var i : Int = 0, val state : State = State()) {
+
+        private var next : VCARNode? = null
+        private var prev : VCARNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = VCARNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawVCARNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : VCARNode {
+            var curr : VCARNode? = prev
+            if (dir === 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
