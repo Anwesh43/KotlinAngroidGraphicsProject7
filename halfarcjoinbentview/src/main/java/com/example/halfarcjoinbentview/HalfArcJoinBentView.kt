13b2ed34b489cdc75d3ca344fc27f2b1aec5c7a4
Anwesh43 <anweshthecool0@gmail.com>
2026-04-27 13:10:11 +0530

    halfarcjoinbentview: adding HalfArcJoinBentView class

--- halfarcjoinbentview/src/main/java/com/example/halfarcjoinbentview/HalfArcJoinBentView.kt
+++ halfarcjoinbentview/src/main/java/com/example/halfarcjoinbentview/HalfArcJoinBentView.kt
@@ -61,3 +61,19 @@ fun Canvas.drawHAJBNode(i : Int, scale : Float, paint : Paint) {
     paint.style = Paint.Style.STROKE
     drawHalfArcJoinBent(scale, w, h, paint)
 }
+
+class HalfArcJoinBentView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
