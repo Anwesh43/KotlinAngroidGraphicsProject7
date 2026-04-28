a986f0f4e3d14a366b58402ddf33d17346f34f76
Anwesh43 <anweshthecool0@gmail.com>
2026-04-28 07:56:46 +0530

    biarcclosedownview: adding view in activity

--- app/src/main/java/com/example/kotlinangroidgraphicsproject7/MainActivity.kt
+++ app/src/main/java/com/example/kotlinangroidgraphicsproject7/MainActivity.kt
@@ -11,13 +11,13 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import com.example.halfarcjoinbentview.HalfArcJoinBentView
+import com.example.biarcclosedownview.BiArcCloseDownView
 import com.example.kotlinangroidgraphicsproject7.ui.theme.KotlinAngroidGraphicsProject7Theme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         //LineRotPartialArcView.create(this)
-        HalfArcJoinBentView.create(this)
+        BiArcCloseDownView.create(this)
     }
 }
