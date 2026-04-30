27b941c77516cbacc1f749a67bdf5ed9220e7bfd
Anwesh43 <anweshthecool0@gmail.com>
2026-04-30 08:25:14 +0530

    linearcrightrotview: adding LineArcRightRotView class

--- linearcrightrotview/src/main/java/com/example/linearcrightrotview/LineArcRightRotView.kt
+++ linearcrightrotview/src/main/java/com/example/linearcrightrotview/LineArcRightRotView.kt
@@ -62,3 +62,19 @@ fun Canvas.drawLARRNode(i : Int, scale : Float, paint : Paint) {
     paint.style = Paint.Style.STROKE
     drawLineArcRightRot(scale, w, h, paint)
 }
+
+class LineArcRightRotView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
