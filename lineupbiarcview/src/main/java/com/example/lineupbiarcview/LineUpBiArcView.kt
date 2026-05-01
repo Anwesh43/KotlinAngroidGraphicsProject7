e28f5a42deeda4a09a1ae5e66ba481924eba2e44
Anwesh43 <anweshthecool0@gmail.com>
2026-05-01 07:36:44 +0530

    lineupbiarcview: adding Canvas extension methods

--- lineupbiarcview/src/main/java/com/example/lineupbiarcview/LineUpBiArcView.kt
+++ lineupbiarcview/src/main/java/com/example/lineupbiarcview/LineUpBiArcView.kt
@@ -27,3 +27,40 @@ val rot : Float = 180f
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+
+fun Canvas.drawXY(x : Float, y : Float, cb : () -> Unit) {
+    save()
+    translate(x, y)
+    cb()
+    restore()
+}
+
+fun Canvas.drawLineUpBiArc(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val dsc : (Int) -> Float = {
+        scale.divideScale(it, parts)
+    }
+    drawXY(w / 2, h / 2) {
+        for (j in 0..1) {
+            drawXY(0f, 0f) {
+                rotate(rot * dsc(4) * j)
+                drawXY(-size + 2 * size * j, 0f) {
+                    drawLine(0f, 0f, 0f, -size * (dsc(3) * j + dsc(0) * (1 - j)), paint)
+                }
+                drawXY(-2 * size * (1 - j), 0f) {
+                    drawArc(RectF(0f, -size, 2 * size, size), -90f + 270f * j, 90f * dsc(1 + j), false, paint)
+                }
+            }
+        }
+    }
+}
+
+fun Canvas.drawLUBANode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = colors[i].toColorInt()
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    paint.style = Paint.Style.STROKE
+    drawLineUpBiArc(scale, w, h, paint)
+}
