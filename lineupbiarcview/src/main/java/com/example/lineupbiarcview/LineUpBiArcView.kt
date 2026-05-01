7b482075db4be3ee0f2a121c634e127f1b574bab
Anwesh43 <anweshthecool0@gmail.com>
2026-05-01 07:42:12 +0530

    lineupbiarcview: adding LUBANode class

--- lineupbiarcview/src/main/java/com/example/lineupbiarcview/LineUpBiArcView.kt
+++ lineupbiarcview/src/main/java/com/example/lineupbiarcview/LineUpBiArcView.kt
@@ -127,4 +127,45 @@ class LineUpBiArcView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class LUBANode(var i : Int = 0, val state : State = State()) {
+
+        private var next : LUBANode? = null
+        private var prev : LUBANode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = LUBANode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawLUBANode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : LUBANode {
+            var curr : LUBANode? = prev
+            if (dir === 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
