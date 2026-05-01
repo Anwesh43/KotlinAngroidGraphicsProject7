741a1e5726ec7e633b872968cf3603e7839a3a07
Anwesh43 <anweshthecool0@gmail.com>
2026-05-02 03:43:26 +0530

    arclinedownrightview: adding ALDRNode class

--- arclinedownrightview/src/main/java/com/example/arclinedownrightview/ArcLineDownRightView.kt
+++ arclinedownrightview/src/main/java/com/example/arclinedownrightview/ArcLineDownRightView.kt
@@ -122,4 +122,45 @@ class ArcLineDownRightView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class ALDRNode(var i : Int = 0, val state : State = State()) {
+
+        private var next : ALDRNode? = null
+        private var prev : ALDRNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = ALDRNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawALDRNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : ALDRNode {
+            var curr : ALDRNode? = prev
+            if (dir === 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
