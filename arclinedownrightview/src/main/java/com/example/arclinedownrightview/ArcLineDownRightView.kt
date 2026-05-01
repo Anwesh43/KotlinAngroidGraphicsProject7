299d051d199b0f4b61289b9d5403878bef92b8c8
Anwesh43 <anweshthecool0@gmail.com>
2026-05-02 03:26:37 +0530

    arclinedownrightview: adding Canvas extension methods

--- arclinedownrightview/src/main/java/com/example/arclinedownrightview/ArcLineDownRightView.kt
+++ arclinedownrightview/src/main/java/com/example/arclinedownrightview/ArcLineDownRightView.kt
@@ -18,7 +18,7 @@ val colors : Array<String> = arrayOf(
 )
 val parts : Int = 5
 val scGap : Float = 0.04f / parts
-val strokeFactro : Float = 90f
+val strokeFactor : Float = 90f
 val sizeFactor : Float = 5.9f
 val delay : Long = 20
 val backColor : Int = "#BDBDBD".toColorInt()
@@ -29,3 +29,33 @@ fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
 
+fun Canvas.drawXY(x : Float, y : Float, cb : () -> Unit) {
+    save()
+    translate(x, y)
+    cb()
+    restore()
+}
+
+fun Canvas.drawArcLineDownRight(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val dsc : (Int) -> Float = {
+        scale.divideScale(it, parts)
+    }
+    drawXY(w / 2 + (w / 2) * dsc(4), h / 2) {
+        rotate(deg * dsc(3))
+        drawArc(RectF(-size / 2, -size, size /2 , 0f), 90f, 180f * dsc(0), false, paint)
+        drawXY(0f, h * 0.5f * (1 - dsc(1))) {
+            rotate(-rot * dsc(2))
+            drawLine(0f, 0f, 0f, size, paint)
+        }
+    }
+}
+
+fun Canvas.drawALDRNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = colors[i].toColorInt()
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    drawArcLineDownRight(scale, w, h, paint)
+}
