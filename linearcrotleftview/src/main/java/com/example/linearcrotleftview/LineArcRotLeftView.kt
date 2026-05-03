530f07e8f94598118fc2e4af51d774b31bce7574
Anwesh43 <anweshthecool0@gmail.com>
2026-05-03 10:20:21 +0530

    linearrcrotleftview: adding Animator class

--- linearcrotleftview/src/main/java/com/example/linearcrotleftview/LineArcRotLeftView.kt
+++ linearcrotleftview/src/main/java/com/example/linearcrotleftview/LineArcRotLeftView.kt
@@ -91,4 +91,32 @@ class LineArcRotLeftView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class Animator(var view : View, var animted : Boolean = false) {
+
+        fun animate(cb : () -> Unit) {
+            if (animated) {
+                cb()
+                try {
+                    Thread.sleep(delay)
+                    view.invalidate()
+                } catch(ex : Exception) {
+
+                }
+            }
+        }
+
+        fun start() {
+            if (!animted) {
+                animted = true
+                view.postInvalidate()
+            }
+        }
+
+        fun stop() {
+            if (animted) {
+                animted = false
+            }
+        }
+    }
 }
