94e6e6bf38ba551a2c4a3648186b09305c927a77
Anwesh43 <anweshthecool0@gmail.com>
2026-05-03 10:11:48 +0530

    linearrcrotleftview: adding LineArcRotLeftView class

--- linearcrotleftview/src/main/java/com/example/linearcrotleftview/LineArcRotLeftView.kt
+++ linearcrotleftview/src/main/java/com/example/linearcrotleftview/LineArcRotLeftView.kt
@@ -56,3 +56,19 @@ fun Canvas.drawLARLNode(i : Int, scale : Float, paint : Paint) {
     paint.style = Paint.Style.STROKE
     drawLineArcRotLeft(scale, w, h, paint)
 }
+
+class LineArcRotLeftView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
