2953836dbf9f637bf795a4801b9fe17bf0396ec9
Anwesh43 <anweshthecool0@gmail.com>
2026-05-04 09:08:33 +0530

    joinarcfroplineview: adding Canvas extension methods

--- joinarcdroplineview/src/main/java/com/example/joinarcdroplineview/JoinArcDropLineView.kt
+++ joinarcdroplineview/src/main/java/com/example/joinarcdroplineview/JoinArcDropLineView.kt
@@ -17,13 +17,52 @@ val colors : Array<String> = arrayOf(
     "#00C853"
 )
 val arcs : Int = 3
-val parts : Int = arcs + 2
+val parts : Int = 4
 val scGap : Float = 0.04f / parts
 val strokeFactor : Float = 90f
 val sizeFactor : Float = 6.9f
 val delay : Long = 20
 val backColor : Int = "#BDBDBD".toColorInt()
+val rot : Float = 180f
 
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+
+fun Canvas.drawXY(x : Float, y : Float, cb : () -> Unit) {
+    save()
+    translate(x, y)
+    cb()
+    restore()
+}
+
+fun Canvas.drawJoinArcDropLine(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val dsc : (Int) -> Float = {
+        scale.divideScale(it, arcs)
+    }
+    val dsk : (Int, Int) -> Float = {a, b ->
+        dsc(a).divideScale(b, parts)
+    }
+    drawXY(w / 2, h / 2) {
+        for (j in 0..(arcs - 1)) {
+            drawXY(-size + size * j, h * 0.5f * dsk(j, 3)) {
+                rotate(dsk(j, 2))
+                drawArc(RectF(-size / 2, 0f, size / 2, -size), 90f * (1 - dsk(j, 0)), 90f * dsk(j, 0), false, paint)
+                drawXY(size / 2, 0f) {
+                    drawLine(-size * 0.5f * dsk(j, 1), 0f, 0f, 0f, paint)
+                }
+            }
+        }
+    }
+}
+
+fun Canvas.drawJADLNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = colors[i].toColorInt()
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    paint.style = Paint.Style.STROKE
+    drawJoinArcDropLine(scale, w, h, paint)
+}
