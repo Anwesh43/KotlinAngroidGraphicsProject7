b7796be2ab19609782536e5f16e4acb21037b287
Anwesh43 <anweshthecool0@gmail.com>
2026-05-04 09:32:23 +0530

    joinarcfroplineview: adding JADLNode class

--- joinarcdroplineview/src/main/java/com/example/joinarcdroplineview/JoinArcDropLineView.kt
+++ joinarcdroplineview/src/main/java/com/example/joinarcdroplineview/JoinArcDropLineView.kt
@@ -130,4 +130,45 @@ class JoinArcDropLineView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class JADLNode(var i : Int = 0, val state : State = State()) {
+
+        private var next : JADLNode? = null
+        private var prev : JADLNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = JADLNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawJADLNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : JADLNode {
+            var curr : JADLNode? = prev
+            if (dir === 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
