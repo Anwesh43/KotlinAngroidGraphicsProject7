19cdb07ba1557f816f8ed4d30322bb96ae608a6b
Anwesh43 <anweshthecool0@gmail.com>
2026-05-04 08:59:56 +0530

    joinarcfroplineview: adding constants and lirbraries

---
+++ joinarcdroplineview/src/main/java/com/example/joinarcdroplineview/JoinArcDropLineView.kt
@@ -0,0 +1,25 @@
+package com.example.joinarcdroplineview
+
+import android.view.View
+import android.view.MotionEvent
+import android.app.Activity
+import android.content.Context
+import android.graphics.Paint
+import android.graphics.Canvas
+import android.graphics.RectF
+import androidx.core.graphics.toColorInt
+
+val colors : Array<String> = arrayOf(
+    "#1A237E",
+    "#EF5350",
+    "#AA00FF",
+    "#C51162",
+    "#00C853"
+)
+val arcs : Int = 3
+val parts : Int = arcs + 2
+val scGap : Float = 0.04f / parts
+val strokeFactor : Float = 90f
+val sizeFactor : Float = 6.9f
+val delay : Long = 20
+val backColor : Int = "#BDBDBD".toColorInt()
