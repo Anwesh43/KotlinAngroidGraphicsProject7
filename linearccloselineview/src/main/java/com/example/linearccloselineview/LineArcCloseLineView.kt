a2283808976d44b4b45b326a3ad0d6966b189dff
Anwesh43 <anweshthecool0@gmail.com>
2026-05-06 14:51:24 +0530

    linearccloselineview: adding Canvas extension methods

--- linearccloselineview/src/main/java/com/example/linearccloselineview/LineArcCloseLineView.kt
+++ linearccloselineview/src/main/java/com/example/linearccloselineview/LineArcCloseLineView.kt
@@ -27,3 +27,38 @@ val rot : Float = 270f
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+
+fun Canvas.drawXY(x : Float, y : Float, cb : () -> Unit) {
+    save()
+    translate(x, y)
+    cb()
+    restore()
+}
+
+fun Canvas.drawLineArcCloseLine(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val dsc : (Int) -> Float = {
+        scale.divideScale(it, parts)
+    }
+    drawXY(w / 2 - (w / 2) * dsc(4), h / 2) {
+        drawXY(-size, 0f) {
+            drawLine(0f, 0f, 0f, -size * dsc(0), paint)
+            drawArc(RectF(-size, -size, size, size), -90f, 90f * dsc(1), false, paint)
+        }
+        drawXY(0f, -h * 0.5f * (1 - dsc(2))) {
+            rotate(rot * dsc(3))
+            drawLine(0f, 0f, 0f, -size, paint)
+        }
+
+    }
+}
+
+fun Canvas.drawLACLNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = colors[i].toColorInt()
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    paint.style = Paint.Style.STROKE
+    paint.strokeCap = Paint.Cap.ROUND
+    drawLACLNode(i, scale, paint)
+}
