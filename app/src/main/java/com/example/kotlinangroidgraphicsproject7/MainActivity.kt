4a7ce8bdb8c35ad3065f9384d1029fc8b2683afd
Anwesh43 <anweshthecool0@gmail.com>
2026-05-07 09:59:41 +0530

    linehorizarcleftview: adding view in activity

--- app/src/main/java/com/example/kotlinangroidgraphicsproject7/MainActivity.kt
+++ app/src/main/java/com/example/kotlinangroidgraphicsproject7/MainActivity.kt
@@ -12,12 +12,12 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.kotlinangroidgraphicsproject7.ui.theme.KotlinAngroidGraphicsProject7Theme
-import com.example.linearccloselineview.LineArcCloseLineView
+import com.example.linehorizarcleftview.LineHorizArcLeftView
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         //LineRotPartialArcView.create(this)
-        LineArcCloseLineView.create(this)
+        LineHorizArcLeftView.create(this)
     }
 }
