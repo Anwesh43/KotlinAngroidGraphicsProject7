c3727eaac8848774d2257706c4f38551206cbe1e
Anwesh43 <anweshthecool0@gmail.com>
2026-05-07 09:47:52 +0530

    linehorizarcleftview: adding LineHorizArcLeftView class

--- linehorizarcleftview/src/main/java/com/example/linehorizarcleftview/LineHorizArcLeftView.kt
+++ linehorizarcleftview/src/main/java/com/example/linehorizarcleftview/LineHorizArcLeftView.kt
@@ -58,3 +58,19 @@ fun Canvas.drawLHALNode(i : Int, scale : Float, paint : Paint) {
     paint.style = Paint.Style.STROKE
     drawLineHorizArcLeft(scale, w, h, paint)
 }
+
+class LineHorizArcLeftView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
