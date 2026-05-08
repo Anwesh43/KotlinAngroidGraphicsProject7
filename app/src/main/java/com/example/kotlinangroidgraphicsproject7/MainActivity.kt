1ae6d75403b8e46e6c7f89f5eaf6219eb02e6be2
Anwesh43 <anweshthecool0@gmail.com>
2026-05-08 07:46:58 +0530

    sqencloserlinerotview: adding view in activity

--- app/src/main/java/com/example/kotlinangroidgraphicsproject7/MainActivity.kt
+++ app/src/main/java/com/example/kotlinangroidgraphicsproject7/MainActivity.kt
@@ -12,12 +12,12 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.kotlinangroidgraphicsproject7.ui.theme.KotlinAngroidGraphicsProject7Theme
-import com.example.linehorizarcleftview.LineHorizArcLeftView
+import com.example.sqencloserlinerotview.SqEncloserLineRotView
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         //LineRotPartialArcView.create(this)
-        LineHorizArcLeftView.create(this)
+        SqEncloserLineRotView.create(this)
     }
 }
