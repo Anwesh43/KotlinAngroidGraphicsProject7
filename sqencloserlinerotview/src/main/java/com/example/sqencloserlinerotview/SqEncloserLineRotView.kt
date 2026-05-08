1e0941b3e9d890e40eb057a01a5a9753d1f75bda
Anwesh43 <anweshthecool0@gmail.com>
2026-05-08 07:30:37 +0530

    sqencloserlinerotview: adding SqEncloserLineRotView class

--- sqencloserlinerotview/src/main/java/com/example/sqencloserlinerotview/SqEncloserLineRotView.kt
+++ sqencloserlinerotview/src/main/java/com/example/sqencloserlinerotview/SqEncloserLineRotView.kt
@@ -71,3 +71,19 @@ fun Canvas.drawSELRNode(i : Int, scale : Float, paint : Paint) {
     paint.style = Paint.Style.STROKE
     drawSqEncloserLineRot(scale, w, h, paint)
 }
+
+class SqEncloserLineRotView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
