100e8ac2dda7250c6c6e16408af45421650edc7e
Anwesh43 <anweshthecool0@gmail.com>
2026-05-09 07:46:06 +0530

    linearcjoinrotview: adding Canvas extension methods

--- concarcrotjoinview/src/main/java/com/example/concarcrotjoinview/ConcArcRotJoinView.kt
+++ concarcrotjoinview/src/main/java/com/example/concarcrotjoinview/ConcArcRotJoinView.kt
@@ -21,9 +21,43 @@ val scGap : Float = 0.04f / parts
 val strokeFactor : Float = 90f
 val sizeFactor : Float = 5.9f
 val delay : Long = 20
-val rot : Float = 90f
+val rot : Float = -180f
+val sweep : Float = 180f
 val backColor : Int = "#BDBDBD".toColorInt()
 
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+
+fun Canvas.drawXY(x : Float, y : Float, cb : () -> Unit) {
+    save()
+    translate(x, y)
+    cb()
+    restore()
+}
+
+fun Canvas.drawConcArcRotJoin(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val dsc : (Int) -> Float = {
+        scale.divideScale(it, parts)
+    }
+    drawXY(w / 2 + (w / 2) * dsc(4), h / 2) {
+        rotate(rot * dsc(3))
+        for (j in 0..1) {
+            drawXY(-2 * size + size * j, 0f) {
+                rotate(rot * (1 - j) * dsc(2))
+                drawArc(RectF(0f, -size / 2, size, size / 2), -sweep * (1 - j) * dsc(j), sweep * dsc(j), false, paint)
+            }
+        }
+    }
+}
+
+fun Canvas.drawCARJNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = colors[i].toColorInt()
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    paint.style = Paint.Style.STROKE
+    drawConcArcRotJoin(scale, w, h, paint)
+}
