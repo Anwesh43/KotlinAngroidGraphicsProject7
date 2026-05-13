7c4245183bfc63e65bff0e7fdb2fe65f8cb58831
Anwesh43 <anweshthecool0@gmail.com>
2026-05-13 13:03:20 +0530

    quarterarcrothalfview: adding Canvas extension methods

--- quarterarcrothalfview/src/main/java/com/example/quarterarcrothalfview/QuarterArcRotHalfView.kt
+++ quarterarcrothalfview/src/main/java/com/example/quarterarcrothalfview/QuarterArcRotHalfView.kt
@@ -27,3 +27,36 @@ val strokeFactor : Float = 90f
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+
+fun Canvas.drawXY(x : Float, y : Float, cb : () -> Unit) {
+    save()
+    translate(x, y)
+    cb()
+    restore()
+}
+
+fun Canvas.drawQuarterArcRotHalf(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val dsc : (Int) -> Float = {
+        scale.divideScale(it, parts)
+    }
+    drawXY(w / 2, h / 2 - (h / 2) * dsc(5)) {
+        rotate(rot * dsc(4))
+        for (j in 0..1) {
+            drawXY((-w / 2 + size / 2 + (w - size) * j) * (1 - dsc(2)), 0f) {
+                rotate(rot * dsc(3) * j)
+                drawArc(RectF(-size / 2, -size / 2, size / 2, size / 2), -rot * (1 + j), rot * dsc(j), false, paint)
+            }
+        }
+    }
+}
+
+fun Canvas.drawQARHNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = colors[i].toColorInt()
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    paint.style = Paint.Style.STROKE
+    drawQuarterArcRotHalf(scale, w, h, paint)
+}
