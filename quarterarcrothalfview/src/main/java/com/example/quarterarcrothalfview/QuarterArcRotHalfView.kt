b376f78ed48ca5dabf5d5893e7c8032bf0c002b6
Anwesh43 <anweshthecool0@gmail.com>
2026-05-13 13:41:48 +0530

    quarterarcrothalfview: adding QARHNode class

--- quarterarcrothalfview/src/main/java/com/example/quarterarcrothalfview/QuarterArcRotHalfView.kt
+++ quarterarcrothalfview/src/main/java/com/example/quarterarcrothalfview/QuarterArcRotHalfView.kt
@@ -123,4 +123,45 @@ class QuarterArcRotHalfView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class QARHNode(var i : Int = 0, val state : State = State()) {
+
+        private var prev : QARHNode? = null
+        private var next : QARHNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = QARHNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawQARHNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : QARHNode {
+            var curr : QARHNode? = prev
+            if (dir === 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
