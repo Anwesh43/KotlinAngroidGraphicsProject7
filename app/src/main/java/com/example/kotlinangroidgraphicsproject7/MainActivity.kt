986645d2ff41b64a7f816ad371a17aab2a2bc8fa
Anwesh43 <anweshthecool0@gmail.com>
2026-05-15 10:53:06 +0530

    linebentrotdownview: adding view in activity

--- app/src/main/java/com/example/kotlinangroidgraphicsproject7/MainActivity.kt
+++ app/src/main/java/com/example/kotlinangroidgraphicsproject7/MainActivity.kt
@@ -12,12 +12,12 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.kotlinangroidgraphicsproject7.ui.theme.KotlinAngroidGraphicsProject7Theme
-import com.example.quarterarcrothalfview.QuarterArcRotHalfView
+import com.example.linebentrotdownview.LineBentRotDownView
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         //LineRotPartialArcView.create(this)
-        QuarterArcRotHalfView.create(this)
+        LineBentRotDownView.create(this)
     }
 }
