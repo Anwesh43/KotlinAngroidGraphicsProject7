936df5aeaf154560b080bab9a05ba200e35d9b24
Anwesh43 <anweshthecool0@gmail.com>
2026-05-16 10:40:24 +0530

    bilinearcrotleftview: adding BLARLNode class

--- bilinearcrotleftview/src/main/java/com/example/bilinearcrotleftview/BiLineArcRotLeftView.kt
+++ bilinearcrotleftview/src/main/java/com/example/bilinearcrotleftview/BiLineArcRotLeftView.kt
@@ -123,4 +123,45 @@ class BiLineArcRotLeftView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class BLARLNode(var i : Int = 0, val state : State = State()) {
+
+        private var next : BLARLNode? = null
+        private var prev : BLARLNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = BLARLNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawBLARNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : BLARLNode {
+            var curr : BLARLNode? = prev
+            if (dir === 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
