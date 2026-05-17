54c0d4dbaae703cb51a975c069420dab8d3223e9
Anwesh43 <anweshthecool0@gmail.com>
2026-05-17 09:32:15 +0530

    bisqencloserdownview: adding Canvas extension methods

--- bisqencloserdownview/src/main/java/com/example/bisqencloserdownview/BiSqEncloserDownView.kt
+++ bisqencloserdownview/src/main/java/com/example/bisqencloserdownview/BiSqEncloserDownView.kt
@@ -9,7 +9,7 @@ import android.graphics.Canvas
 import android.graphics.RectF
 import androidx.core.graphics.toColorInt
 
-val colorS : Array<String> = arrayOf(
+val colors : Array<String> = arrayOf(
     "#1A237E",
     "#EF5350",
     "#AA00FF",
@@ -27,3 +27,43 @@ val backColor : Int = "#BDBDBD".toColorInt()
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+
+fun Canvas.drawXY(x : Float, y : Float, cb : () -> Unit) {
+    save()
+    translate(x, y)
+    cb()
+    restore()
+}
+
+fun Canvas.drawBiSqEncloserDown(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float  = Math.min(w, h) / sizeFactor
+    val dsc : (Int) -> Float = {
+        scale.divideScale(it, parts)
+    }
+    drawXY(w / 2, h / 2 + (h / 2) * dsc(4)) {
+        rotate(rot * dsc(3))
+        for (j in 0..1) {
+            drawXY(0f, 0f) {
+                scale(1f - 2 * j, 1f)
+                drawXY(-w * 0.25f * (1 - dsc(2)), 0f) {
+                    drawXY(0f, -size) {
+                        drawLine(0f, 0f, -size * dsc(0), 0f, paint)
+                        drawXY(-size, 0f) {
+                            drawLine(0f, 0f, 0f, size * dsc(1), paint)
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+
+fun Canvas.drawBSEDNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = colors[i].toColorInt()
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    paint.style = Paint.Style.STROKE
+    drawBiSqEncloserDown(scale, w, h, paint)
+}
