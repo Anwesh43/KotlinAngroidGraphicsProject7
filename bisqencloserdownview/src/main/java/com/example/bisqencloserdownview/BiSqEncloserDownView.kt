f6747fef181a1b2d992b2f94564da2aeafd91c12
Anwesh43 <anweshthecool0@gmail.com>
2026-05-17 10:22:55 +0530

    bisqencloserdownview: adding BSEDNode class

--- bisqencloserdownview/src/main/java/com/example/bisqencloserdownview/BiSqEncloserDownView.kt
+++ bisqencloserdownview/src/main/java/com/example/bisqencloserdownview/BiSqEncloserDownView.kt
@@ -130,4 +130,45 @@ class BiSqEncloserDownView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class BSEDNode(var i : Int = 0, val state : State = State()) {
+
+        private var next : BSEDNode? = null
+        private var prev : BSEDNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = BSEDNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawBSEDNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : BSEDNode {
+            var curr : BSEDNode? = prev
+            if (dir === 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
