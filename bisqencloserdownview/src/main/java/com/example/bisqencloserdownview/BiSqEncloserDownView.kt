36e5a0622d68d9f5187a4fd2363cedddc1b15c1b
Anwesh43 <anweshthecool0@gmail.com>
2026-05-17 09:57:58 +0530

    bisqencloserdownview: adding BiSqEncloserDownView class

--- bisqencloserdownview/src/main/java/com/example/bisqencloserdownview/BiSqEncloserDownView.kt
+++ bisqencloserdownview/src/main/java/com/example/bisqencloserdownview/BiSqEncloserDownView.kt
@@ -67,3 +67,19 @@ fun Canvas.drawBSEDNode(i : Int, scale : Float, paint : Paint) {
     paint.style = Paint.Style.STROKE
     drawBiSqEncloserDown(scale, w, h, paint)
 }
+
+class BiSqEncloserDownView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
