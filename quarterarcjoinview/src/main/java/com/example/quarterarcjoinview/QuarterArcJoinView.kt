4318b38abb68fe55428042cd8da5368e0907f6ba
Anwesh43 <anweshthecool0@gmail.com>
2026-05-19 08:28:47 +0530

    quarterarcjoinview: adding Canvas extension methods

--- quarterarcjoinview/src/main/java/com/example/quarterarcjoinview/QuarterArcJoinView.kt
+++ quarterarcjoinview/src/main/java/com/example/quarterarcjoinview/QuarterArcJoinView.kt
@@ -27,3 +27,41 @@ val backColor : Int = "#BDBDBD".toColorInt()
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+
+fun Canvas.drawXY(x : Float, y : Float, cb : () -> Unit) {
+    save()
+    translate(x, y)
+    cb()
+    restore()
+}
+
+fun Canvas.drawQuarterArcJoin(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val dsc : (Int) -> Float = {
+        scale.divideScale(it, parts)
+    }
+    drawXY(w / 2, h / 2) {
+        for (j in 0..1) {
+            drawXY(0f, 0f) {
+                scale(1f - 2 * j, 1f)
+                drawXY((w / 4) * (1 - dsc(2)), 0f) {
+                    drawArc(RectF(-size, -size, size, size), 180f, 90f * dsc(0), false, paint)
+                    drawXY(0f, -size) {
+                        drawLine(0f, 0f, 0f, size * dsc(1), paint)
+                    }
+                }
+
+            }
+        }
+    }
+}
+
+fun Canvas.drawQAJNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = colors[i].toColorInt()
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    paint.style = Paint.Style.STROKE
+    drawQuarterArcJoin(scale, w, h, paint)
+}
