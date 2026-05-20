96ca95bb83642b05700966bba5ac0a5595c11e57
Anwesh43 <anweshthecool0@gmail.com>
2026-05-20 17:03:25 +0530

    arclineextenddownview: adding view in activity

--- app/src/main/java/com/example/kotlinangroidgraphicsproject7/MainActivity.kt
+++ app/src/main/java/com/example/kotlinangroidgraphicsproject7/MainActivity.kt
@@ -11,13 +11,13 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.example.arclineextenddownview.ArcLineExtendDownView
 import com.example.kotlinangroidgraphicsproject7.ui.theme.KotlinAngroidGraphicsProject7Theme
-import com.example.quarterarcjoinview.QuarterArcJoinView
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         //LineRotPartialArcView.create(this)
-        QuarterArcJoinView.create(this)
+        ArcLineExtendDownView.create(this)
     }
 }
