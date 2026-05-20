1f4147e603ab63f6d4342e46daaf14aacb848720
Anwesh43 <anweshthecool0@gmail.com>
2026-05-20 16:49:56 +0530

    arclineextenddownview: adding ALEDNode class

--- arclineextenddownview/src/main/java/com/example/arclineextenddownview/ArcLineExtendDownView.kt
+++ arclineextenddownview/src/main/java/com/example/arclineextenddownview/ArcLineExtendDownView.kt
@@ -122,4 +122,45 @@ class ArcLineExtendDownView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class ALEDNode(var i : Int = 0, val state : State = State()) {
+
+        private var next : ALEDNode? = null
+        private var prev : ALEDNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = ALEDNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawALEDNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : ALEDNode {
+            var curr : ALEDNode? = prev
+            if (dir === 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
