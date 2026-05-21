264d253cd285e137b7dce9a4728e988602601fb8
Anwesh43 <anweshthecool0@gmail.com>
2026-05-21 10:12:25 +0530

    endtomidarclineview: adding Canvas extension methods

--- endtomidarclineview/src/main/java/com/example/endtomidarclineview/EndToMidArcLineView.kt
+++ endtomidarclineview/src/main/java/com/example/endtomidarclineview/EndToMidArcLineView.kt
@@ -29,3 +29,36 @@ val sweepDeg : Float = 90f
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+
+fun Canvas.drawXY(x : Float, y : Float, cb : () -> Unit) {
+    save()
+    translate(x, y)
+    cb()
+    restore()
+}
+
+fun Canvas.drawEndToMidArcLine(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val dsc : (Int) -> Float = {
+        scale.divideScale(it, parts)
+    }
+    val dsk : (Int, Int) -> Float = {i, j ->
+        dsc(i).divideScale(j, 2)
+    }
+    drawXY(w / 2 - (w / 2) * dsc(4) , h / 2 - (h / 2) * dsc(4)) {
+        rotate(rot * dsc(3))
+        drawLine(0f, -h * 0.5f * (1 - dsk(0, 0)), 0f, -h / 2 + (h / 2 - size) * dsk(1, 0), paint)
+        drawLine(0f, w * 0.5f * (1 - dsk(0, 1)), 0f, w / 2 - (w / 2 - size) * dsk(1, 1), paint)
+        drawArc(RectF(-size, -size, size, size), startDeg, sweepDeg * dsc(2), false, paint)
+    }
+}
+
+fun Canvas.drawEMALNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = colors[i].toColorInt()
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    paint.style = Paint.Style.STROKE
+    drawEndToMidArcLine(scale, w, h, paint)
+}
