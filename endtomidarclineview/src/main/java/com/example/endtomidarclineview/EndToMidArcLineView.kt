e8af44fc000df656b9f251af36046eb911cb43e7
Anwesh43 <anweshthecool0@gmail.com>
2026-05-21 10:34:57 +0530

    endtomidarclineview: adding EndToMidArcLine class

--- endtomidarclineview/src/main/java/com/example/endtomidarclineview/EndToMidArcLineView.kt
+++ endtomidarclineview/src/main/java/com/example/endtomidarclineview/EndToMidArcLineView.kt
@@ -154,7 +154,7 @@ class EndToMidArcLineView(ctx : Context) : View(ctx) {
             state.startUpdating(cb)
         }
 
-        fun getNext(dir : Int, cb : () -> Float) : EMALNode {
+        fun getNext(dir : Int, cb : () -> Unit) : EMALNode {
             var curr : EMALNode? = prev
             if (dir === 1) {
                 curr = next
@@ -164,6 +164,29 @@ class EndToMidArcLineView(ctx : Context) : View(ctx) {
             }
             cb()
             return this
+        }
+    }
+
+    data class EndToMidArcLine(var i : Int) {
+
+        private var curr : EMALNode = EMALNode(0)
+        private var dir : Int = 1
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            curr.draw(canvas, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            curr.update {
+                curr = curr.getNext(dir) {
+                    dir *= -1
+                }
+                cb(it)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            curr.startUpdating(cb)
         }
     }
 }
