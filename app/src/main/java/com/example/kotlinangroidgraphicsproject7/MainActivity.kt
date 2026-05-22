c5c5ecc5b99ec126007add15a7c43ba7f02eda01
Anwesh43 <anweshthecool0@gmail.com>
2026-05-23 04:36:00 +0530

    quarterarcextendlineview: adding view in activity

--- app/src/main/java/com/example/kotlinangroidgraphicsproject7/MainActivity.kt
+++ app/src/main/java/com/example/kotlinangroidgraphicsproject7/MainActivity.kt
@@ -11,13 +11,13 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import com.example.biarcjoindownview.BiArcJoinDownView
 import com.example.kotlinangroidgraphicsproject7.ui.theme.KotlinAngroidGraphicsProject7Theme
+import com.example.quarterarcextendlineview.QuarterArcExtendLineView
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         //LineRotPartialArcView.create(this)
-        BiArcJoinDownView.create(this)
+        QuarterArcExtendLineView.create(this)
     }
 }
