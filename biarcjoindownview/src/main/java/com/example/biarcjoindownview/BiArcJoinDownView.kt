01ab60e73a02f8f7fcfe8698acbb6a0465bc7362
Anwesh43 <anweshthecool0@gmail.com>
2026-05-22 17:45:33 +0530

    biarcjoindownview: adding static create method

--- biarcjoindownview/src/main/java/com/example/biarcjoindownview/BiArcJoinDownView.kt
+++ biarcjoindownview/src/main/java/com/example/biarcjoindownview/BiArcJoinDownView.kt
@@ -218,4 +218,12 @@ class BiArcJoinDownView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    companion object {
+        fun create(activity : Activity) : BiArcJoinDownView {
+            val view : BiArcJoinDownView = BiArcJoinDownView(activity)
+            activity.setContentView(view)
+            return view
+        }
+    }
 }
