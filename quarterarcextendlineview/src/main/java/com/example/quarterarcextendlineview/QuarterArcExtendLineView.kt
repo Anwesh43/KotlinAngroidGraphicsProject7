519a65a0870f483aae34c3c9d2faff7dff37c1ec
Anwesh43 <anweshthecool0@gmail.com>
2026-05-23 04:13:46 +0530

    quarterarcextendlineview: adding QuarterAcExtendLineView class

--- quarterarcextendlineview/src/main/java/com/example/quarterarcextendlineview/QuarterArcExtendLineView.kt
+++ quarterarcextendlineview/src/main/java/com/example/quarterarcextendlineview/QuarterArcExtendLineView.kt
@@ -58,3 +58,19 @@ fun Canvas.drawQAELNode(i : Int, scale : Float, paint : Paint) {
     paint.style = Paint.Style.STROKE
     drawQuarterArcExtendLine(scale, w, h, paint)
 }
+
+class QuarterArcExtendLineView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
