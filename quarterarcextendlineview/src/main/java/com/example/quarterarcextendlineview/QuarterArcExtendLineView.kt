e37c2198a05306d2d06f806c28f0d830a35e12ed
Anwesh43 <anweshthecool0@gmail.com>
2026-05-23 04:32:08 +0530

    quarterarcextendlineview: adding Renderer class

--- quarterarcextendlineview/src/main/java/com/example/quarterarcextendlineview/QuarterArcExtendLineView.kt
+++ quarterarcextendlineview/src/main/java/com/example/quarterarcextendlineview/QuarterArcExtendLineView.kt
@@ -185,4 +185,27 @@ class QuarterArcExtendLineView(ctx : Context) : View(ctx) {
             curr.startUpdating(cb)
         }
     }
+
+    data class Renderer(var view : QuarterArcExtendLineView, var animated : Boolean = false) {
+
+        private val animator : Animator = Animator(view)
+        private val paint : Paint = Paint(Paint.ANTI_ALIAS_FLAG)
+        private val qael : QuarterArcExtendLine = QuarterArcExtendLine(0)
+
+        fun render(canvas : Canvas) {
+            canvas.drawColor(backColor)
+            qael.draw(canvas, paint)
+            animator.animate {
+                qael.update {
+                    animator.stop()
+                }
+            }
+        }
+
+        fun handleTap() {
+            qael.startUpdating {
+                animator.start()
+            }
+        }
+    }
 }
