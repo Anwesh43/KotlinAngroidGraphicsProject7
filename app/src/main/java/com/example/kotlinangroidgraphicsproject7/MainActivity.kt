985a1e0abe288fe1a7e780034d10a7278fbe89d0
Anwesh43 <anweshthecool0@gmail.com>
2026-05-22 17:46:43 +0530

    biarcjoindownview: adding view in activity

--- app/src/main/java/com/example/kotlinangroidgraphicsproject7/MainActivity.kt
+++ app/src/main/java/com/example/kotlinangroidgraphicsproject7/MainActivity.kt
@@ -11,13 +11,13 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import com.example.endtomidarclineview.EndToMidArcLineView
+import com.example.biarcjoindownview.BiArcJoinDownView
 import com.example.kotlinangroidgraphicsproject7.ui.theme.KotlinAngroidGraphicsProject7Theme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         //LineRotPartialArcView.create(this)
-        EndToMidArcLineView.create(this)
+        BiArcJoinDownView.create(this)
     }
 }
