feaa08decfb745bdd81a6ac66fa8a01a5107e767
Anwesh43 <anweshthecool0@gmail.com>
2026-05-26 08:31:08 +0530

    linejoinarcupview: adding LJAUNode class

--- linejoinarcupview/src/main/java/com/example/linejoinarcupview/LineJoinArcUpView.kt
+++ linejoinarcupview/src/main/java/com/example/linejoinarcupview/LineJoinArcUpView.kt
@@ -121,4 +121,45 @@ class LineJoinArcUpView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class LJAUNode(var i : Int = 0, val state : State = State()) {
+
+        private var next : LJAUNode? = null
+        private var prev : LJAUNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = LJAUNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawLJAUNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : LJAUNode {
+            var curr : LJAUNode? = prev
+            if (dir === 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
