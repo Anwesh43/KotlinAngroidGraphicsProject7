1c8034b6895c7d4352a9d6d260b34665ad7718b8
Anwesh43 <anweshthecool0@gmail.com>
2026-05-26 08:32:31 +0530

    linejoinarcupview: adding LineJoinArcUp class

--- linejoinarcupview/src/main/java/com/example/linejoinarcupview/LineJoinArcUpView.kt
+++ linejoinarcupview/src/main/java/com/example/linejoinarcupview/LineJoinArcUpView.kt
@@ -162,4 +162,27 @@ class LineJoinArcUpView(ctx : Context) : View(ctx) {
             return this
         }
     }
+
+    data class LineJoinArcUp(var i : Int) {
+
+        private var curr : LJAUNode = LJAUNode(0)
+        private var dir : Int = 1
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            curr.draw(canvas, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            curr.update {
+                curr = curr.getNext(dir) {
+                    dir *= -1
+                }
+                cb(it)
+            }
+        }
+
+        fun startUdpating(cb : () -> Unit) {
+            curr.startUpdating(cb)
+        }
+    }
 }
