4ffacbaea7cb7e4c630d1d1eab89db1738bbde4b
Anwesh43 <anweshthecool0@gmail.com>
2026-05-27 11:20:52 +0530

    rightlineencloserview: adding fix

--- rightlineencloserview/src/main/java/com/example/rightlineencloserview/RightEncloserLineView.kt
+++ rightlineencloserview/src/main/java/com/example/rightlineencloserview/RightEncloserLineView.kt
@@ -40,7 +40,7 @@ fun Canvas.drawRightEncloserLine(scale : Float, w : Float, h : Float, paint : Pa
     val dsc : (Int) -> Float = {
         scale.divideScale(it, parts)
     }
-    drawXY(w / 2, h / 2) {
+    drawXY(w / 2 + (w / 2) * dsc(4), h / 2) {
         rotate(rot * dsc(2))
         drawXY(0f, -size) {
             drawLine(0f, 0f, size * dsc(0), 0f, paint)
