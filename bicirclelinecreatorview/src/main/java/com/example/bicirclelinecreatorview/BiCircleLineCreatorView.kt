d74cb2c285b5342af72aa969e84405d2c8673bae
Anwesh43 <anweshthecool0@gmail.com>
2026-05-28 10:32:29 +0530

    bircirclelinecreatorview: adding Canvas extension methods

--- bicirclelinecreatorview/src/main/java/com/example/bicirclelinecreatorview/BiCircleLineCreatorView.kt
+++ bicirclelinecreatorview/src/main/java/com/example/bicirclelinecreatorview/BiCircleLineCreatorView.kt
@@ -16,7 +16,7 @@ val colors : Array<String> = arrayOf(
     "#C51162",
     "#00C853"
 )
-val parts : Int = 5
+val parts : Int = 4
 val scGap : Float = 0.04f/ parts
 val rot : Float = 90f
 val sizeFactor : Float = 5.9f
@@ -27,3 +27,38 @@ val delay : Long = 20
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+
+fun Canvas.drawXY(x : Float, y : Float, cb : () -> Unit) {
+    save()
+    translate(x, y)
+    cb()
+    restore()
+}
+
+fun Canvas.drawBiCircleLineCreator(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val dsc : (Int) -> Float = {
+        scale.divideScale(it, parts)
+    }
+    drawXY(w / 2, h / 2) {
+        for (j in 0..1) {
+            drawXY(0f, h * 0.5f * dsc(3) * (1f - 2 * j)) {
+                rotate(rot * dsc(2))
+                drawArc(RectF(-size, -size, size, size), 0f, 180f * dsc(0), false, paint)
+                drawXY(0f, size) {
+                    drawLine(0f, 0f, 0f, -size * 0.5f * dsc(1), paint)
+                }
+            }
+        }
+    }
+}
+
+fun Canvas.drawBCLCNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = colors[i].toColorInt()
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    paint.style = Paint.Style.STROKE
+    drawBiCircleLineCreator(scale, w, h, paint)
+}
