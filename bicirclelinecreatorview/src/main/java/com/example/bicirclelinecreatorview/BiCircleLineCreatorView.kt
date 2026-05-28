003a6b7074af0dc8ee35a39dd5cebf9848b2722b
Anwesh43 <anweshthecool0@gmail.com>
2026-05-28 11:04:52 +0530

    bircirclelinecreatorview: adding BiCircleLineCreatorView class

--- bicirclelinecreatorview/src/main/java/com/example/bicirclelinecreatorview/BiCircleLineCreatorView.kt
+++ bicirclelinecreatorview/src/main/java/com/example/bicirclelinecreatorview/BiCircleLineCreatorView.kt
@@ -62,3 +62,19 @@ fun Canvas.drawBCLCNode(i : Int, scale : Float, paint : Paint) {
     paint.style = Paint.Style.STROKE
     drawBiCircleLineCreator(scale, w, h, paint)
 }
+
+class BiCircleLineCreatorView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
