6ad5a981a5e3a1a1a22d82426a96fbde17d4aa45
Anwesh43 <anweshthecool0@gmail.com>
2026-05-29 13:03:00 +0530

    linejoinarcrightview: adding LineJoinArcRightView class

--- linejoinarcrightview/src/main/java/com/example/linejoinarcrightview/LineJoinArcRightView.kt
+++ linejoinarcrightview/src/main/java/com/example/linejoinarcrightview/LineJoinArcRightView.kt
@@ -64,3 +64,19 @@ fun Canvas.drawLJARNode(i : Int, scale : Float, paint : Paint) {
     paint.strokeWidth = Math.min(w, h) / strokeFactor
     drawLineJoinArcRight(scale, w, h, paint)
 }
+
+class LineJoinArcRightView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
