d3f4fde539bbfc81d7f58821f847fac3e0d5d6bf
Anwesh43 <anweshthecool0@gmail.com>
2026-05-29 15:18:17 +0530

    linejoinarcrightview: adding view in activity

--- app/src/main/java/com/example/kotlinangroidgraphicsproject7/MainActivity.kt
+++ app/src/main/java/com/example/kotlinangroidgraphicsproject7/MainActivity.kt
@@ -11,13 +11,13 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import com.example.bicirclelinecreatorview.BiCircleLineCreatorView
 import com.example.kotlinangroidgraphicsproject7.ui.theme.KotlinAngroidGraphicsProject7Theme
+import com.example.linejoinarcrightview.LineJoinArcRightView
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         //LineRotPartialArcView.create(this)
-        BiCircleLineCreatorView.create(this)
+        LineJoinArcRightView.create(this)
     }
 }
