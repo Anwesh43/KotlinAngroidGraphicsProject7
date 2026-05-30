2ab5a880a82af1d135e6c399a7b77b1a6402f15d
Anwesh43 <anweshthecool0@gmail.com>
2026-05-30 10:16:14 +0530

    bilinebentarcview: adding BiLIneBentArc class

--- bilinebentarcview/src/main/java/com/example/bilinebentarcview/BiLineBentArcView.kt
+++ bilinebentarcview/src/main/java/com/example/bilinebentarcview/BiLineBentArcView.kt
@@ -175,4 +175,27 @@ class BiLineBentArcView(ctx : Context) : View(ctx) {
             return this
         }
     }
+
+    data class BiLineBentArc(var i : Int) {
+
+        private var curr : BLBANode = BLBANode(0)
+        private var dir : Int = 1
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            curr.draw(canvas, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            curr.update {
+                curr = curr.getNext(dir) {
+                    dir *= -1
+                }
+                cb(it)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            curr.startUpdating(cb)
+        }
+    }
 }
