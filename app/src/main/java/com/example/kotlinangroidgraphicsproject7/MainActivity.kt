1050471e9b0c6e7eb511f357b62cdb474c6e705b
Anwesh43 <anweshthecool0@gmail.com>
2026-05-31 14:07:15 +0530

    linebntarcrightview: adding module in build

--- app/src/main/java/com/example/kotlinangroidgraphicsproject7/MainActivity.kt
+++ app/src/main/java/com/example/kotlinangroidgraphicsproject7/MainActivity.kt
@@ -11,13 +11,12 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import com.example.bilinebentarcview.BiLineBentArcView
 import com.example.kotlinangroidgraphicsproject7.ui.theme.KotlinAngroidGraphicsProject7Theme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         //LineRotPartialArcView.create(this)
-        BiLineBentArcView.create(this)
+
     }
 }
