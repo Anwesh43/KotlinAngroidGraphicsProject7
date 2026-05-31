991eb6215b3106c4120e23d38343f86d8cf15986
Anwesh43 <anweshthecool0@gmail.com>
2026-05-31 14:34:51 +0530

    linebntarcrightview: adding canvas extension methods

--- linebentarcrightview/src/main/java/com/example/linebentarcrightview/LineBentArcRightView.kt
+++ linebentarcrightview/src/main/java/com/example/linebentarcrightview/LineBentArcRightView.kt
@@ -27,3 +27,46 @@ val rot : Float = 270f
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+
+fun Canvas.drawXY(x : Float, y : Float, cb : () -> Unit) {
+    save()
+    translate(x, y)
+    cb()
+    restore()
+}
+
+fun Canvas.drawLineWithoutDot(x1 : Float, y1 : Float, x2 : Float, y2 : Float, paint : Paint) {
+    if (Math.abs(x1 - x2) < 0.1 && Math.abs(y1 - y2) < 0.1) {
+        return
+    }
+    drawLine(x1, y1, x2, y2, paint)
+}
+
+fun Canvas.drawLineBentArcRight(w : Float, h : Float, scale : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val dsc : (Int) -> Float = {
+        scale.divideScale(it, parts)
+    }
+    drawXY(w / 2 + (w / 2) * dsc(4), h  / 2) {
+        drawXY(0f, -size) {
+            drawXY(0f, 0f) {
+                rotate(rot * dsc(2))
+                drawLine(0f, 0f, size * dsc(0), 0f, paint)
+            }
+            drawXY(size, -size) {
+                drawLine(0f, 0f, -size * dsc(1), size * dsc(1), paint)
+            }
+        }
+        drawArc(RectF(0f, -size / 2, size, size / 2), 0f, 180f * dsc(3), false, paint)
+    }
+}
+
+fun Canvas.drawLBARNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = colors[i].toColorInt()
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    paint.style = Paint.Style.STROKE
+    drawLineBentArcRight(w, h, scale, paint)
+}
