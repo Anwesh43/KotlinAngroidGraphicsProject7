7622378df8cc589a2089c775ca55b85aeba15a47
Anwesh43 <anweshthecool0@gmail.com>
2026-06-01 07:36:53 +0530

    bentlinerightview: adding BentLineRightView class

--- bentlinerightview/src/main/java/com/example/bentlinerightview/BentLineRightView.kt
+++ bentlinerightview/src/main/java/com/example/bentlinerightview/BentLineRightView.kt
@@ -62,3 +62,19 @@ fun Canvas.drawBLRNode(i : Int, scale : Float, paint : Paint) {
     paint.strokeCap = Paint.Cap.ROUND
     drawBentLineRight(scale, w, h, paint)
 }
+
+class BentLineRightView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
