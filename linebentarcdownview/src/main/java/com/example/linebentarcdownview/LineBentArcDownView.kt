62aa92a7b4313db06f797c19a14f128bc4d15dda
Anwesh43 <anweshthecool0@gmail.com>
2026-06-02 07:35:22 +0530

    linebentarcdownview: adding Canvas extension methods

--- linebentarcdownview/src/main/java/com/example/linebentarcdownview/LineBentArcDownView.kt
+++ linebentarcdownview/src/main/java/com/example/linebentarcdownview/LineBentArcDownView.kt
@@ -23,8 +23,42 @@ val sizeFactor : Float = 5.9f
 val delay : Long = 20
 val backColor : Int = "#BDBDBD".toColorInt()
 val rot : Float = 90f
-val startDeg : Float = 45f
+val startDeg : Float = rot / 2
 
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+
+fun Canvas.drawXY(x : Float, y : Float, cb : () -> Unit) {
+    save()
+    translate(x, y)
+    cb()
+    restore()
+}
+
+fun Canvas.drawLineBentArcDown(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val dsc : (Int) -> Float = {
+        scale.divideScale(it, parts)
+    }
+    drawXY(w / 2, h / 2 + (h / 2) * dsc(4)) {
+        drawXY(0f, 0f) {
+            rotate(startDeg - rot * dsc(1))
+            drawLine(0f, 0f, -size * dsc(0), 0f, paint)
+        }
+        drawArc(RectF(-size, -size, size, size), rot + startDeg * (1 - dsc(2)), startDeg * dsc(2), false, paint)
+        drawXY(0f, size) {
+            drawLine(0f, 0f, 0f, -size * dsc(3), paint)
+        }
+    }
+}
+
+fun Canvas.drawLBADNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = colors[i].toColorInt()
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    paint.style = Paint.Style.STROKE
+    drawLineBentArcDown(scale, w, h, paint)
+}
