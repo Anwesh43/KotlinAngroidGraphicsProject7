f3be922b4c98115b04bbbf46832d54ec3557bfcf
Anwesh43 <anweshthecool0@gmail.com>
2026-06-04 18:22:41 +0530

    bilineperpleftview: adding Canvas extension methods

--- bilineperpleftview/src/main/java/com/example/bilineperpleftview/BiLinePerpLeftView.kt
+++ bilineperpleftview/src/main/java/com/example/bilineperpleftview/BiLinePerpLeftView.kt
@@ -30,4 +30,43 @@ fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
 
+fun Canvas.drawXY(x : Float, y : Float, cb : () -> Unit) {
+    save()
+    translate(x, y)
+    cb()
+    restore()
+}
+
+fun Canvas.scaleXY(sx : Float, sy : Float, cb : () -> Unit) {
+    drawXY(0f, 0f) {
+        scale(sx, sy)
+        cb()
+    }
+}
+
+fun Canvas.drawBiLinePerpLeft(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val dsc : (Int) -> Float = {
+        scale.divideScale(it, parts)
+    }
+    drawXY(w / 2, h / 2) {
+        for (j in 0..1) {
+            drawXY(0f, h * 0.5f * (1 - 2 * j) * (1 - dsc(2 * j))) {
+                rotate(rot * dsc(2 * j + 1))
+                drawLine(0f, 0f, 0f, size * (1f - 2 * j), paint)
+            }
+        }
+        drawArc(RectF(-size, -size, size, size), start, rot * 2, false, paint)
+    }
+}
+
+fun Canvas.drawBLPLNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = colors[i].toColorInt()
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    paint.style = Paint.Style.STROKE
+    drawBiLinePerpLeft(scale, w, h, paint)
+}
 
