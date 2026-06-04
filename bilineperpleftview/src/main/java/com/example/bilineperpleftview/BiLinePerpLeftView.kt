67adfe35f18c6e87aa313ae2b2ff2a410640d0fc
Anwesh43 <anweshthecool0@gmail.com>
2026-06-04 18:28:38 +0530

    bilineperpleftview: adding BiLinePerpLeftView class

--- bilineperpleftview/src/main/java/com/example/bilineperpleftview/BiLinePerpLeftView.kt
+++ bilineperpleftview/src/main/java/com/example/bilineperpleftview/BiLinePerpLeftView.kt
@@ -70,3 +70,18 @@ fun Canvas.drawBLPLNode(i : Int, scale : Float, paint : Paint) {
     drawBiLinePerpLeft(scale, w, h, paint)
 }
 
+class BiLinePerpLeftView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
