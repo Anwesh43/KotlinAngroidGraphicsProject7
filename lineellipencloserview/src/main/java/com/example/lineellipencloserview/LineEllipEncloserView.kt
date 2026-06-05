16a27b1620691faa205820c5f4fb3334bfd97709
Anwesh43 <anweshthecool0@gmail.com>
2026-06-05 11:50:20 +0530

    lineellipencloserview: adding LineEllipEncloserView class

--- lineellipencloserview/src/main/java/com/example/lineellipencloserview/LineEllipEncloserView.kt
+++ lineellipencloserview/src/main/java/com/example/lineellipencloserview/LineEllipEncloserView.kt
@@ -61,3 +61,19 @@ fun Canvas.drawLEENode(i : Int, scale : Float, paint : Paint) {
     paint.style = Paint.Style.STROKE
     drawLineEllipEncloser(scale, w, h, paint)
 }
+
+class LineEllipEncloserView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
