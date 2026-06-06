9d92156c7840526262274003d0a552ddb2977747
Anwesh43 <anweshthecool0@gmail.com>
2026-06-06 12:06:20 +0530

    arcextendlineleftview: adding AELLNode class

--- arcextendlineleftview/src/main/java/com/example/arcextendlineleftview/ArcExtendLineLeftView.kt
+++ arcextendlineleftview/src/main/java/com/example/arcextendlineleftview/ArcExtendLineLeftView.kt
@@ -122,4 +122,43 @@ class ArcExtendLineLeftView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class AELLNode(var i : Int = 0, val state : State = State()) {
+
+        private var next : AELLNode? = null
+        private var prev : AELLNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            next = AELLNode(i + 1)
+            next?.prev = this
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawAELLNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.update(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : AELLNode {
+            var curr : AELLNode? = prev
+            if (dir === 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
