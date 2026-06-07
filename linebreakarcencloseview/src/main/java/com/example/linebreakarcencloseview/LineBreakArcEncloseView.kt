54ba0ca17387966803c451411fce811fdeeecb60
Anwesh43 <anweshthecool0@gmail.com>
2026-06-07 11:23:09 +0530

    linebreakarcencloseview: adding static create method

--- linebreakarcencloseview/src/main/java/com/example/linebreakarcencloseview/LineBreakArcEncloseView.kt
+++ linebreakarcencloseview/src/main/java/com/example/linebreakarcencloseview/LineBreakArcEncloseView.kt
@@ -215,4 +215,12 @@ class LineBreakArcEncloseView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    companion object {
+        fun create(activity: Activity) : LineBreakArcEncloseView {
+            val view : LineBreakArcEncloseView = LineBreakArcEncloseView(activity)
+            activity.setContentView(view)
+            return view
+        }
+    }
 }
