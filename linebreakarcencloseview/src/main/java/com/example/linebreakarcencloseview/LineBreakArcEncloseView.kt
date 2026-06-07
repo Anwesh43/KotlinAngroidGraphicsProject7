be46ef8e69f5941336c604c079b0028787b99edb
Anwesh43 <anweshthecool0@gmail.com>
2026-06-07 10:49:44 +0530

    linebreakarcencloseview: adding LBAENode class

--- linebreakarcencloseview/src/main/java/com/example/linebreakarcencloseview/LineBreakArcEncloseView.kt
+++ linebreakarcencloseview/src/main/java/com/example/linebreakarcencloseview/LineBreakArcEncloseView.kt
@@ -126,4 +126,45 @@ class LineBreakArcEncloseView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class LBAENode(var i : Int = 0, val state : State = State()) {
+
+        private var next : LBAENode? = null
+        private var prev : LBAENode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = LBAENode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawLBAENode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : LBAENode {
+            var curr : LBAENode? = prev
+            if (dir === 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
