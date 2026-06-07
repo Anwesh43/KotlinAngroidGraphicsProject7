8029dc262a2377f752b9578687f2f9c062dc6301
Anwesh43 <anweshthecool0@gmail.com>
2026-06-07 10:38:49 +0530

    linebreakarcencloseview: adding LineBreakArcEncloseView class

--- linebreakarcencloseview/src/main/java/com/example/linebreakarcencloseview/LineBreakArcEncloseView.kt
+++ linebreakarcencloseview/src/main/java/com/example/linebreakarcencloseview/LineBreakArcEncloseView.kt
@@ -63,3 +63,19 @@ fun Canvas.drawLBAENode(i : Int, scale : Float, paint : Paint) {
     paint.style = Paint.Style.STROKE
     drawLineBreakArcEnclose(scale, w, h, paint)
 }
+
+class LineBreakArcEncloseView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
