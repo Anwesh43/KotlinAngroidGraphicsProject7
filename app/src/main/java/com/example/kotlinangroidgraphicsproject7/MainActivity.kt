0fb6bdd9fc0d2533861cf8ed8b554262629d59a8
Anwesh43 <anweshthecool0@gmail.com>
2026-06-09 08:53:20 +0530

    linearcrotrightview: adding view in activity

--- app/src/main/java/com/example/kotlinangroidgraphicsproject7/MainActivity.kt
+++ app/src/main/java/com/example/kotlinangroidgraphicsproject7/MainActivity.kt
@@ -12,12 +12,12 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.kotlinangroidgraphicsproject7.ui.theme.KotlinAngroidGraphicsProject7Theme
-import com.example.linebreakarcencloseview.LineBreakArcEncloseView
+import com.example.linearcrotrightview.LineArcRotRightView
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         //LineRotPartialArcView.create(this)
-        LineBreakArcEncloseView.create(this)
+        LineArcRotRightView.create(this)
     }
 }
