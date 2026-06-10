e2bdde9561bd3a15b8d831158c0800cbda7dd706
Anwesh43 <anweshthecool0@gmail.com>
2026-06-10 10:48:02 +0530

    bilinearcdownview: adding BiLineArcDownView class

--- bilinearcdownview/src/main/java/com/example/bilinearcdownview/BiLineArcDownView.kt
+++ bilinearcdownview/src/main/java/com/example/bilinearcdownview/BiLineArcDownView.kt
@@ -60,3 +60,19 @@ fun Canvas.drawBLADNode(i : Int, scale : Float, paint : Paint) {
     paint.style = Paint.Style.STROKE
     drawBiLineArcDown(scale, w, h, paint)
 }
+
+class BiLineArcDownView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
