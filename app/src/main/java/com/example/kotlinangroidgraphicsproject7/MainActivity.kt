c379acbbcab9fd82677faab3530a70ee378a7474
Anwesh43 <anweshthecool0@gmail.com>
2026-06-13 20:22:17 +0530

    rightlinearcencloseview: adding view in activity

--- app/src/main/java/com/example/kotlinangroidgraphicsproject7/MainActivity.kt
+++ app/src/main/java/com/example/kotlinangroidgraphicsproject7/MainActivity.kt
@@ -11,13 +11,13 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import com.example.bilinearcdownview.BiLineArcDownView
 import com.example.kotlinangroidgraphicsproject7.ui.theme.KotlinAngroidGraphicsProject7Theme
+import com.example.rightlinearcencloseview.RightLineArcEncloseView
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         //LineRotPartialArcView.create(this)
-        BiLineArcDownView.create(this)
+        RightLineArcEncloseView.create(this)
     }
 }
