3cf439c625e04a8cc4df5f8756a970c6472ed698
Anwesh43 <anweshthecool0@gmail.com>
2026-06-13 19:44:56 +0530

    rightlinearcencloseview: adding Canavas extension methods

--- rightlinearcencloseview/src/main/java/com/example/rightlinearcencloseview/RightLineArcEncloseView.kt
+++ rightlinearcencloseview/src/main/java/com/example/rightlinearcencloseview/RightLineArcEncloseView.kt
@@ -27,3 +27,37 @@ val rot : Float = 180f
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+
+fun Canvas.drawXY(x : Float, y : Float, cb : () -> Unit) {
+    save()
+    translate(x, y)
+    cb()
+    restore()
+}
+
+fun Canvas.drawRightLineArcEncloser(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val dsc : (Int) -> Float = {
+        scale.divideScale(it, parts)
+    }
+    drawXY(w / 2 - (w / 2) * dsc(4), h / 2 + (h / 2) * dsc(4)) {
+        rotate(rot * dsc(3))
+        drawXY(0f, -h * 0.5f * (1 - dsc(0))) {
+            drawLine(0f, 0f, 0f, -size, paint)
+        }
+        drawXY((w / 2) * (1 - dsc(1)), 0f) {
+            drawLine(0f, 0f, size, 0f, paint)
+        }
+        drawArc(RectF(0f, -2 * size, 2 * size, 0f), 90f, 90f * dsc(2), false, paint)
+    }
+}
+
+fun Canvas.drawRLAENode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.style = Paint.Style.STROKE
+    paint.color = colors[i].toColorInt()
+    drawRightLineArcEncloser(scale, w, h, paint)
+}
