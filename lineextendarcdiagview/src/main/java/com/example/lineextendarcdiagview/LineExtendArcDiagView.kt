03ca2c16cee8a8d54d1f73e727c774ecc3d21d66
Anwesh43 <anweshthecool0@gmail.com>
2026-06-14 13:30:16 +0530

    lineextendarcdiagview: adding LineExtendArcDiagView class

--- lineextendarcdiagview/src/main/java/com/example/lineextendarcdiagview/LineExtendArcDiagView.kt
+++ lineextendarcdiagview/src/main/java/com/example/lineextendarcdiagview/LineExtendArcDiagView.kt
@@ -65,3 +65,19 @@ fun Canvas.drawLEADNode(i : Int, scale : Float, paint : Paint) {
     paint.style = Paint.Style.STROKE
     drawLineExtendArcDiag(scale, w, h, paint)
 }
+
+class LineExtendArcDiagView(ctx : Context): View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+        }
+        }
+        return true
+    }
+}
