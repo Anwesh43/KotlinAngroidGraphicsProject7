5a75d288df3bf0fece75680fc7bf05a07b535213
Anwesh43 <anweshthecool0@gmail.com>
2026-06-14 13:29:06 +0530

    lineextendarcdiagview: adding Canas extensoion methods

--- lineextendarcdiagview/src/main/java/com/example/lineextendarcdiagview/LineExtendArcDiagView.kt
+++ lineextendarcdiagview/src/main/java/com/example/lineextendarcdiagview/LineExtendArcDiagView.kt
@@ -27,3 +27,41 @@ val rot : Float = 90f
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+
+fun Canvas.drawXY(x : Float, y : Float, cb : () -> Unit) {
+    save()
+    translate(x, y)
+    cb()
+    restore()
+}
+
+fun Canvas.drawLineExtendArcDiag(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val dsc : (Int) -> Float = {
+        scale.divideScale(it, parts)
+    }
+    drawXY(w / 2, h / 2 - (h / 2) * dsc(4)) {
+        rotate(rot * dsc(3))
+        drawXY(-size, 0f) {
+            drawLine(0f, 0f, -size * dsc(0), size * dsc(0), paint)
+        }
+        drawXY(0f, -size) {
+            drawLine(0f, 0f, 0f, size * dsc(1), paint)
+        }
+        for (j in 0..1) {
+            drawXY(-size * 0.5f * j, 0f) {
+                drawArc(RectF(-size / 2, -size / 4, size / 4, 0f), 0f, 180f * dsc(2).divideScale(j, 2), false, paint)
+            }
+        }
+    }
+}
+
+fun Canvas.drawLEADNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = colors[i].toColorInt()
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    paint.style = Paint.Style.STROKE
+    drawLineExtendArcDiag(scale, w, h, paint)
+}
