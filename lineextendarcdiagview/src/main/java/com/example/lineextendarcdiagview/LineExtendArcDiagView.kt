a1b4bb6d0cc0938edbfb164f607e0042c18c74ce
Anwesh43 <anweshthecool0@gmail.com>
2026-06-14 13:16:05 +0530

    lineextendarcdiagview: adding contants and libraries

---
+++ lineextendarcdiagview/src/main/java/com/example/lineextendarcdiagview/LineExtendArcDiagView.kt
@@ -0,0 +1,25 @@
+package com.example.lineextendarcdiagview
+
+import android.view.View
+import android.view.MotionEvent
+import android.content.Context
+import android.app.Activity
+import android.graphics.Paint
+import android.graphics.Canvas
+import android.graphics.RectF
+import androidx.core.graphics.toColorInt
+
+val colors : Array<String> = arrayOf(
+    "#1A237E",
+    "#EF5350",
+    "#AA00FF",
+    "#C51162",
+    "#00C853"
+)
+val parts : Int = 5
+val scGap : Float= 0.05f
+val delay : Long = 20
+val backColor : Int = "#BDBDBD".toColorInt()
+val sizeFactor : Float = 5.9f
+val strokeFactor : Float = 90f
+val rot : Float = 90f
