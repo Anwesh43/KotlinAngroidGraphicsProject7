838f527c310a4277e9e606a3f1b06f58a0934d38
Anwesh43 <anweshthecool0@gmail.com>
2026-06-14 14:13:34 +0530

    lineextendarcdiagview: adding view in activity

--- app/src/main/java/com/example/kotlinangroidgraphicsproject7/MainActivity.kt
+++ app/src/main/java/com/example/kotlinangroidgraphicsproject7/MainActivity.kt
@@ -12,12 +12,12 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.kotlinangroidgraphicsproject7.ui.theme.KotlinAngroidGraphicsProject7Theme
-import com.example.rightlinearcencloseview.RightLineArcEncloseView
+import com.example.lineextendarcdiagview.LineExtendArcDiagView
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         //LineRotPartialArcView.create(this)
-        RightLineArcEncloseView.create(this)
+        LineExtendArcDiagView.create(this)
     }
 }
