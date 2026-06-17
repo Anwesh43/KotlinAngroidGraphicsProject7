54040ecbe3c412fbacc57350a5db3a6866baaf4f
Anwesh43 <anweshthecool0@gmail.com>
2026-06-17 13:23:24 +0530

    lineconcellipview: adding static create method

--- lineconcellipview/src/main/java/com/example/lineconcellipview/LineConcEllipView.kt
+++ lineconcellipview/src/main/java/com/example/lineconcellipview/LineConcEllipView.kt
@@ -211,4 +211,13 @@ class LineConcEllipView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    companion object {
+
+        fun create(activity: Activity) : LineConcEllipView {
+            val view : LineConcEllipView = LineConcEllipView(activity)
+            activity.setContentView(view)
+            return view
+        }
+    }
 }
