e940048c999d2e0f046fa017f6a634828e38b59d
Anwesh43 <anweshthecool0@gmail.com>
2026-06-17 13:24:09 +0530

    lineconcellipview: adding view in activity

--- app/src/main/java/com/example/kotlinangroidgraphicsproject7/MainActivity.kt
+++ app/src/main/java/com/example/kotlinangroidgraphicsproject7/MainActivity.kt
@@ -12,12 +12,12 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.kotlinangroidgraphicsproject7.ui.theme.KotlinAngroidGraphicsProject7Theme
-import com.example.lineextendarcdiagview.LineExtendArcDiagView
+import com.example.lineconcellipview.LineConcEllipView
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         //LineRotPartialArcView.create(this)
-        LineExtendArcDiagView.create(this)
+        LineConcEllipView.create(this)
     }
 }
