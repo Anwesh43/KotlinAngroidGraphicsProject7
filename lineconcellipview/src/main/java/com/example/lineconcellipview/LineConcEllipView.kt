a126ae333805070511b7d1cef4b0a9275ca27b14
Anwesh43 <anweshthecool0@gmail.com>
2026-06-17 13:14:55 +0530

    lineconcellipview: adding LCENode class

--- lineconcellipview/src/main/java/com/example/lineconcellipview/LineConcEllipView.kt
+++ lineconcellipview/src/main/java/com/example/lineconcellipview/LineConcEllipView.kt
@@ -122,4 +122,45 @@ class LineConcEllipView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class LCENode(var i : Int = 0, val state : State = State()) {
+
+        private var prev : LCENode? = null
+        private var next : LCENode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = LCENode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawLCENode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : LCENode {
+            var curr : LCENode? = prev
+            if (dir === 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
