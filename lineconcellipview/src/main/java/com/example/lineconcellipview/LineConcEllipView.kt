1e2abfce9af7e24aa654f7b315a7e2d1e47eb008
Anwesh43 <anweshthecool0@gmail.com>
2026-06-17 11:03:32 +0530

    lineconcellipview: adding LineConcEllipView class

--- lineconcellipview/src/main/java/com/example/lineconcellipview/LineConcEllipView.kt
+++ lineconcellipview/src/main/java/com/example/lineconcellipview/LineConcEllipView.kt
@@ -59,3 +59,19 @@ fun Canvas.drawLCENode(i : Int, scale : Float, paint : Paint) {
     paint.style = Paint.Style.STROKE
     drawLineConcEllipView(scale, w, h, paint)
 }
+
+class LineConcEllipView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
