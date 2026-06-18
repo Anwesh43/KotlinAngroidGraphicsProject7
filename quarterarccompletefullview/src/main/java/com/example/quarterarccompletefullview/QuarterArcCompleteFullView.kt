f0bcb4f41ebe76ef6debdc43716546cfd8e076d6
Anwesh43 <anweshthecool0@gmail.com>
2026-06-18 15:33:39 +0530

    quarterarccompletefullview: adding QACFNode class

--- quarterarccompletefullview/src/main/java/com/example/quarterarccompletefullview/QuarterArcCompleteFullView.kt
+++ quarterarccompletefullview/src/main/java/com/example/quarterarccompletefullview/QuarterArcCompleteFullView.kt
@@ -135,4 +135,45 @@ class QuarterArcCompleteFullView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class QACFNode(var i : Int = 0, val state : State = State()) {
+
+        private var next : QACFNode? = null
+        private var prev : QACFNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = QACFNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawQACFNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : QACFNode {
+            var curr : QACFNode? = prev
+            if (dir === 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
