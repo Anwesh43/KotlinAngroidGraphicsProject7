6b1018b2466b7fd517ad1bdec9832b1f2cc99af3
Anwesh43 <anweshthecool0@gmail.com>
2026-06-18 13:54:05 +0530

    quarterarccompletefullview: adding Canvas extension methods

--- quarterarccompletefullview/src/main/java/com/example/quarterarccompletefullview/QuarterArcCompleteFullView.kt
+++ quarterarccompletefullview/src/main/java/com/example/quarterarccompletefullview/QuarterArcCompleteFullView.kt
@@ -22,8 +22,53 @@ val strokeFactor : Float = 90f
 val sizeFactor : Float = 5.9f
 val delay : Long = 20
 val backColor : Int = "#BDBDBD".toColorInt()
-val rot : Float = 90f
+val rot : Float = 180f
 
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+
+fun Canvas.drawXY(x : Float, y : Float, cb : () -> Unit) {
+    save()
+    translate(x, y)
+    cb()
+    restore()
+}
+
+fun Canvas.scaleXY(sx : Float, sy : Float, cb : () -> Unit) {
+    drawXY(0f, 0f) {
+        scale(sx, sy)
+        cb()
+    }
+}
+
+fun Canvas.drawQuarterArcCompleteFull(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val dsc : (Int) -> Float = {
+        scale.divideScale(it, parts)
+    }
+    drawXY(w / 2, h / 2 + (h / 2 + paint.strokeWidth / 2) * dsc(4)) {
+        rotate(rot * dsc(3))
+        for (j in 0..1) {
+            scaleXY(1f - 2 * j, 1f) {
+                drawXY((-w * 0.5f + size) * (1 - dsc(1)), 0f) {
+                    drawArc(RectF(-size, -size, size, size), 180f, 90f * dsc(0), false, paint)
+                }
+                drawXY(-size, 0f) {
+                    drawLine(0f, 0f, size * dsc(2), 0f, paint)
+                }
+            }
+        }
+
+    }
+}
+
+fun Canvas.drawQACFNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = colors[i].toColorInt()
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    paint.style = Paint.Style.STROKE
+    drawQuarterArcCompleteFull(scale, w, h, paint)
+}
