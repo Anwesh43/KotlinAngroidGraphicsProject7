3afecf861944d467bd187c0bf4c34c09276b5fd5
Anwesh43 <anweshthecool0@gmail.com>
2026-06-19 15:12:52 +0530

    arcdividecreateview: adding view in activity

--- app/src/main/java/com/example/kotlinangroidgraphicsproject7/MainActivity.kt
+++ app/src/main/java/com/example/kotlinangroidgraphicsproject7/MainActivity.kt
@@ -11,13 +11,13 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.example.arcdividecreateview.ArcDivideCreateView
 import com.example.kotlinangroidgraphicsproject7.ui.theme.KotlinAngroidGraphicsProject7Theme
-import com.example.quarterarccompletefullview.QuarterArcCompleteFullView
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         //LineRotPartialArcView.create(this)
-        QuarterArcCompleteFullView.create(this)
+        ArcDivideCreateView.create(this)
     }
 }
