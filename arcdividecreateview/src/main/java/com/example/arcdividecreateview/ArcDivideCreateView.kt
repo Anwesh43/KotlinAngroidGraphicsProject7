f71497f904cee7c0a47a1aa8be32ce5c0a139245
Anwesh43 <anweshthecool0@gmail.com>
2026-06-19 14:18:23 +0530

    arcdividecreateview: adding ADCNode class

--- arcdividecreateview/src/main/java/com/example/arcdividecreateview/ArcDivideCreateView.kt
+++ arcdividecreateview/src/main/java/com/example/arcdividecreateview/ArcDivideCreateView.kt
@@ -127,4 +127,45 @@ class ArcDivideCreateView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class ADCNode(var i : Int = 0, val state : State = State()) {
+
+        private var next : ADCNode? = null
+        private var prev : ADCNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = ADCNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawADCNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : ADCNode {
+            var curr : ADCNode? = prev
+            if (dir === 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
