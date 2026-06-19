6b3cf21d8131daa039ff61cf5283ce89798a4f13
Anwesh43 <anweshthecool0@gmail.com>
2026-06-19 13:23:29 +0530

    arcdividecreateview: adding Canvas extension methods

--- arcdividecreateview/src/main/java/com/example/arcdividecreateview/ArcDivideCreateView.kt
+++ arcdividecreateview/src/main/java/com/example/arcdividecreateview/ArcDivideCreateView.kt
@@ -16,14 +16,51 @@ val colors : Array<String> = arrayOf(
     "#C51162",
     "#00C853"
 )
-val parts : Int = 5
-val scGap : Float = 0.04f / parts
+val parts : Int = 6
+val scGap : Float = 0.05f / parts
 val strokeFactor : Float = 90f
 val delay : Long = 20
 val backColor : Int = "#BDBDBD".toColorInt()
 val rot : Float = 45f
 val sizeFactor : Float = 5.9f
+val deg : Float = 180f
 
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+
+fun Canvas.drawXY(x : Float, y : Float, cb : () -> Unit) {
+    save()
+    translate(x, y)
+    cb()
+    restore()
+}
+
+fun Canvas.drawArcDivideCreate(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val dsc : (Int) -> Float = {
+        scale.divideScale(it, parts)
+    }
+    drawXY(w / 2, h / 2 + (h / 2) * dsc(5)) {
+        rotate(deg * dsc(4))
+        drawXY(0f, 0f) {
+            rotate(-rot)
+            drawLine(0f, 0f, 0f, -size * 0.5f * dsc(0), paint)
+        }
+        drawArc(RectF(-size / 2, -size / 2, size / 2, size / 2), 180f + rot, rot * dsc(1), false, paint)
+        drawXY(0f, -size / 2) {
+            drawLine(0f, 0f, 0f, -size * 0.5f * dsc(2), paint)
+        }
+        drawArc(RectF(-size, -size, size, size), -90f, rot * dsc(3), false, paint)
+    }
+}
+
+fun Canvas.drawADCNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = colors[i].toColorInt()
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    paint.style = Paint.Style.STROKE
+    drawArcDivideCreate(scale, w, h, paint)
+}
