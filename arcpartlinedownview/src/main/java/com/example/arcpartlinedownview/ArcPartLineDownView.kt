8e5adc66307eb74f0676450e249abeb1d3a4c1d2
Anwesh43 <anweshthecool0@gmail.com>
2026-06-20 08:36:37 +0530

    arcpartlinedownview: adding Renderer class

--- arcpartlinedownview/src/main/java/com/example/arcpartlinedownview/ArcPartLineDownView.kt
+++ arcpartlinedownview/src/main/java/com/example/arcpartlinedownview/ArcPartLineDownView.kt
@@ -189,4 +189,27 @@ class ArcPartLineDownView(ctx : Context) : View(ctx) {
             curr.startUpdating(cb)
         }
     }
+
+    data class Renderer(var view : ArcPartLineDownView) {
+
+        private val animator : Animator = Animator(view)
+        private val paint : Paint = Paint(Paint.ANTI_ALIAS_FLAG)
+        private val apld : ArcPartLineDown = ArcPartLineDown(0)
+
+        fun render(canvas : Canvas) {
+            canvas.drawColor(backColor)
+            apld.draw(canvas, paint)
+            animator.animate {
+                apld.update {
+                    animator.stop()
+                }
+            }
+        }
+
+        fun handleTap() {
+            apld.startUpdating {
+                animator.start()
+            }
+        }
+    }
 }
