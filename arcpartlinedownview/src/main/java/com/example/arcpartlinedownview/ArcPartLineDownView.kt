0f93b1a81acfa4d183a7cf503d06acefbde30426
Anwesh43 <anweshthecool0@gmail.com>
2026-06-20 08:38:14 +0530

    arcpartlinedownview: adding static create method

--- arcpartlinedownview/src/main/java/com/example/arcpartlinedownview/ArcPartLineDownView.kt
+++ arcpartlinedownview/src/main/java/com/example/arcpartlinedownview/ArcPartLineDownView.kt
@@ -214,4 +214,12 @@ class ArcPartLineDownView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    companion object {
+        fun create(activity : Activity) :ArcPartLineDownView {
+            val view : ArcPartLineDownView = ArcPartLineDownView(activity)
+            activity.setContentView(view)
+            return view
+        }
+    }
 }
