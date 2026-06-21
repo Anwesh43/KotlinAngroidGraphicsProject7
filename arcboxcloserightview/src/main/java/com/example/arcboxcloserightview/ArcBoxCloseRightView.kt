abb8bf8d5313c03d22d5b429b3efe2b58ecf0d45
Anwesh43 <anweshthecool0@gmail.com>
2026-06-22 01:31:24 +0530

    arcboxcloserightview: adding ArcBoxCloseRight class

--- arcboxcloserightview/src/main/java/com/example/arcboxcloserightview/ArcBoxCloseRightView.kt
+++ arcboxcloserightview/src/main/java/com/example/arcboxcloserightview/ArcBoxCloseRightView.kt
@@ -152,7 +152,7 @@ class ArcBoxCloseRightView(ctx : Context) : View(ctx) {
             state.update(cb)
         }
 
-        fun startUpdaitng(cb : () -> Unit) {
+        fun startUpdating(cb : () -> Unit) {
             state.startUpdating(cb)
         }
 
@@ -166,6 +166,29 @@ class ArcBoxCloseRightView(ctx : Context) : View(ctx) {
             }
             cb()
             return this
+        }
+    }
+
+    data class ArcBoxCloseRight(var i : Int) {
+
+        private var curr : ABCRNode = ABCRNode(0)
+        private var dir : Int = 1
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            curr.draw(canvas, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            curr.update {
+                curr = curr.getNext(dir) {
+                    dir *= -1
+                }
+                cb(it)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            curr.startUpdating(cb)
         }
     }
 }
