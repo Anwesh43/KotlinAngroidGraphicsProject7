0482af25351fa63524df4ebc245b04c6c223a300
Anwesh43 <anweshthecool0@gmail.com>
2026-06-21 14:33:29 +0530

    perplinebentupview: adding PLBUNode class

--- perplinebentupview/src/main/java/com/example/perplinebentupview/PerpLineBentUpView.kt
+++ perplinebentupview/src/main/java/com/example/perplinebentupview/PerpLineBentUpView.kt
@@ -122,4 +122,45 @@ class PerpLineBentUpView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class PLBUNode(var i : Int = 0, val state : State = State()) {
+
+        private var next : PLBUNode? = null
+        private var prev : PLBUNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = PLBUNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawPLBUNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : PLBUNode {
+            var curr : PLBUNode? = prev
+            if (dir === 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
