84ad8abb081b6d7e0bdf380ed705461d9d01227c
Anwesh43 <anweshthecool0@gmail.com>
2026-06-22 01:40:12 +0530

    arcboxcloserightview: adding Renderer class

--- arcboxcloserightview/src/main/java/com/example/arcboxcloserightview/ArcBoxCloseRightView.kt
+++ arcboxcloserightview/src/main/java/com/example/arcboxcloserightview/ArcBoxCloseRightView.kt
@@ -191,4 +191,27 @@ class ArcBoxCloseRightView(ctx : Context) : View(ctx) {
             curr.startUpdating(cb)
         }
     }
+
+    data class Renderer(var view : ArcBoxCloseRightView) {
+
+        private val animator : Animator = Animator(view)
+        private val abcr : ArcBoxCloseRight = ArcBoxCloseRight(0)
+        private val paint : Paint = Paint(Paint.ANTI_ALIAS_FLAG)
+
+        fun render(canvas : Canvas) {
+            canvas.drawColor(backColor)
+            abcr.draw(canvas, paint)
+            animator.animate {
+                abcr.update {
+                    animator.stop()
+                }
+            }
+        }
+
+        fun handleTap() {
+            abcr.startUpdating {
+                animator.start()
+            }
+        }
+    }
 }
