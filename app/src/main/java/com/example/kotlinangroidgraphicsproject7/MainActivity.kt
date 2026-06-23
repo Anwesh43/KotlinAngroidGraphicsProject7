79c059f14e441380dc4daa31ef5c56a813eb3638
Anwesh43 <anweshthecool0@gmail.com>
2026-06-23 13:09:32 +0530

    stretchlinefullcircleview: adding view in activity

--- app/src/main/java/com/example/kotlinangroidgraphicsproject7/MainActivity.kt
+++ app/src/main/java/com/example/kotlinangroidgraphicsproject7/MainActivity.kt
@@ -11,13 +11,13 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import com.example.arcboxcloserightview.ArcBoxCloseRightView
 import com.example.kotlinangroidgraphicsproject7.ui.theme.KotlinAngroidGraphicsProject7Theme
+import com.example.stretchlinefullcircleview.StretchLineFullCircleView
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         //LineRotPartialArcView.create(this)
-        ArcBoxCloseRightView.create(this)
+        StretchLineFullCircleView.create(this)
     }
 }
