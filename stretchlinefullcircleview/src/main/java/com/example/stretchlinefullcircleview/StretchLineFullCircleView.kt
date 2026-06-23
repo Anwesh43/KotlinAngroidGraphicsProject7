a8b382d752a2b9c4a702c0ab15b5cc77b0a463b3
Anwesh43 <anweshthecool0@gmail.com>
2026-06-23 12:03:12 +0530

    stretchlinefullcircleview: adding Animator class

--- stretchlinefullcircleview/src/main/java/com/example/stretchlinefullcircleview/StretchLineFullCircleView.kt
+++ stretchlinefullcircleview/src/main/java/com/example/stretchlinefullcircleview/StretchLineFullCircleView.kt
@@ -75,6 +75,7 @@ class StretchLineFullCircleView(ctx : Context) : View(ctx) {
 
             }
         }
+        return true
     }
 
     data class State(var scale : Float = 0f, var dir : Float = 0f, var prevScale : Float = 0f) {
@@ -93,6 +94,33 @@ class StretchLineFullCircleView(ctx : Context) : View(ctx) {
             if (dir === 0f) {
                 dir = 1f - 2 * prevScale
                 cb()
+            }
+        }
+    }
+
+    data class Animator(var view : View, var animated : Boolean = false) {
+
+        fun animate(cb : () -> Unit) {
+            if (animated) {
+                cb()
+                try {
+                    Thread.sleep(delay)
+                    view.invalidate()
+                } catch(ex : Exception) {
+
+                }
+            }
+        }
+        fun start() {
+            if (!animated) {
+                animated = true
+                view.postInvalidate()
+            }
+        }
+
+        fun stop() {
+            if (animated) {
+                animated = false
             }
         }
     }
