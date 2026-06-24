077905925da12f91e5e7c7a5b3a6723790eb9a13
Anwesh43 <anweshthecool0@gmail.com>
2026-06-24 12:52:47 +0530

    rightlineupleftview: adding RLULNode class

--- rightlineupleftview/src/main/java/com/example/rightlineupleftview/RightLineUpLeftView.kt
+++ rightlineupleftview/src/main/java/com/example/rightlineupleftview/RightLineUpLeftView.kt
@@ -123,5 +123,46 @@ class RightLineUpLeftView(ctx : Context) : View(ctx) {
                 }
             }
         }
+
+        data class RLULNode(var i : Int = 0, val state : State = State()) {
+
+            private var next : RLULNode? = null
+            private var prev : RLULNode? = null
+
+            init {
+                addNeighbor()
+            }
+
+            fun addNeighbor() {
+                if (i < colors.size - 1) {
+                    next = RLULNode(i + 1)
+                    next?.prev = this
+                }
+            }
+
+            fun draw(canvas : Canvas, paint : Paint) {
+                canvas.drawRLULNode(i, state.scale, paint)
+            }
+
+            fun update(cb : (Float) -> Unit) {
+                state.update(cb)
+            }
+
+            fun startUpdating(cb : () -> Unit) {
+                state.startUpdating(cb)
+            }
+
+            fun getNext(dir : Int, cb : () -> Unit) : RLULNode {
+                var curr : RLULNode? = prev
+                if (dir === 1) {
+                    curr = next
+                }
+                if (curr != null) {
+                    return curr
+                }
+                cb()
+                return this
+            }
+        }
     }
 }
