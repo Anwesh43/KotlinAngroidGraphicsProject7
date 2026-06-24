064a580cc40760f593c04f7a9d096b6d68150e11
Anwesh43 <anweshthecool0@gmail.com>
2026-06-24 12:47:23 +0530

    rightlineupleftview: adding RightLineUpLeftView class

--- rightlineupleftview/src/main/java/com/example/rightlineupleftview/RightLineUpLeftView.kt
+++ rightlineupleftview/src/main/java/com/example/rightlineupleftview/RightLineUpLeftView.kt
@@ -61,3 +61,19 @@ fun Canvas.drawRLULNode(i : Int, scale : Float, paint : Paint) {
     paint.style = Paint.Style.STROKE
     drawRightLineUpLeft(scale, w, h, paint)
 }
+
+class RightLineUpLeftView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
