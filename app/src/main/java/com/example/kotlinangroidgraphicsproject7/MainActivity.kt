bb43c2d661e4f1773fd922d4deea4c85b180dd2d
Anwesh43 <anweshthecool0@gmail.com>
2026-06-25 13:54:41 +0530

    doublearcmoveupview: adding view in activity

--- app/src/main/java/com/example/kotlinangroidgraphicsproject7/MainActivity.kt
+++ app/src/main/java/com/example/kotlinangroidgraphicsproject7/MainActivity.kt
@@ -11,13 +11,13 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.example.doublearcmoveupview.DoubleArcMoveUpView
 import com.example.kotlinangroidgraphicsproject7.ui.theme.KotlinAngroidGraphicsProject7Theme
-import com.example.rightlineupleftview.RightLineUpLeftView
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         //LineRotPartialArcView.create(this)
-        RightLineUpLeftView.create(this)
+        DoubleArcMoveUpView.create(this)
     }
 }
