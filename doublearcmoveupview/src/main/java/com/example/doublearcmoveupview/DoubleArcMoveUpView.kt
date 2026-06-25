3d3935732e7ad953fb38ff297e75e0fe864ce3a3
Anwesh43 <anweshthecool0@gmail.com>
2026-06-25 13:57:06 +0530

    doublearcmoveupview: adding fix

--- doublearcmoveupview/src/main/java/com/example/doublearcmoveupview/DoubleArcMoveUpView.kt
+++ doublearcmoveupview/src/main/java/com/example/doublearcmoveupview/DoubleArcMoveUpView.kt
@@ -22,7 +22,7 @@ val strokeFactor : Float = 90f
 val sizeFactor : Float = 5.9f
 val delay : Long = 20
 val backColor : Int = "#BDBDBD".toColorInt()
-val rot : Float = 90f
+val rot : Float = 180f
 
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
@@ -41,7 +41,6 @@ fun Canvas.drawDoubleArcMoveUp(scale : Float, w : Float, h : Float, paint : Pain
         scale.divideScale(it, parts)
     }
     drawXY(w / 2, h / 2 - (h / 2) * dsc(5)) {
-        rotate(rot * dsc(0))
         for (j in 0..1) {
             drawXY(-(w / 2) * (1f - 2 * j) * (1 - dsc(2 * j + 1)), 0f) {
                 rotate(rot * (1 - j) * (dsc(2) - dsc(4)))
