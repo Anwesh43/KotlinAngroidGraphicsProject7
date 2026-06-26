76757c84ff7c3262fda88b71fc1466c9f5f7e7fa
Anwesh43 <anweshthecool0@gmail.com>
2026-06-26 13:12:52 +0530

    arcjoinrotdownview: adding ArcJoinRotDown class

--- arcjoinrotdownview/src/main/java/com/example/arcjoinrotdownview/ArcJoinRotDownView.kt
+++ arcjoinrotdownview/src/main/java/com/example/arcjoinrotdownview/ArcJoinRotDownView.kt
@@ -162,4 +162,27 @@ class ArcJoinRotDownView(ctx : Context) : View(ctx) {
             return this
         }
     }
+
+    data class ArcJoinRotDown(var i : Int) {
+
+        private var curr : AJRDNode = AJRDNode(0)
+        private var dir : Int = 1
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            curr.draw(canvas, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            curr.update {
+                curr = curr.getNext(dir) {
+                    dir *= -1
+                }
+                cb(it)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            curr.startUpdating(cb)
+        }
+    }
 }
