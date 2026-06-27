4de5acfead1bb6fed81dad47f04986a0bf3b92b7
Anwesh43 <anweshthecool0@gmail.com>
2026-06-27 12:08:52 +0530

    linebentarccompleteview: adding LineBentArcCompleteView class

--- linebentarccompleteview/src/main/java/com/example/linebentarccompleteview/LineBentArcCompleteView.kt
+++ linebentarccompleteview/src/main/java/com/example/linebentarccompleteview/LineBentArcCompleteView.kt
@@ -62,3 +62,19 @@ fun Canvas.drawLBACNode(i : Int, scale : Float, paint : Paint) {
     paint.style = Paint.Style.STROKE
     drawLineBentArcComplete(scale, w, h, paint)
 }
+
+class LineBentArcCompleteView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
