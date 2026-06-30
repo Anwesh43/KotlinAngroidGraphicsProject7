861369ce9735863b5aad51f8e11179f9557bf313
Anwesh43 <anweshthecool0@gmail.com>
2026-06-30 10:44:45 +0530

    semiarcleftrotview: adding SemiArcLeftRotView class

--- semiarcleftrotview/src/main/java/com/example/semiarcleftrotview/SemiArcLeftRotView.kt
+++ semiarcleftrotview/src/main/java/com/example/semiarcleftrotview/SemiArcLeftRotView.kt
@@ -62,3 +62,19 @@ fun Canvas.drawSALRNode(i : Int, scale : Float, paint : Paint) {
     paint.style = Paint.Style.STROKE
     drawSemiArcLeftRot(scale, w, h, paint)
 }
+
+class SemiArcLeftRotView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
