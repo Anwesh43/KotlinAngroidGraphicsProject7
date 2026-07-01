1f2f91dcd921b23cf98f908d23220bec22eb5765
Anwesh43 <anweshthecool0@gmail.com>
2026-07-01 12:59:29 +0530

    linebentarcupview: adding LBAUNode class

--- linebentarcupview/src/main/java/com/example/linebentarcupview/LineBentArcUpView.kt
+++ linebentarcupview/src/main/java/com/example/linebentarcupview/LineBentArcUpView.kt
@@ -124,4 +124,45 @@ class LineBentArcUpView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class LBAUNode(var i : Int = 0, val state : State = State()) {
+
+        private var next : LBAUNode? = null
+        private var prev : LBAUNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = LBAUNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawLBAUNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : LBAUNode {
+            var curr : LBAUNode? = prev
+            if (dir === 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
