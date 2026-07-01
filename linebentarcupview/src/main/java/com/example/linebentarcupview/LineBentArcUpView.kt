bd961cdfd7cd37cd0166e86e1b81a64783ade7a0
Anwesh43 <anweshthecool0@gmail.com>
2026-07-01 13:05:36 +0530

    linebentarcupview: adding Renderer class

--- linebentarcupview/src/main/java/com/example/linebentarcupview/LineBentArcUpView.kt
+++ linebentarcupview/src/main/java/com/example/linebentarcupview/LineBentArcUpView.kt
@@ -188,4 +188,27 @@ class LineBentArcUpView(ctx : Context) : View(ctx) {
             curr.startUpdating(cb)
         }
     }
+
+    data class Renderer(var view : LineBentArcUpView) {
+
+        private val animator : Animator = Animator(view)
+        private val paint : Paint = Paint(Paint.ANTI_ALIAS_FLAG)
+        private val lbau : LineBentArcUp = LineBentArcUp(0)
+
+        fun render(canvas : Canvas) {
+            canvas.drawColor(backColor)
+            lbau.draw(canvas, paint)
+            animator.animate {
+                lbau.update {
+                    animator.stop()
+                }
+            }
+        }
+
+        fun handleTap() {
+            lbau.startUpdating {
+                animator.start()
+            }
+        }
+    }
 }
