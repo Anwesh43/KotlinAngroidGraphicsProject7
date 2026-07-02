c8d33b1d70936992dc8de6953e1f05b6ace0fa4a
Anwesh43 <anweshthecool0@gmail.com>
2026-07-03 05:03:38 +0530

    linearcdownrotview: adding LineArcDownRotView class

--- linearcdownrotview/src/main/java/com/example/linearcdownrotview/LineArcDownRotView.kt
+++ linearcdownrotview/src/main/java/com/example/linearcdownrotview/LineArcDownRotView.kt
@@ -58,3 +58,19 @@ fun Canvas.drawLADRNode(i : Int, scale : Float, paint : Paint) {
     paint.style = Paint.Style.STROKE
     drawLineArcDownRot(scale, w, h, paint)
 }
+
+class LineArcDownRotView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
