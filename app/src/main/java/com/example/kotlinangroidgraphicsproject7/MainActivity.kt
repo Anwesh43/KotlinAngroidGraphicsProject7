ea2effb6002068b8300d93647d3838fad486fc21
Anwesh43 <anweshthecool0@gmail.com>
2026-07-06 18:45:39 +0530

    linesweeprotdiagview: adding view in activity

--- app/src/main/java/com/example/kotlinangroidgraphicsproject7/MainActivity.kt
+++ app/src/main/java/com/example/kotlinangroidgraphicsproject7/MainActivity.kt
@@ -12,12 +12,12 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.kotlinangroidgraphicsproject7.ui.theme.KotlinAngroidGraphicsProject7Theme
-import com.example.linedropextendarcview.LineDropExtendArcView
+import com.example.linesweeprotdiagview.LineSweepDiagRotView
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         //LineRotPartialArcView.create(this)
-        LineDropExtendArcView.create(this)
+        LineSweepDiagRotView.create(this)
     }
 }
