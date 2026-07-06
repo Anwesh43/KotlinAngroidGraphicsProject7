3e48a617db595967739f5ebcd767b7606b1295a3
Anwesh43 <anweshthecool0@gmail.com>
2026-07-06 18:40:31 +0530

    linesweeprotdiagview: adding LSDRNode class

--- linesweeprotdiagview/src/main/java/com/example/linesweeprotdiagview/LineSweepDiagRotView.kt
+++ linesweeprotdiagview/src/main/java/com/example/linesweeprotdiagview/LineSweepDiagRotView.kt
@@ -123,4 +123,45 @@ class LineSweepDiagRotView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class LSDRNode(var i : Int = 0, val state : State = State()) {
+
+        private var next : LSDRNode? = null
+        private var prev : LSDRNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = LSDRNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawLSDRNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : LSDRNode {
+            var curr : LSDRNode? = prev
+            if (dir === 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
