fba844d332e992bc3b9bfdf7318cb78ddce342d8
Anwesh43 <anweshthecool0@gmail.com>
2026-07-07 11:20:20 +0530

    multidroprotlineview: adding fix 2

--- multidroprotlineview/src/main/java/com/example/multidroprotlineview/MultiDropRotLineView.kt
+++ multidroprotlineview/src/main/java/com/example/multidroprotlineview/MultiDropRotLineView.kt
@@ -15,8 +15,8 @@ val colors : Array<String> = arrayOf(
     "#C51162",
     "#00C853"
 )
-val parts : Int = 5
-val scGap : Float = 0.04f / parts
+val parts : Int = 3
+val scGap : Float = 0.03f / parts
 val strokeFactor : Float = 90f
 val sizeFactor : Float = 10.9f
 val delay : Long = 20
