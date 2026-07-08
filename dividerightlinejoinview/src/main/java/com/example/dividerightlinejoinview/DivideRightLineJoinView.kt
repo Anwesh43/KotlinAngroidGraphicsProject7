d3a476a52e76572df3fa2a37eadf7b1a70fbb0b0
Anwesh43 <anweshthecool0@gmail.com>
2026-07-08 08:15:17 +0530

    dividerightlinejoinview: adding DRLJNode class

--- dividerightlinejoinview/src/main/java/com/example/dividerightlinejoinview/DivideRightLineJoinView.kt
+++ dividerightlinejoinview/src/main/java/com/example/dividerightlinejoinview/DivideRightLineJoinView.kt
@@ -127,4 +127,45 @@ class DivideRightLineJoinView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class DRLJNode(var i : Int = 0, val state : State = State()) {
+
+        private var next : DRLJNode? = null
+        private var prev : DRLJNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = DRLJNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawDRLJNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : DRLJNode {
+            var curr : DRLJNode? = prev
+            if (dir === 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
