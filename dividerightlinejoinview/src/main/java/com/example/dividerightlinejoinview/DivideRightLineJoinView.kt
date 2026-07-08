6853b50abc9c6a20a87228483b1acd863d22ec46
Anwesh43 <anweshthecool0@gmail.com>
2026-07-08 08:03:14 +0530

    dividerightlinejoinview: adding Canvas extension methods

--- dividerightlinejoinview/src/main/java/com/example/dividerightlinejoinview/DivideRightLineJoinView.kt
+++ dividerightlinejoinview/src/main/java/com/example/dividerightlinejoinview/DivideRightLineJoinView.kt
@@ -15,8 +15,8 @@ val colors : Array<String> = arrayOf(
     "#C51162",
     "#00C853"
 )
-val parts : Int = 5
-val scGap : Float = 0.04f / parts
+val parts : Int = 6
+val scGap : Float = 0.05f / parts
 val strokeFactor : Float = 90f
 val sizeFactor : Float = 5.9f
 val delay : Long = 20
@@ -26,3 +26,41 @@ val rot : Float = 90f
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+
+fun Canvas.drawXY(x : Float, y : Float, cb : () -> Unit) {
+    save()
+    translate(x, y)
+    cb()
+    restore()
+}
+
+fun Canvas.drawDivideRightLineJoin(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val dsc : (Int) -> Float = {
+        scale.divideScale(it, parts)
+    }
+    drawXY(w / 2, h / 2 + (h / 2) * dsc(5)) {
+        rotate(rot * dsc(4))
+
+        for (j in 0..2) {
+            val gap : Float = (size / 3)
+            drawXY(gap * j, 0f) {
+                drawLine(0f, 0f, gap * dsc(j).divideScale(0, 2), 0f, paint)
+                drawXY(gap, 0f) {
+                    drawLine(0f, 0f, 0f, -gap * j * dsc(j).divideScale(1, 2), paint)
+                }
+            }
+        }
+        drawLine(0f, 0f, size * dsc(3), -size * dsc(3), paint)
+    }
+}
+
+fun Canvas.drawDRLJNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = colors[i].toColorInt()
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    paint.style = Paint.Style.STROKE
+    drawDivideRightLineJoin(scale, w, h, paint)
+}
