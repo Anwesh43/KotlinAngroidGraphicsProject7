17e05a775e0133a2fed1971d30552b2e1226a545
Anwesh43 <anweshthecool0@gmail.com>
2026-07-08 08:06:52 +0530

    dividerightlinejoinview: adding DivideRightLineJoinView class

--- dividerightlinejoinview/src/main/java/com/example/dividerightlinejoinview/DivideRightLineJoinView.kt
+++ dividerightlinejoinview/src/main/java/com/example/dividerightlinejoinview/DivideRightLineJoinView.kt
@@ -64,3 +64,19 @@ fun Canvas.drawDRLJNode(i : Int, scale : Float, paint : Paint) {
     paint.style = Paint.Style.STROKE
     drawDivideRightLineJoin(scale, w, h, paint)
 }
+
+class DivideRightLineJoinView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
