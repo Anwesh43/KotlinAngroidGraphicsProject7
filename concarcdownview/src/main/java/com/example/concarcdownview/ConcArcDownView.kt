26efa99b658d3dca808ae42a765818093780c61e
Anwesh43 <anweshthecool0@gmail.com>
2026-07-09 08:50:34 +0530

    concarcdownview: adding CADNode class

--- concarcdownview/src/main/java/com/example/concarcdownview/ConcArcDownView.kt
+++ concarcdownview/src/main/java/com/example/concarcdownview/ConcArcDownView.kt
@@ -122,4 +122,45 @@ class ConcArcDownView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class CADNode(var i : Int = 0, val state : State = State()) {
+
+        private var next : CADNode? = null
+        private var prev : CADNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = CADNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawCADNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : CADNode {
+            var curr : CADNode? = prev
+            if (dir === 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
