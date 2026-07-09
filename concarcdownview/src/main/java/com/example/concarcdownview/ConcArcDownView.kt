1e4e58342841666dc912c3bbf18e069eb291f8e1
Anwesh43 <anweshthecool0@gmail.com>
2026-07-09 09:02:27 +0530

    concarcdownview: adding fix

--- concarcdownview/src/main/java/com/example/concarcdownview/ConcArcDownView.kt
+++ concarcdownview/src/main/java/com/example/concarcdownview/ConcArcDownView.kt
@@ -41,7 +41,7 @@ fun Canvas.drawConcArcDown(scale : Float, w : Float, h : Float, paint : Paint) {
     val dsc : (Int) -> Float = {
         scale.divideScale(it, parts)
     }
-    drawXY(w / 2, h / 2 + (h / 2) * dsc(4)) {
+    drawXY(w / 2, h / 2 - (h / 2) * dsc(4)) {
         rotate(rot * dsc(3))
         for (j in 0..(arcs - 1)) {
             val r : Float = size / (1 + j)
