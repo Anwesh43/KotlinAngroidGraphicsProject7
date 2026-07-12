b3ab4bf0a7f8ea5bddec255196d26b30f7747eff
Anwesh43 <anweshthecool0@gmail.com>
2026-07-12 11:53:11 +0530

    benteithersidedownview: adding BESDNode class

--- benteithersidedownview/src/main/java/com/example/benteithersidedownview/BentEitherSideDownView.kt
+++ benteithersidedownview/src/main/java/com/example/benteithersidedownview/BentEitherSideDownView.kt
@@ -124,4 +124,45 @@ class BentEitherSideDownView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class BESDNode(var i : Int = 0, val state : State = State()) {
+
+        private var next : BESDNode? = null
+        private var prev : BESDNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = BESDNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawBESDNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : BESDNode {
+            var curr : BESDNode? = prev
+            if (dir === 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
