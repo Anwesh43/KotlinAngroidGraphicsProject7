e49f9674cec672ff76175b7773ca96cb99733444
Anwesh43 <anweshthecool0@gmail.com>
2026-07-12 11:38:43 +0530

    benteithersidedownview: adding BentEitherSideDownView class

--- benteithersidedownview/src/main/java/com/example/benteithersidedownview/BentEitherSideDownView.kt
+++ benteithersidedownview/src/main/java/com/example/benteithersidedownview/BentEitherSideDownView.kt
@@ -61,3 +61,19 @@ fun Canvas.drawBESDNode(i : Int, scale : Float, paint : Paint) {
     paint.style = Paint.Style.STROKE
     drawBentEitherSideDown(scale, w, h, paint)
 }
+
+class BentEitherSideDownView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
