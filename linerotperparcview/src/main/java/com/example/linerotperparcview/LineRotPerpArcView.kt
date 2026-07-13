b297351c30d89b951d999e13dc13692d0b3996ed
Anwesh43 <anweshthecool0@gmail.com>
2026-07-13 14:19:42 +0530

    linerotperparcview: adding Canvas extension method

--- linerotperparcview/src/main/java/com/example/linerotperparcview/LineRotPerpArcView.kt
+++ linerotperparcview/src/main/java/com/example/linerotperparcview/LineRotPerpArcView.kt
@@ -28,3 +28,38 @@ val sweep : Float = 180f
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+
+fun Canvas.drawXY(x : Float, y : Float, cb : () -> Unit) {
+    save()
+    translate(x, y)
+    cb()
+    restore()
+}
+
+fun Canvas.drawLineRotPerpArc(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val dsc : (Int) -> Float = {
+        scale.divideScale(it, parts)
+    }
+    drawXY(w / 2, h / 2) {
+        drawXY(0f, 0f) {
+            rotate(rot * dsc(3))
+            drawLine(0f, 0f, -size * dsc(0), 0f, paint)
+        }
+        drawXY(0f, -h * 0.5f * (1 - dsc(1))) {
+            rotate(sweep * dsc(2))
+            drawLine(0f, 0f, 0f, -size, paint)
+        }
+        drawArc(RectF(-size, -size, size, size), -90f, sweep * dsc(2), false, paint)
+    }
+}
+
+fun Canvas.drawLRPANode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = colors[i].toColorInt()
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    paint.style = Paint.Style.STROKE
+    drawLineRotPerpArc(scale, w, h, paint)
+}
