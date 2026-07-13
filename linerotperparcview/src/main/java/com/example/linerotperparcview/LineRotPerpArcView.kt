528ad37e34d1b4257b044eec6bd819720efd1491
Anwesh43 <anweshthecool0@gmail.com>
2026-07-13 14:55:57 +0530

    linerotperparcview: adding fix

--- linerotperparcview/src/main/java/com/example/linerotperparcview/LineRotPerpArcView.kt
+++ linerotperparcview/src/main/java/com/example/linerotperparcview/LineRotPerpArcView.kt
@@ -41,7 +41,7 @@ fun Canvas.drawLineRotPerpArc(scale : Float, w : Float, h : Float, paint : Paint
     val dsc : (Int) -> Float = {
         scale.divideScale(it, parts)
     }
-    drawXY(w / 2, h / 2) {
+    drawXY(w / 2 + (w / 2) * dsc(4), h / 2) {
         drawXY(0f, 0f) {
             rotate(rot * dsc(3))
             drawLine(0f, 0f, -size * dsc(0), 0f, paint)
