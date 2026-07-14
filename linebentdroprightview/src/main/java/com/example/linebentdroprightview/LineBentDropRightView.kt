afca048b79d08f4580e88f4f763eb55d54f37275
Anwesh43 <anweshthecool0@gmail.com>
2026-07-14 08:23:29 +0530

    linebentdroprightview: adding Renderer class

--- linebentdroprightview/src/main/java/com/example/linebentdroprightview/LineBentDropRightView.kt
+++ linebentdroprightview/src/main/java/com/example/linebentdroprightview/LineBentDropRightView.kt
@@ -186,4 +186,27 @@ class LineBentDropRightView(ctx : Context) : View(ctx) {
             curr.startUpdating(cb)
         }
     }
+
+    data class Renderer(var view : LineBentDropRightView) {
+
+        private val paint : Paint = Paint(Paint.ANTI_ALIAS_FLAG)
+        private val lbdr : LineBentDropRight = LineBentDropRight(0)
+        private val animator : Animator = Animator(view)
+
+        fun render(canvas : Canvas) {
+            canvas.drawColor(backColor)
+            lbdr.draw(canvas, paint)
+            animator.animate {
+                lbdr.update {
+                    animator.stop()
+                }
+            }
+        }
+
+        fun handleTap() {
+            lbdr.startUpdating {
+                animator.start()
+            }
+        }
+    }
 }
