668c04df10b6206539e9b6c5b5a0ae024960d5a3
Anwesh43 <anweshthecool0@gmail.com>
2026-07-16 12:46:00 +0530

    linehookarcleftview: adding Canvas extension methods

--- linehookarcleftview/src/main/java/com/example/linehookarcleftview/LineHookArcLeftView.kt
+++ linehookarcleftview/src/main/java/com/example/linehookarcleftview/LineHookArcLeftView.kt
@@ -16,7 +16,7 @@ val colors : Array<String> = arrayOf(
     "#C51162",
     "#00C853"
 )
-val parts : Int = 4
+val parts : Int = 5
 val scGap : Float = 0.04f / parts
 val strokeFactor : Float = 90f
 val sizeFactor : Float = 5.9f
@@ -28,3 +28,34 @@ val sweep : Float = 180f
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+
+fun Canvas.drawXY(x : Float, y : Float, cb : () -> Unit) {
+    save()
+    translate(x, y)
+    cb()
+    restore()
+}
+
+fun Canvas.drawLineHookArcLeft(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val dsc : (Int) -> Float = {
+        scale.divideScale(it, parts)
+    }
+    drawXY(w / 2 - (w / 2) * dsc(4), h * 0.5f * dsc(2)) {
+        rotate(rot * dsc(3))
+        drawLine(0f, 0f, 0f, size * dsc(0), paint)
+        drawXY(0f, size) {
+            drawArc(RectF(-size / 4, -size / 2, size / 4, 0f), 90f, sweep * dsc(1), false, paint)
+        }
+    }
+}
+
+fun Canvas.drawLHALNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = colors[i].toColorInt()
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    paint.style = Paint.Style.STROKE
+    drawLineHookArcLeft(scale, w , h, paint)
+}
