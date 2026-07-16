8bbfdd5bb767f965dfff54bba1cc29d411e2a0d0
Anwesh43 <anweshthecool0@gmail.com>
2026-07-16 12:54:45 +0530

    linehookarcleftview: adding LHALNode class

--- linehookarcleftview/src/main/java/com/example/linehookarcleftview/LineHookArcLeftView.kt
+++ linehookarcleftview/src/main/java/com/example/linehookarcleftview/LineHookArcLeftView.kt
@@ -122,4 +122,45 @@ class LineHookArcLeftView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class LHALNode(var i : Int = 0 , val state : State = State()) {
+
+        private var prev : LHALNode? = null
+        private var next : LHALNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = LHALNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawLHALNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : LHALNode {
+            var curr : LHALNode? = prev
+            if (dir === 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
