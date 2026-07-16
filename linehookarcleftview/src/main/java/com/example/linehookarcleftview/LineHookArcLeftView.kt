fd470b40bb3d35ed54ec811379a33892024d345d
Anwesh43 <anweshthecool0@gmail.com>
2026-07-16 12:46:58 +0530

    linehookarcleftview: adding LineHookArcLeftView class

--- linehookarcleftview/src/main/java/com/example/linehookarcleftview/LineHookArcLeftView.kt
+++ linehookarcleftview/src/main/java/com/example/linehookarcleftview/LineHookArcLeftView.kt
@@ -59,3 +59,19 @@ fun Canvas.drawLHALNode(i : Int, scale : Float, paint : Paint) {
     paint.style = Paint.Style.STROKE
     drawLineHookArcLeft(scale, w , h, paint)
 }
+
+class LineHookArcLeftView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
