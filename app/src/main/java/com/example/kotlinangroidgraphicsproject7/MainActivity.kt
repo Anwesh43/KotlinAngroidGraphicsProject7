ed485ee32fbe3aa06dc36afdd4dc611935db1ac6
Anwesh43 <anweshthecool0@gmail.com>
2026-07-17 10:07:08 +0530

    linearcupdownview: adding view in activity

--- app/src/main/java/com/example/kotlinangroidgraphicsproject7/MainActivity.kt
+++ app/src/main/java/com/example/kotlinangroidgraphicsproject7/MainActivity.kt
@@ -12,12 +12,12 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.kotlinangroidgraphicsproject7.ui.theme.KotlinAngroidGraphicsProject7Theme
-import com.example.linehookarcleftview.LineHookArcLeftView
+import com.example.linearcupdownview.LineArcUpDownView
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         //LineRotPartialArcView.create(this)
-        LineHookArcLeftView.create(this)
+        LineArcUpDownView.create(this)
     }
 }
