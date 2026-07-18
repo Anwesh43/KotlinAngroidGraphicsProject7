23c374cafd293787102ed8a02df6e29b44f0cd1c
Anwesh43 <anweshthecool0@gmail.com>
2026-07-18 11:15:03 +0530

    halfcirclerotencloseview: adding HCRENode class

--- halfcirclerotencloseview/src/main/java/com/example/halfcirclerotencloseview/HalfCircleRotEncloseView.kt
+++ halfcirclerotencloseview/src/main/java/com/example/halfcirclerotencloseview/HalfCircleRotEncloseView.kt
@@ -125,4 +125,45 @@ class HalfCircleRotEncloseView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class HCRENode(var i : Int = 0, val state : State = State()) {
+
+        private var next : HCRENode? = null
+        private var prev : HCRENode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = HCRENode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawHCRENode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : HCRENode {
+            var curr : HCRENode? = prev
+            if (dir === 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
