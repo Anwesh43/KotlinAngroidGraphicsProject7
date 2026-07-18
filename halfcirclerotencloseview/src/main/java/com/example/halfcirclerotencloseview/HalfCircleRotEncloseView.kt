624ddfe184d05184651b1a632d42d365a79ccc83
Anwesh43 <anweshthecool0@gmail.com>
2026-07-18 11:06:45 +0530

    halfcirclerotencloseview: adding HalfCircleRotEncloseView class

--- halfcirclerotencloseview/src/main/java/com/example/halfcirclerotencloseview/HalfCircleRotEncloseView.kt
+++ halfcirclerotencloseview/src/main/java/com/example/halfcirclerotencloseview/HalfCircleRotEncloseView.kt
@@ -62,3 +62,19 @@ fun Canvas.drawHCRENode(i : Int, scale : Float, paint : Paint) {
     paint.style = Paint.Style.STROKE
     drawHalfCircleRotEnclose(scale, w, h, paint)
 }
+
+class HalfCircleRotEncloseView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
