cbf0a4dde1f0cdeecd3f14102b93221f0afb6773
Anwesh43 <anweshthecool0@gmail.com>
2026-07-19 10:22:47 +0530

    bijoinarclinedownview: adding BJLADNode class

--- bijoinarclinedownview/src/main/java/com/example/bijoinarclinedownview/BiJoinArcLineDownView.kt
+++ bijoinarclinedownview/src/main/java/com/example/bijoinarclinedownview/BiJoinArcLineDownView.kt
@@ -123,4 +123,45 @@ class BiJoinArcLineDownView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class BJLADNode(var i : Int = 0, val state : State = State()) {
+
+        private var next : BJLADNode? = null
+        private var prev : BJLADNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = BJLADNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawBJALDNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : BJLADNode {
+            var curr : BJLADNode? = prev
+            if (dir === 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
