d9b8f3d7778e1ee52eed632ea95e36427b8cf1a0
Anwesh43 <anweshthecool0@gmail.com>
2026-07-19 10:33:10 +0530

    bijoinarclinedownview: adding static create method

--- bijoinarclinedownview/src/main/java/com/example/bijoinarclinedownview/BiJoinArcLineDownView.kt
+++ bijoinarclinedownview/src/main/java/com/example/bijoinarclinedownview/BiJoinArcLineDownView.kt
@@ -212,4 +212,12 @@ class BiJoinArcLineDownView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    companion object {
+        fun create(activity : Activity) : BiJoinArcLineDownView {
+            val view : BiJoinArcLineDownView = BiJoinArcLineDownView(activity)
+            activity.setContentView(view)
+            return view
+        }
+    }
 }
