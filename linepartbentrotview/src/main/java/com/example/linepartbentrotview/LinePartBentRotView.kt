f9db88e6d8b58122a4761e7911d6b078c75e8e08
Anwesh43 <anweshthecool0@gmail.com>
2026-07-25 11:09:28 +0530

    linepartbentrotview: adding Canvas extension methods

--- linepartbentrotview/src/main/java/com/example/linepartbentrotview/LinePartBentRotView.kt
+++ linepartbentrotview/src/main/java/com/example/linepartbentrotview/LinePartBentRotView.kt
@@ -28,3 +28,38 @@ val bentDeg : Float = 30f
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n: Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+
+fun Canvas.drawXY(x : Float, y : Float, cb : () -> Unit) {
+    save()
+    translate(x, y)
+    cb()
+    restore()
+}
+
+fun Canvas.drawLinePartBentRot(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val dsc : (Int) -> Float = {
+        scale.divideScale(it, parts)
+    }
+    drawXY(w / 2 - (w / 2) * dsc(5), h / 2 + (h / 2) * dsc(5)) {
+        rotate(rot * (dsc(2) + dsc(4)))
+        for (j in 0..1) {
+            drawXY(0f, 0f) {
+                rotate(bentDeg * (dsc(1) + dsc(3)) * j)
+                drawLine(0f, 0f, 0f, -size * dsc(0), paint)
+            }
+        }
+        drawArc(RectF(-size, -size, size, size), -90f, bentDeg * (dsc(1) + dsc(3)), false, paint)
+    }
+}
+
+fun Canvas.drawLPBRNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = colors[i].toColorInt()
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    paint.style = Paint.Style.STROKE
+    drawLinePartBentRot(scale, w, h, paint)
+}
+
