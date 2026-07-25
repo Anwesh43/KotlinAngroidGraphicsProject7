27b1d7434e8f745d84d3c4e91fd65e241c2129f8
Anwesh43 <anweshthecool0@gmail.com>
2026-07-25 10:15:05 +0530

    linerotarccloseview: adding LineRotArcClose class

--- linerotarccloseview/src/main/java/com/example/linerotarccloseview/LineRotArcCloseView.kt
+++ linerotarccloseview/src/main/java/com/example/linerotarccloseview/LineRotArcCloseView.kt
@@ -164,4 +164,27 @@ class LineRotArcCloseView(ctx : Context) : View(ctx) {
             return this
         }
     }
+
+    data class LineRotArcClose(var i : Int) {
+
+        private var curr : LRACNode = LRACNode(0)
+        private var dir : Int = 1
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            curr.draw(canvas, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            curr.update {
+                curr = curr.getNext(dir) {
+                    dir *= -1
+                }
+                cb(it)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            curr.startUpdating(cb)
+        }
+    }
 }
