67089e592aa1559d1bb78b94b4f35336a9996bf6
Anwesh43 <anweshthecool0@gmail.com>
2026-07-26 14:16:51 +0530

    arcsweeprotupview: adding ArcSweepRotUpView class

--- arcsweeprotupview/src/main/java/com/example/arcsweeprotupview/ArcSweepRotUpView.kt
+++ arcsweeprotupview/src/main/java/com/example/arcsweeprotupview/ArcSweepRotUpView.kt
@@ -66,3 +66,19 @@ fun Canvas.drawASRUNode(i : Int, scale : Float, paint : Paint) {
     paint.style = Paint.Style.STROKE
     drawArcSweepRotUp(scale, w, h, paint)
 }
+
+class ArcSweepRotUpView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
