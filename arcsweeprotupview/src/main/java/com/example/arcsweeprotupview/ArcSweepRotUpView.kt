040dc95c77cb1d1938cd5df6abe670cc9d07128f
Anwesh43 <anweshthecool0@gmail.com>
2026-07-26 15:25:50 +0530

    arcsweeprotupview: adding ASRUNode class

--- arcsweeprotupview/src/main/java/com/example/arcsweeprotupview/ArcSweepRotUpView.kt
+++ arcsweeprotupview/src/main/java/com/example/arcsweeprotupview/ArcSweepRotUpView.kt
@@ -129,4 +129,45 @@ class ArcSweepRotUpView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class ASRUNode(var i : Int = 0, val state : State = State()) {
+
+        private var next : ASRUNode? = null
+        private var prev : ASRUNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = ASRUNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawASRUNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : ASRUNode {
+            var curr : ASRUNode? = prev
+            if (dir === 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
