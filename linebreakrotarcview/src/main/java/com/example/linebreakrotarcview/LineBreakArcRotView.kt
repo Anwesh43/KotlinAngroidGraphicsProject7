d165330e46d9e1489b5ec080cdb29191050e0aac
Anwesh43 <anweshthecool0@gmail.com>
2026-07-29 15:28:06 +0530

    linebreakrotarcview: adding Canvas extension methods

--- linebreakrotarcview/src/main/java/com/example/linebreakrotarcview/LineBreakArcRotView.kt
+++ linebreakrotarcview/src/main/java/com/example/linebreakrotarcview/LineBreakArcRotView.kt
@@ -4,7 +4,7 @@ import android.view.View
 import android.view.MotionEvent
 import android.graphics.Paint
 import android.graphics.RectF
-import android.graphics.Color
+import android.graphics.Canvas
 import androidx.core.graphics.toColorInt
 import android.app.Activity
 import android.content.Context
@@ -27,3 +27,37 @@ val rot : Float = 90f
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+
+fun Canvas.drawXY(x : Float, y : Float, cb : () -> Unit) {
+    save()
+    translate(x, y)
+    cb()
+    restore()
+}
+
+fun Canvas.drawLineBreakArcRot(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val dsc : (Int) -> Float = {
+        scale.divideScale(it, parts)
+    }
+    drawXY(w / 2 - (w / 2) * dsc(4), h / 2) {
+        rotate(rot * 2 * dsc(3))
+        for (j in 0..1) {
+            drawXY(size * 0.5f * j, 0f) {
+                rotate(rot * dsc(1) * j)
+                drawLine(0f, 0f, size * 0.5f * dsc(0).divideScale(j, 2), 0f, paint)
+            }
+        }
+        drawArc(RectF(0f, -size / 2, size, size / 2), rot, rot * dsc(2), false, paint)
+    }
+}
+
+fun Canvas.drawLBARNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = colors[i].toColorInt()
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    paint.style = Paint.Style.STROKE
+    drawLineBreakArcRot(scale, w, h, paint)
+}
