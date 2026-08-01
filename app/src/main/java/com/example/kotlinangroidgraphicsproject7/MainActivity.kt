15747d13b23a83ac8417f6b173b73ea12764e1a0
Anwesh43 <anweshthecool0@gmail.com>
2026-08-01 11:22:45 +0530

    linedownarcrotview: adding view in activity

--- app/src/main/java/com/example/kotlinangroidgraphicsproject7/MainActivity.kt
+++ app/src/main/java/com/example/kotlinangroidgraphicsproject7/MainActivity.kt
@@ -12,12 +12,12 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.kotlinangroidgraphicsproject7.ui.theme.KotlinAngroidGraphicsProject7Theme
-import com.example.lineshrinkexpandarcview.LineShrinkExpandArcView
+import com.example.linedownarcrotview.LineArcDownView
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         //LineRotPartialArcView.create(this)
-        LineShrinkExpandArcView.create(this)
+        LineArcDownView.create(this)
     }
 }
