c6339301c8239ccfcef9025ce0db1d7a2742f148
Anwesh43 <anweshthecool0@gmail.com>
2026-08-02 12:46:07 +0530

    lineencloserightarcview: adding view in activity

--- app/src/main/java/com/example/kotlinangroidgraphicsproject7/MainActivity.kt
+++ app/src/main/java/com/example/kotlinangroidgraphicsproject7/MainActivity.kt
@@ -12,12 +12,12 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.kotlinangroidgraphicsproject7.ui.theme.KotlinAngroidGraphicsProject7Theme
-import com.example.linedownarcrotview.LineArcDownView
+import com.example.lineencloserightarcview.LineEncloseRightArcView
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         //LineRotPartialArcView.create(this)
-        LineArcDownView.create(this)
+        LineEncloseRightArcView.create(this)
     }
 }
